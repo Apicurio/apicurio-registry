778de19675bf8e782f8fbc28df323e449d0ae37f
Eric Wittmann <eric.wittmann@gmail.com>
2019-08-07 21:04:41 -0400

    Proposed unified storage interface (#27)
    * Proposed unified storage interface
    
    Signed-off-by: Eric Wittmann <eric.wittmann@gmail.com>
    
    * Adding/updating content needs to return the meta-data
    
    * Updated storage layer based on code review/feedback etc.
    
    Signed-off-by: Eric Wittmann <eric.wittmann@gmail.com>

---
+++ app/src/main/java/io/apicurio/registry/store/RuleAlreadyExistsException.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2019 Red Hat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.apicurio.registry.store;
+
+/**
+ * @author eric.wittmann@gmail.com
+ */
+public class RuleAlreadyExistsException extends StorageException {
+
+    private static final long serialVersionUID = 2412206165461946827L;
+    
+    private final String ruleName;
+
+    /**
+     * Constructor.
+     */
+    public RuleAlreadyExistsException(String ruleName) {
+        this.ruleName = ruleName;
+    }
+
+    /**
+     * @return the ruleName
+     */
+    public String getRuleName() {
+        return ruleName;
+    }
+}

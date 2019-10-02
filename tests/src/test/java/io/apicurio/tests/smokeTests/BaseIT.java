e30487bc9427c2b2e03e6016bbab141f319a5e4d
Jakub Stejskal <stejskinek@gmail.com>
2019-10-02 22:21:34 +0200

    NEW: add checkstyle for tests and import Kafka cluster
    Signed-off-by: Jakub Stejskal <stejskinek@gmail.com>

--- tests/src/test/java/io/apicurio/tests/smokeTests/BaseIT.java
+++ tests/src/test/java/io/apicurio/tests/smokeTests/BaseIT.java
@@ -17,20 +17,43 @@
 package io.apicurio.tests.smokeTests;
 
 import io.apicurio.registry.util.H2DatabaseService;
+import io.apicurio.tests.KafkaFacade;
+import io.vertx.core.Vertx;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.client.WebClientOptions;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 public abstract class BaseIT {
 
+    static final Logger LOGGER = LogManager.getLogger(BaseIT.class);
+    static long timeout = 5L;
+
+    static Vertx vertx;
+    static WebClient client;
+
     private static H2DatabaseService h2ds = new H2DatabaseService();
+
+    static KafkaFacade kafkaCluster = new KafkaFacade();
 
     @BeforeAll
     static void beforeAll() throws Exception {
         h2ds.start();
+        kafkaCluster.start();
+        vertx = Vertx.vertx();
+
+        client = WebClient.create(vertx, new WebClientOptions()
+                .setDefaultHost("0.0.0.0")
+                .setDefaultPort(8080)
+        );
     }
 
     @AfterAll
     static void afterAll() {
+        kafkaCluster.stop();
+        vertx.close();
         h2ds.stop();
     }
 }

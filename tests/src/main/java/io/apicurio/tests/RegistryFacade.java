a18ec9380a211506917e33abdf7efb3466200299
Eric Wittmann <eric.wittmann@gmail.com>
2019-11-04 08:47:19 -0500

    Update RegistryFacade.java

--- tests/src/main/java/io/apicurio/tests/RegistryFacade.java
+++ tests/src/main/java/io/apicurio/tests/RegistryFacade.java
@@ -29,7 +29,7 @@ import java.util.concurrent.Future;
 public class RegistryFacade {
     static final Logger LOGGER = LoggerFactory.getLogger(RegistryFacade.class);
 
-    public static final String DEFAULT_REGISTRY_JAR_PATH = "../app/target/apicurio-registry-app-1.0.6-SNAPSHOT-runner.jar";
+    public static final String DEFAULT_REGISTRY_JAR_PATH = "../app/target/apicurio-registry-app-1.0.5-SNAPSHOT-runner.jar";
     public static final String DEFAULT_REGISTRY_PORT = "8080";
     public static final String DEFAULT_REGISTRY_URL = "localhost";
 

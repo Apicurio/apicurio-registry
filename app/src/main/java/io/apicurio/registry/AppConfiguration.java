db2ed28c2a87dd5791217aaf337fd56d24528555
Aleš Justin <ales.justin@gmail.com>
2020-03-03 20:06:21 +0100

    Move search to config. (#301)

--- app/src/main/java/io/apicurio/registry/AppConfiguration.java
+++ app/src/main/java/io/apicurio/registry/AppConfiguration.java
@@ -16,6 +16,8 @@
 
 package io.apicurio.registry;
 
+import io.apicurio.registry.search.client.SearchClient;
+import io.apicurio.registry.types.Current;
 import io.apicurio.registry.utils.PropertiesUtil;
 import io.apicurio.registry.utils.RegistryProperties;
 
@@ -36,6 +38,13 @@ public class AppConfiguration {
     public Properties properties(InjectionPoint ip) {
         RegistryProperties kp = ip.getAnnotated().getAnnotation(RegistryProperties.class);
         return PropertiesUtil.properties(kp);
+    }
+
+    @Produces
+    @ApplicationScoped
+    @Current
+    public SearchClient searchClient(@RegistryProperties("registry.search-index.") Properties properties) {
+        return SearchClient.create(properties);
     }
 
 }

3357fe30d98a295a0af3c3f36607edbbad66368f
Eric Wittmann <eric.wittmann@gmail.com>
2020-05-08 15:10:57 -0400

    Working on adding UI to the maven build

--- ui/packages/registry/src/index.tsx
+++ ui/packages/registry/src/index.tsx
@@ -3,7 +3,5 @@ import ReactDOM from "react-dom";
 import "@patternfly/react-core/dist/styles/base.css";
 import App from './app/app';
 import './app/app.css';
-import './config.js';
-import './version.js';
 
 ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);

2f52270f2d4908f326fa943487046ca9425e74b0
Eric Wittmann <eric.wittmann@gmail.com>
2020-05-11 14:26:59 -0400

    added favicon.  added redoc support to /api endpoint

--- ui/packages/registry/src/index.tsx
+++ ui/packages/registry/src/index.tsx
@@ -2,6 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "@patternfly/react-core/dist/styles/base.css";
 import App from './app/app';
-import './app/app.css';
+import "./app/app.css";
 
 ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);

e3f9f259798e25af55c4e8ff19f83d39809451cb
Aleš Justin <ales.justin@gmail.com>
2020-05-12 19:45:51 +0200

    Add secured readme, handle empty config. (#458)

--- common/src/main/java/io/apicurio/registry/utils/PropertiesUtil.java
+++ common/src/main/java/io/apicurio/registry/utils/PropertiesUtil.java
@@ -4,6 +4,8 @@ import io.quarkus.runtime.configuration.ProfileManager;
 import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.spi.ConfigProviderResolver;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Properties;
 
@@ -19,6 +21,14 @@ public class PropertiesUtil {
         if (profile != null && profile.length() > 0) {
             prefix = "%" + profile + "." + prefix;
         }
+        Map<String, String> defaults = new HashMap<>();
+        if (kp != null) {
+            String[] empties = kp.empties();
+            for (String e : empties) {
+                int p = e.indexOf("=");
+                defaults.put(e.substring(0, p), e.substring(p + 1));
+            }
+        }
         Properties properties = new Properties();
         for (String key : config.getPropertyNames()) {
             if (key.startsWith(prefix)) {
@@ -26,6 +36,12 @@ public class PropertiesUtil {
                 Optional<String> value = config.getOptionalValue(key, String.class);
                 if (value.isPresent()) {
                     properties.put(key.substring(prefix.length()), value.get());
+                } else if (defaults.size() > 0) {
+                    String sKey = key.substring(prefix.length());
+                    String defaultValue = defaults.get(sKey);
+                    if (defaultValue != null) {
+                        properties.put(sKey, defaultValue);
+                    }
                 }
             }
         }

bdcc9400b272a55a95e74ccca8ab587b8f40050a
Eric Wittmann <eric.wittmann@gmail.com>
2020-06-25 15:31:52 -0400

    Update RegistryClientTest.java

--- app/src/test/java/io/apicurio/registry/RegistryClientTest.java
+++ app/src/test/java/io/apicurio/registry/RegistryClientTest.java
@@ -270,9 +270,9 @@ public class RegistryClientTest extends AbstractResourceTestBase {
             Assertions.assertEquals(secondName, descResults.getArtifacts().get(1).getName());
             Assertions.assertEquals(firstName, descResults.getArtifacts().get(2).getName());
 
-            /*ArtifactSearchResults searchIdOverName = */client.searchArtifacts(firstArtifactId, 0, 5, SearchOver.name, SortOrder.asc);
-            Assertions.assertEquals(firstName, ascResults.getArtifacts().get(0).getName());
-            Assertions.assertEquals(firstArtifactId, ascResults.getArtifacts().get(0).getId());
+            ArtifactSearchResults searchIdOverName = client.searchArtifacts(firstArtifactId, 0, 5, SearchOver.name, SortOrder.asc);
+            Assertions.assertEquals(firstName, searchIdOverName.getArtifacts().get(0).getName());
+            Assertions.assertEquals(firstArtifactId, searchIdOverName.getArtifacts().get(0).getId());
 
         } finally {
             client.deleteArtifact(firstArtifactId);

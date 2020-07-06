107484f7709a4929e8caf71c37908df44d48ff12
Fabian Martinez <famartin@redhat.com>
2020-07-06 13:29:26 +0200

    tests - new kubernetes workflow

---
+++ .github/scripts/deploy_operator.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+TEST_NAMESPACE=apicurio-registry-test
+
+kubectl create namespace $TEST_NAMESPACE
+
+function waitForPodByLabel() {
+    LABEL_SEARCH=$1
+    while [[ $(kubectl get pods -n $TEST_NAMESPACE -l $LABEL_SEARCH -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo "waiting for pod $LABEL_SEARCH" && sleep 5; done
+}
+
+curl -sSL https://raw.githubusercontent.com/apicurio/apicurio-registry-operator/master/docs/resources/install.yaml | sed "s/{NAMESPACE}/$TEST_NAMESPACE/g" | kubectl apply -n $TEST_NAMESPACE -f -
+waitForPodByLabel "name=apicurio-registry-operator"
+
+curl -sSL https://github.com/strimzi/strimzi-kafka-operator/releases/download/0.18.0/strimzi-cluster-operator-0.18.0.yaml | sed "s/namespace: .*/namespace: $TEST_NAMESPACE/g" | kubectl apply -n $TEST_NAMESPACE -f -
+waitForPodByLabel "name=strimzi-cluster-operator"
+
+kubectl apply -n $TEST_NAMESPACE -f .github/scripts/kubefiles/kafka
+sleep 30
+kubectl get pod -n $TEST_NAMESPACE
+
+kubectl apply -n $TEST_NAMESPACE -f .github/scripts/kubefiles/apicurio/apicurio-registry-streams.yaml
+sleep 5
+kubectl get pod -n $TEST_NAMESPACE
+waitForPodByLabel "app=apicurio-registry-streams"
+kubectl wait deployment -l app=apicurio-registry-streams --for condition=available --timeout=180s
+kubectl get ingress -n $TEST_NAMESPACE
+
+# set -a
+# REGISTRY_HOST=localhost
+# REGISTRY_PORT=80
---
+++ .github/scripts/setup_kind.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+curl -Lo ./kind "https://kind.sigs.k8s.io/dl/v0.8.1/kind-$(uname)-amd64"
+chmod +x ./kind
+
+cat <<EOF | ./kind create cluster --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  kubeadmConfigPatches:
+  - |
+    kind: InitConfiguration
+    nodeRegistration:
+      kubeletExtraArgs:
+        node-labels: "ingress-ready=true"
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 80
+    protocol: TCP
+  - containerPort: 443
+    hostPort: 443
+    protocol: TCP
+EOF
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
+kubectl patch deployment ingress-nginx-controller -n ingress-nginx --type=json -p '[{"op":"add","path":"/spec/template/spec/containers/0/args/-","value":"--enable-ssl-passthrough"}]'

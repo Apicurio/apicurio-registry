68bafebf86905b6d881f2fe007940e36d9a9eb03
Miguel Soriano <msoriano@redhat.com>
2020-07-24 18:15:36 +0200

    Add rest_http_request_duration Timer metric

--- app/src/main/java/io/apicurio/registry/metrics/RestMetricsResponseFilter.java
+++ app/src/main/java/io/apicurio/registry/metrics/RestMetricsResponseFilter.java
@@ -3,12 +3,15 @@ package io.apicurio.registry.metrics;
 import static io.apicurio.registry.metrics.MetricIDs.REST_GROUP_TAG;
 import static org.eclipse.microprofile.metrics.MetricRegistry.Type.APPLICATION;
 import static org.eclipse.microprofile.metrics.MetricType.COUNTER;
+import static org.eclipse.microprofile.metrics.MetricType.TIMER;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 import javax.ws.rs.Path;
 import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.container.ResourceInfo;
@@ -19,13 +22,16 @@ import org.eclipse.microprofile.metrics.Counter;
 import org.eclipse.microprofile.metrics.Metadata;
 import org.eclipse.microprofile.metrics.MetricRegistry;
 import org.eclipse.microprofile.metrics.Tag;
+import org.eclipse.microprofile.metrics.Timer;
 import org.eclipse.microprofile.metrics.annotation.RegistryType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.smallrye.metrics.app.Clock;
+
 @Provider
 @RestMetricsResponseFilteredNameBinding
-public class RestMetricsResponseFilter implements ContainerResponseFilter {
+public class RestMetricsResponseFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
 	@Inject
 	@RegistryType(type = APPLICATION)
@@ -36,8 +42,21 @@ public class RestMetricsResponseFilter implements ContainerResponseFilter {
 	String REST_HTTP_REQUESTS_TOTAL = "rest_http_requests_total";
 	String REST_HTTP_REQUESTS_TOTAL_DESC = "Total number of REST HTTP Requests";
 
+	String REST_HTTP_REQUESTS_TIME = "rest_http_request_duration";
+	String REST_HTTP_REQUESTS_TIME_DESC = "Execution time of REST HTTP Requests in seconds";
+
+	private Clock clock = Clock.defaultClock();
+
+	public static final String REQUEST_START_TIME_CONTEXT_PROPERTY_NAME = "request-start-time";
+
 	@Context
 	private ResourceInfo resourceInfo;
+
+	@Override
+	public void filter(ContainerRequestContext requestContext) throws IOException {
+		long sample = clock.getTick();
+		requestContext.setProperty(REQUEST_START_TIME_CONTEXT_PROPERTY_NAME, sample);
+	}
 
 	@Override
 	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
@@ -67,6 +86,23 @@ public class RestMetricsResponseFilter implements ContainerResponseFilter {
 				new Tag("endpoint", this.getUri()), new Tag("method", requestContext.getMethod()), };
 		Counter statusFamilyCounter = metricRegistry.counter(metadata, counterTags);
 		statusFamilyCounter.inc();
+
+		long startTimeSample = (long) requestContext.getProperty(REQUEST_START_TIME_CONTEXT_PROPERTY_NAME);
+		long endTimeSample = clock.getTick();
+		long elapsed = endTimeSample - startTimeSample;
+
+		final Metadata timerMetadata = Metadata.builder().withName(REST_HTTP_REQUESTS_TIME)
+				.withDescription(REST_HTTP_REQUESTS_TIME_DESC).withType(TIMER).build();
+		Tag[] timerTags = { new Tag("group", REST_GROUP_TAG), new Tag("metric", REST_HTTP_REQUESTS_TIME),
+				new Tag("status", String.format("%dxx", statusFamilyCode)),
+				// TODO this information those not include base path (/api). Should we include
+				// it as part
+				// of the endpoint tag? or being in the rest_http_requests_total context it's
+				// assumed that user
+				// knows the path? maybe another tag named basePath?
+				new Tag("endpoint", this.getUri()), new Tag("method", requestContext.getMethod()), };
+		Timer timer = metricRegistry.timer(timerMetadata, timerTags);
+		timer.update(elapsed, TimeUnit.NANOSECONDS);
 	}
 
 	private String getUri() {

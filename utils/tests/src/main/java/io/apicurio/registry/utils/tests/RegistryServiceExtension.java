2e438711db61f56e945f9824a369724c81a1ed56
Eric Wittmann <eric.wittmann@gmail.com>
2020-08-04 11:37:40 -0400

    fix npe

--- utils/tests/src/main/java/io/apicurio/registry/utils/tests/RegistryServiceExtension.java
+++ utils/tests/src/main/java/io/apicurio/registry/utils/tests/RegistryServiceExtension.java
@@ -194,7 +194,6 @@ public class RegistryServiceExtension implements TestTemplateInvocationContextPr
                                     System.err.println("======== [RegistryServiceExtension] TCCL : " + tccl);
                                     System.err.println("======== [RegistryServiceExtension]      : " + tccl.getClass());
                                     System.err.println("======== [RegistryServiceExtension]      : " + tccl.getParent());
-                                    System.err.println("======== [RegistryServiceExtension]      : " + tccl.getParent().getClass());
                                     Class<?> clientClass = tccl.loadClass(RegistryClient.class.getName());
                                     Method factoryMethod = clientClass.getMethod(wrapper.method, String.class);
                                     wrapper.service = (AutoCloseable) factoryMethod.invoke(null, wrapper.registryUrl);

24d587a261a484416cf9438c9f35cca5a9c32953
Eric Wittmann <eric.wittmann@gmail.com>
2020-08-25 13:48:40 -0400

    Created an Antora based build for the documentation (#785)
    * added antora config and playbook
    
    * update the config
    
    * make good the move to .antora
    
    * dockerfile and build script to build site on windows
    
    * removed unneeded newlines
    
    * add a no-jekyll output file
    
    * updates to antora config and build script
    
    * added a readme
    
    Co-authored-by: Paul Wright <pwright@redhat.com>

---
+++ .antora/build.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+mkdir /work
+cd /work
+
+echo "Cloning git repository: $GIT_REPO"
+git clone $GIT_REPO
+cd /work/*
+git checkout $GIT_BRANCH
+cd $GIT_PATH
+
+echo "Cleaning output directory."
+rm -rf /antora-dist/*
+
+echo "Running : antora $PLAYBOOK"
+antora $PLAYBOOK
+echo "Antora build completed successfully."
+
+echo "Customizing output."
+find ./dist -name '*.html' -exec sed -i 's/_images/assets-images/g' {} \;
+find ./dist -name '_images' -ignore_readdir_race -execdir mv _images assets-images \;
+
+echo "Copying files..."
+cp -rf dist/* /antora-dist/.
+echo "Done."

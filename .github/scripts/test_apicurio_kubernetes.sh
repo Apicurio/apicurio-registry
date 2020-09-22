3e50c10033754aec31f2ad7e8d3964dec5f82f9f
Fabian Martinez <famartin@redhat.com>
2020-09-22 15:24:47 +0200

    fix kubernetes tests script

--- .github/scripts/test_apicurio_kubernetes.sh
+++ .github/scripts/test_apicurio_kubernetes.sh
@@ -2,7 +2,7 @@
 set -e -a
 
 E2E_APICURIO_PROJECT_DIR=$(pwd)
-BUNDLE_URL=$(pwd)/apicurio-registry-operator/docs/resources/install.yaml
+BUNDLE_URL=$(pwd)/apicurio-registry-k8s-tests-e2e/apicurio-registry-operator/docs/resources/install.yaml
 
 git clone https://github.com/Apicurio/apicurio-registry-k8s-tests-e2e.git
 

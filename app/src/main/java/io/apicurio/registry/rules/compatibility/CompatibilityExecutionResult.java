b5eb00eb9b0d6497940a845685711e20871c1973
adiohana <ohanaadi@gmail.com>
2020-10-28 15:37:19 +0200

    fixed typo

--- app/src/main/java/io/apicurio/registry/rules/compatibility/CompatibilityExecutionResult.java
+++ app/src/main/java/io/apicurio/registry/rules/compatibility/CompatibilityExecutionResult.java
@@ -8,7 +8,7 @@ import java.util.Set;
 /**
  * Created by aohana
  *
- * Holds the result for compatibility check
+ * Holds the result for a compatibility check
  * isCompatible - whether the compatibility check is successful or not
  * incompatibleDifferences - will contain values in case the schema type has difference type information in case the
  * new schema is not compatible (only JSON schema as of now)

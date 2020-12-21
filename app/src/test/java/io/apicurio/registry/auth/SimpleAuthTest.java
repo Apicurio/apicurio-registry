1088b1030eaddcdc72a621f47956ce85c0b5b145
Fabian Martinez <famartin@redhat.com>
2020-12-21 11:25:33 +0100

    auth tests

---
+++ app/src/test/java/io/apicurio/registry/auth/SimpleAuthTest.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright 2020 Red Hat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.apicurio.registry.auth;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.ByteArrayInputStream;
+import java.util.Collections;
+import java.util.List;
+
+import javax.ws.rs.NotAuthorizedException;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.apicurio.registry.AbstractResourceTestBase;
+import io.apicurio.registry.client.RegistryRestClient;
+import io.apicurio.registry.client.RegistryRestClientFactory;
+import io.apicurio.registry.client.exception.ArtifactNotFoundException;
+import io.apicurio.registry.rest.beans.ArtifactMetaData;
+import io.apicurio.registry.rest.beans.Rule;
+import io.apicurio.registry.types.ArtifactType;
+import io.apicurio.registry.types.RuleType;
+import io.apicurio.registry.utils.tests.TestUtils;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.TestProfile;
+
+@QuarkusTest
+@TestProfile(AuthTestProfile.class)
+public class SimpleAuthTest extends AbstractResourceTestBase {
+
+    @ConfigProperty(name = "registry.keycloak.url")
+    String authServerUrl;
+
+    @ConfigProperty(name = "registry.keycloak.realm")
+    String realm;
+
+    @ConfigProperty(name = "quarkus.oidc.tenant-enabled")
+    Boolean authEnabled;
+
+    String adminClientId = "registry-api";
+    String developerClientId = "registry-api-dev";
+    String readOnlyClientId = "registry-api-readonly";
+
+    String clientSecret = "test1";
+
+    @BeforeEach
+    void verifyAuth() {
+        System.out.println("Auth is " + authEnabled);
+    }
+
+    @AfterEach
+    void cleanArtifacts() throws Exception {
+        Auth auth = new KeycloakAuth(authServerUrl, realm, adminClientId, "test1");
+        RegistryRestClient client = RegistryRestClientFactory.create(registryUrl, Collections.emptyMap(), auth);
+        List<String> artifacts = client.listArtifacts();
+        for (String artifactId : artifacts) {
+            try {
+                client.deleteArtifact(artifactId);
+            } catch (AssertionError e) {
+                //because of async storage artifact may be already deleted but listed anyway
+//                LOGGER.info(e.getMessage());
+            } catch (Exception e) {
+//                LOGGER.error("", e);
+            }
+        }
+    }
+
+    @Test
+    public void testReadOnly() {
+
+        Auth auth = new KeycloakAuth(authServerUrl, realm, readOnlyClientId, "test1");
+
+        RegistryRestClient client = RegistryRestClientFactory.create(registryUrl, Collections.emptyMap(), auth);
+
+        client.listArtifacts();
+
+        Assertions.assertThrows(ArtifactNotFoundException.class, () -> {
+            client.getArtifactByGlobalId(2);
+        });
+
+        Assertions.assertThrows(ArtifactNotFoundException.class, () -> {
+            client.getLatestArtifact("abc");
+        });
+
+        Assertions.assertThrows(NotAuthorizedException.class, () -> {
+            client.createArtifact("ccc", ArtifactType.JSON, new ByteArrayInputStream("{}".getBytes()));
+        });
+
+    }
+
+    @Test
+    public void testDevRole() throws Exception {
+
+        Auth auth = new KeycloakAuth(authServerUrl, realm, developerClientId, "test1");
+
+        RegistryRestClient client = RegistryRestClientFactory.create(registryUrl, Collections.emptyMap(), auth);
+
+        try {
+            client.listArtifacts();
+
+            ArtifactMetaData meta = client.createArtifact("ccc", ArtifactType.JSON, new ByteArrayInputStream("{}".getBytes()));
+            TestUtils.retry(() -> client.getArtifactMetaDataByGlobalId(meta.getGlobalId()));
+
+            assertNotNull(client.getLatestArtifact(meta.getId()));
+
+            Rule ruleConfig = new Rule();
+            ruleConfig.setType(RuleType.VALIDITY);
+            client.createArtifactRule(meta.getId(), ruleConfig);
+
+            Assertions.assertThrows(NotAuthorizedException.class, () -> {
+                client.createGlobalRule(ruleConfig);
+            });
+        } finally {
+            client.deleteArtifact("ccc");
+        }
+
+
+
+    }
+
+    @Test
+    public void testAdminRole() throws Exception {
+
+        Auth auth = new KeycloakAuth(authServerUrl, realm, adminClientId, "test1");
+
+        RegistryRestClient client = RegistryRestClientFactory.create(registryUrl, Collections.emptyMap(), auth);
+
+        try {
+            client.listArtifacts();
+
+            ArtifactMetaData meta = client.createArtifact("ccc", ArtifactType.JSON, new ByteArrayInputStream("{}".getBytes()));
+            TestUtils.retry(() -> client.getArtifactMetaDataByGlobalId(meta.getGlobalId()));
+
+            assertNotNull(client.getLatestArtifact(meta.getId()));
+
+            Rule ruleConfig = new Rule();
+            ruleConfig.setType(RuleType.VALIDITY);
+            client.createArtifactRule(meta.getId(), ruleConfig);
+
+            client.createGlobalRule(ruleConfig);
+        } finally {
+            client.deleteArtifact("ccc");
+        }
+
+    }
+
+}

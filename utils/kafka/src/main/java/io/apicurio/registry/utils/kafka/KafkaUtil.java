d7607d41fef743e62e05e350664861411d61ef6a
Eric Wittmann <eric.wittmann@gmail.com>
2021-01-05 11:59:51 -0500

    Update KafkaUtil.java

--- utils/kafka/src/main/java/io/apicurio/registry/utils/kafka/KafkaUtil.java
+++ utils/kafka/src/main/java/io/apicurio/registry/utils/kafka/KafkaUtil.java
@@ -27,7 +27,6 @@ import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;

672b0687f804aabff97d729519a8a3e555886792
Eric Wittmann <eric.wittmann@gmail.com>
2021-01-20 16:23:54 -0500

    JAX-RS generated classes for v2 of the Registry API.

---
+++ common/src/main/java/io/apicurio/registry/rest/v2/beans/Properties.java
@@ -0,0 +1,19 @@
+
+package io.apicurio.registry.rest.v2.beans;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+
+/**
+ * User-defined name-value pairs. Name and value must be strings.
+ * 
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+
+})
+public class Properties {
+
+
+}

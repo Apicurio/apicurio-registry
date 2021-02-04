ff4650f3675ddeaae01facf6e11b6d3b8dde7e5c
Fabian Martinez <46371672+famartinrh@users.noreply.github.com>
2021-02-04 18:40:46 +0100

    [Apicurio] Please merge generated API project (#11)
    * CNCF Schema Registry 0.1
    
    * Delete JaxRsApplication.java

---
+++ app/src/main/java/io/apicurio/registry/cncf/schemaregistry/beans/SchemaId.java
@@ -0,0 +1,27 @@
+
+package io.apicurio.registry.cncf.schemaregistry.beans;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "id"
+})
+public class SchemaId {
+
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("id")
+    public String getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(String id) {
+        this.id = id;
+    }
+
+}

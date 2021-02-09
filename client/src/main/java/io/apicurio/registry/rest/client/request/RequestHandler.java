dd95d15f97b293ab5569ef102f01ca110c349d5c
Carles Arnal <carlesarnal92@gmail.com>
2021-02-09 14:46:30 +0100

    Simplify request handler

--- client/src/main/java/io/apicurio/registry/rest/client/request/RequestHandler.java
+++ client/src/main/java/io/apicurio/registry/rest/client/request/RequestHandler.java
@@ -40,82 +40,82 @@ import java.util.Optional;
  */
 public class RequestHandler {
 
-	private final HttpClient client;
-	private final String endpoint;
+    private final HttpClient client;
+    private final String endpoint;
 
-	public RequestHandler(String endpoint ) {
-		if (!endpoint.endsWith("/")) {
-			endpoint += "/";
-		}
-		final HttpClient.Builder httpClientBuilder = HttpClient.newBuilder();
+    public RequestHandler(String endpoint) {
+        if (!endpoint.endsWith("/")) {
+            endpoint += "/";
+        }
+        final HttpClient.Builder httpClientBuilder = HttpClient.newBuilder();
 
-		this.endpoint = endpoint;
-		this.client = httpClientBuilder.build();
-	}
+        this.endpoint = endpoint;
+        this.client = httpClientBuilder.build();
+    }
 
-	public  <T> T sendRequest(Operation operation, String requestPath, Map<String, List<String>> queryParams, HttpResponse.BodyHandler<T> bodyHandler, Object... pathParams) {
+    public <T> T sendRequest(Operation operation, String requestPath, Map<String, List<String>> queryParams, HttpResponse.BodyHandler<T> bodyHandler, Object... pathParams) {
 
-		return sendRequest(operation, requestPath, Collections.emptyMap(), queryParams, bodyHandler, Optional.empty(), pathParams);
-	}
+        return sendRequest(operation, requestPath, Collections.emptyMap(), queryParams, bodyHandler, Optional.empty(), pathParams);
+    }
 
-	public <T> T sendRequest(Operation operation, String requestPath, Map<String, String> headers,  Map<String, List<String>> queryParams, HttpResponse.BodyHandler<T> bodyHandler, Optional<InputStream> data, Object... pathParams) {
+    public <T> T sendRequest(Operation operation, String requestPath, Map<String, String> headers, Map<String, List<String>> queryParams, HttpResponse.BodyHandler<T> bodyHandler, Optional<InputStream> data, Object... pathParams) {
 
-		try {
-			HttpRequest.Builder requestBuilder = HttpRequest.newBuilder()
-					.uri(buildURI(endpoint + requestPath, queryParams, pathParams))
-					.header("Content-Type", "application/json")
-					.header("Accept", "application/json");
+        try {
+            HttpRequest.Builder requestBuilder = HttpRequest.newBuilder()
+                    .uri(buildURI(endpoint + requestPath, queryParams, pathParams))
+                    .header("Content-Type", "application/json")
+                    .header("Accept", "application/json");
 
-			headers.forEach(requestBuilder::header);
+            headers.forEach(requestBuilder::header);
 
-			switch (operation) {
-			case GET:
-				requestBuilder.GET();
-				break;
-			case PUT:
-				requestBuilder.PUT(HttpRequest.BodyPublishers.ofByteArray(data.get().readAllBytes()));
-				break;
-			case POST:
-				requestBuilder.POST(HttpRequest.BodyPublishers.ofByteArray(data.get().readAllBytes()));
-				break;
-			case DELETE:
-				requestBuilder.DELETE();
-				break;
-			default:
-				throw new IllegalStateException("Operation not allowed");
-		}
+            switch (operation) {
+                case GET:
+                    requestBuilder.GET();
+                    break;
+                case PUT:
+                    requestBuilder.PUT(HttpRequest.BodyPublishers.ofByteArray(data.get().readAllBytes()));
+                    break;
+                case POST:
+                    requestBuilder.POST(HttpRequest.BodyPublishers.ofByteArray(data.get().readAllBytes()));
+                    break;
+                case DELETE:
+                    requestBuilder.DELETE();
+                    break;
+                default:
+                    throw new IllegalStateException("Operation not allowed");
+            }
 
-		return client.send(requestBuilder.build(), bodyHandler)
-				.body();
+            return client.send(requestBuilder.build(), bodyHandler)
+                    .body();
 
-		} catch (URISyntaxException | IOException | InterruptedException e) {
-			throw parseError(e);
-		}
-	}
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            throw parseError(e);
+        }
+    }
 
-	private static URI buildURI(String basePath, Map<String, List<String>> queryParams, Object... pathParams) throws URISyntaxException {
+    private static URI buildURI(String basePath, Map<String, List<String>> queryParams, Object... pathParams) throws URISyntaxException {
 
-		final URIBuilder uriBuilder = new URIBuilder(String.format(basePath, pathParams));
+        final URIBuilder uriBuilder = new URIBuilder(String.format(basePath, pathParams));
 
-		final List<NameValuePair> queryParamsExpanded = new ArrayList<>();
+        final List<NameValuePair> queryParamsExpanded = new ArrayList<>();
 
-		//Iterate over query params list so we can add multiple query params with the same key
-		queryParams.forEach((key, paramList) -> paramList
-				.forEach(value -> queryParamsExpanded.add(new BasicNameValuePair(key, value))));
+        //Iterate over query params list so we can add multiple query params with the same key
+        queryParams.forEach((key, paramList) -> paramList
+                .forEach(value -> queryParamsExpanded.add(new BasicNameValuePair(key, value))));
 
-		uriBuilder.setParameters(queryParamsExpanded);
+        uriBuilder.setParameters(queryParamsExpanded);
 
-		return uriBuilder.build();
-	}
+        return uriBuilder.build();
+    }
 
-	private RestClientException parseError(Exception ex) {
+    private RestClientException parseError(Exception ex) {
 
-		//TODO build error
-		return new RestClientException(new Error());
-	}
+        //TODO build error
+        return new RestClientException(new Error());
+    }
 
 
-	public enum Operation {
-		PUT, POST, GET, DELETE
-	}
+    public enum Operation {
+        PUT, POST, GET, DELETE
+    }
 }

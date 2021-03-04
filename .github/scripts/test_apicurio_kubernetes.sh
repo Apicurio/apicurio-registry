04b3dc358f054e25435b06027e772bf97d037be2
Fabian Martinez <46371672+famartinrh@users.noreply.github.com>
2021-03-04 16:57:32 +0100

    Skip tests for asynmem storage variant (#1301)
    * improve verify workflow
    
    * skip tests for asyncmem
    
    * uncomment
    
    * fix kubernetes tests

--- .github/scripts/test_apicurio_kubernetes.sh
+++ .github/scripts/test_apicurio_kubernetes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -a
 
-# E2E_APICURIO_PROJECT_DIR=$(pwd)
+E2E_APICURIO_PROJECT_DIR=$(pwd)
 # BUNDLE_URL=$(pwd)/apicurio-registry-k8s-tests-e2e/apicurio-registry-operator/docs/resources/install.yaml
 
 git clone https://github.com/Apicurio/apicurio-registry-k8s-tests-e2e.git

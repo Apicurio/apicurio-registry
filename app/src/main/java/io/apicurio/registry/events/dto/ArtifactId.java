7c5b572901d5512250f7aaa3c527cf2a5c192ffd
Fabian Martinez <46371672+famartinrh@users.noreply.github.com>
2021-03-04 12:45:21 +0100

    integration events - add more event types (#1296)

---
+++ app/src/main/java/io/apicurio/registry/events/dto/ArtifactId.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2021 Red Hat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.apicurio.registry.events.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+
+/**
+ * Root Type for ArtifactId
+ * <p>
+ *
+ *
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "groupId",
+    "artifactId",
+    "version"
+})
+public class ArtifactId {
+
+    @JsonProperty("groupId")
+    private String groupId;
+
+    /**
+     *
+     * (Required)
+     *
+     */
+    @JsonProperty("artifactId")
+    private String artifactId;
+
+    @JsonProperty("version")
+    private String version;
+
+    @JsonProperty("groupId")
+    public String getGroupId() {
+        return groupId;
+    }
+
+    @JsonProperty("groupId")
+    public void setGroupId(String groupId) {
+        this.groupId = groupId;
+    }
+
+    /**
+     *
+     * (Required)
+     *
+     */
+    @JsonProperty("artifactId")
+    public String getArtifactId() {
+        return artifactId;
+    }
+
+    /**
+     *
+     * (Required)
+     *
+     */
+    @JsonProperty("artifactId")
+    public void setArtifactId(String artifactId) {
+        this.artifactId = artifactId;
+    }
+
+    @JsonProperty("version")
+    public String getVersion() {
+        return version;
+    }
+
+    @JsonProperty("version")
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+}

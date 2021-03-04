2b2078625971da499f620bed59ac5b945d85d7df
Carles Arnal <carnalca@redhat.com>
2021-03-04 17:48:42 +0100

    Fix ipv6 issue (#1295)
    * Fix ipv6 issue
    
    * Fix issue with ip with port
    
    * Update StreamsRegistryConfiguration.java
    
    Co-authored-by: Eric Wittmann <eric.wittmann@gmail.com>

--- storage/streams/src/main/java/io/apicurio/registry/streams/StreamsRegistryConfiguration.java
+++ storage/streams/src/main/java/io/apicurio/registry/streams/StreamsRegistryConfiguration.java
@@ -148,10 +148,16 @@ public class StreamsRegistryConfiguration {
     @Produces
     @Singleton
     public HostInfo storageLocalHost(StreamsProperties props) {
-        String appServer = props.getApplicationServer();
-        String[] hostPort = appServer.split(":");
+        // Remove brackets in case we have an ipv6 address of the form [2001:0db8:85a3:0000:0000:8a2e:0370:7334]:8080.
+        String appServer = props.getApplicationServer().replaceAll("\\[", "")
+                .replaceAll("\\]", "");
+
+        // In all cases we assume there is a :{port} included.  So we split on the last : and take the rest as the host.
+        int lastIndexOf = appServer.lastIndexOf(":");
+        String host = appServer.substring(0, lastIndexOf);
+        String port = appServer.substring(lastIndexOf + 1);
         log.info("Application server gRPC: '{}'", appServer);
-        return new HostInfo(hostPort[0], Integer.parseInt(hostPort[1]));
+        return new HostInfo(host, Integer.parseInt(port));
     }
 
     @Produces

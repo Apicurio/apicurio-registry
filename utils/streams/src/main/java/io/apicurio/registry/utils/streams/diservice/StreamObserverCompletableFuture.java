017ebf1b8ac049ebda1ddd1b7581e49986c304e5
Carles Arnal <carlesarnal92@gmail.com>
2021-03-12 16:01:16 +0100

    Add some logs to the grpc bridge

--- utils/streams/src/main/java/io/apicurio/registry/utils/streams/diservice/StreamObserverCompletableFuture.java
+++ utils/streams/src/main/java/io/apicurio/registry/utils/streams/diservice/StreamObserverCompletableFuture.java
@@ -1,10 +1,15 @@
 package io.apicurio.registry.utils.streams.diservice;
 
 import io.grpc.stub.StreamObserver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.CompletableFuture;
 
 public class StreamObserverCompletableFuture<T> extends CompletableFuture<T> implements StreamObserver<T> {
+
+    private static final Logger logger = LoggerFactory.getLogger(AsyncBiFunctionServiceGrpcLocalDispatcher.class);
+
     @Override
     public void onNext(T value) {
         complete(value);
@@ -12,6 +17,7 @@ public class StreamObserverCompletableFuture<T> extends CompletableFuture<T> imp
 
     @Override
     public void onError(Throwable throwable) {
+        logger.error("Error in stream observer completable future", throwable);
         completeExceptionally(throwable);
     }
 

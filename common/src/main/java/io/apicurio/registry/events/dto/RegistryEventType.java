1a62d254a5d07ffe79669c77362f14a57911b8a6
Fabian Martinez <famartin@redhat.com>
2021-03-22 09:50:20 +0100

    move events dtos to common module

--- common/src/main/java/io/apicurio/registry/events/dto/RegistryEventType.java
+++ common/src/main/java/io/apicurio/registry/events/dto/RegistryEventType.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.apicurio.registry.events;
+package io.apicurio.registry.events.dto;
 
 /**
  * @author Fabian Martinez

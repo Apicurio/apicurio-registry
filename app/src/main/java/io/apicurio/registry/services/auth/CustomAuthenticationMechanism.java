90365ff533e855b7781332fc8549636ed0c32372
Carles Arnal <carlesarnal92@gmail.com>
2021-03-30 16:07:28 +0200

    Add bearer token requester and basic auth test

--- app/src/main/java/io/apicurio/registry/services/auth/CustomAuthenticationMechanism.java
+++ app/src/main/java/io/apicurio/registry/services/auth/CustomAuthenticationMechanism.java
@@ -16,20 +16,34 @@
 
 package io.apicurio.registry.services.auth;
 
+import io.netty.handler.codec.http.HttpHeaderNames;
+import io.quarkus.oidc.AccessTokenCredential;
+import io.quarkus.oidc.runtime.BearerAuthenticationMechanism;
 import io.quarkus.oidc.runtime.OidcAuthenticationMechanism;
 import io.quarkus.security.identity.IdentityProviderManager;
 import io.quarkus.security.identity.SecurityIdentity;
 import io.quarkus.security.identity.request.AuthenticationRequest;
-import io.quarkus.vertx.http.runtime.security.BasicAuthenticationMechanism;
+import io.quarkus.security.identity.request.TokenAuthenticationRequest;
 import io.quarkus.vertx.http.runtime.security.ChallengeData;
 import io.quarkus.vertx.http.runtime.security.HttpAuthenticationMechanism;
 import io.quarkus.vertx.http.runtime.security.HttpCredentialTransport;
+import io.smallrye.mutiny.Uni;
 import io.vertx.ext.web.RoutingContext;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.keycloak.authorization.client.AuthzClient;
+import org.keycloak.authorization.client.Configuration;
 
 import javax.annotation.Priority;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Alternative;
 import javax.inject.Inject;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
 import java.util.Set;
 
 @Alternative
@@ -37,32 +51,113 @@ import java.util.Set;
 @ApplicationScoped
 public class CustomAuthenticationMechanism implements HttpAuthenticationMechanism {
 
-	@Inject
-	BasicAuthenticationMechanism basicAuthenticationMechanism;
 
-	@Inject
-	OidcAuthenticationMechanism oidcAuthenticationMechanism;
+    @Inject
+    OidcAuthenticationMechanism oidcAuthenticationMechanism;
 
-	@Override
-	public io.smallrye.mutiny.Uni<SecurityIdentity> authenticate(RoutingContext context, IdentityProviderManager identityProviderManager) {
-		// do some custom action and delegate to proper auth mechanism
+    @ConfigProperty(name = "registry.keycloak.url")
+    String authServerUrl;
 
-		return oidcAuthenticationMechanism.authenticate(context, identityProviderManager);
-	}
+    @ConfigProperty(name = "registry.keycloak.realm")
+    String authRealm;
 
-	@Override
-	public io.smallrye.mutiny.Uni<ChallengeData> getChallenge(RoutingContext context) {
+    @ConfigProperty(name = "quarkus.oidc.client-secret")
+    String clientSecret;
 
-		return oidcAuthenticationMechanism.getChallenge(context);
-	}
+    @ConfigProperty(name = "quarkus.oidc.client-id")
+    String clientId;
 
-	@Override
-	public Set<Class<? extends AuthenticationRequest>> getCredentialTypes() {
-		return oidcAuthenticationMechanism.getCredentialTypes();
-	}
+    private final BearerAuthenticationMechanism bearerAuth = new BearerAuthenticationMechanism();
 
-	@Override
-	public HttpCredentialTransport getCredentialTransport() {
-		return oidcAuthenticationMechanism.getCredentialTransport();
-	}
+    @Override
+    public Uni<SecurityIdentity> authenticate(RoutingContext context, IdentityProviderManager identityProviderManager) {
+        //Extracts username, password pair from the header and request a token to keycloak
+        String jwtToken = new BearerTokenExtractor(context, authServerUrl, authRealm, clientId, clientSecret).getBearerToken();
+        if (jwtToken != null) {
+            //If we manage to get a token from basic credentials, try to authenticate it using the fetched token using the identity provider manager
+            return identityProviderManager
+                    .authenticate(new TokenAuthenticationRequest(new AccessTokenCredential(jwtToken, context)));
+        } else {
+            return oidcAuthenticationMechanism.authenticate(context, identityProviderManager);
+        }
+    }
+
+    @Override
+    public Uni<ChallengeData> getChallenge(RoutingContext context) {
+        return bearerAuth.getChallenge(context);
+    }
+
+    @Override
+    public Set<Class<? extends AuthenticationRequest>> getCredentialTypes() {
+        return Collections.singleton(TokenAuthenticationRequest.class);
+    }
+
+    @Override
+    public HttpCredentialTransport getCredentialTransport() {
+        return new HttpCredentialTransport(HttpCredentialTransport.Type.AUTHORIZATION, "bearer");
+    }
+
+
+    //Extracts username, password pair from header if exists and request an access token to keycloak using those credentials
+    //Once Quarkus support both basic and bearer working at the same time against Keycloak, we can remove this
+    private static class BearerTokenExtractor {
+
+        /**
+         * A comma separated list of patterns and charsets. The pattern is a regular expression.
+         * <p>
+         * Because different browsers user different encodings this allows for the correct encoding to be selected based
+         * on the current browser. In general though it is recommended that BASIC auth not be used when passwords contain
+         * characters outside ASCII, as some browsers use the current locate to determine encoding.
+         * <p>
+         * This list must have an even number of elements, as it is interpreted as pattern,charset,pattern,charset,...
+         */
+
+        private final String BASIC = "basic";
+        private final String BASIC_PREFIX = BASIC + " ";
+        private final String LOWERCASE_BASIC_PREFIX = BASIC_PREFIX.toLowerCase(Locale.ENGLISH);
+        private final int PREFIX_LENGTH = BASIC_PREFIX.length();
+        private final Charset charset;
+        private final AuthzClient keycloakClient;
+        private final RoutingContext context;
+
+        public BearerTokenExtractor(RoutingContext context, final String authServerUrl, String realmName, String clientId, String clientSecret) {
+            this.context = context;
+            this.charset = StandardCharsets.UTF_8;
+            final HashMap<String, Object> credentials = new HashMap<>();
+            credentials.put("secret", clientSecret);
+            final Configuration keycloakConfiguration = new Configuration(authServerUrl, realmName, clientId, credentials, null);
+            this.keycloakClient = AuthzClient.create(keycloakConfiguration);
+        }
+
+        protected String getBearerToken() {
+            List<String> authHeaders = context.request().headers().getAll(HttpHeaderNames.AUTHORIZATION);
+            if (authHeaders != null) {
+                for (String current : authHeaders) {
+                    if (current.toLowerCase(Locale.ENGLISH).startsWith(LOWERCASE_BASIC_PREFIX)) {
+
+                        String base64Challenge = current.substring(PREFIX_LENGTH);
+                        String plainChallenge;
+                        byte[] decode = Base64.getDecoder().decode(base64Challenge);
+
+                        plainChallenge = new String(decode, charset);
+                        int colonPos;
+                        String COLON = ":";
+                        if ((colonPos = plainChallenge.indexOf(COLON)) > -1) {
+                            String userName = plainChallenge.substring(0, colonPos);
+                            String password = plainChallenge.substring(colonPos + 1);
+
+                            return authenticateRequest(userName, password);
+                        }
+                    }
+                }
+            }
+            //XX Intended null return
+            return null;
+        }
+
+        private String authenticateRequest(String username, String password) {
+
+            return keycloakClient.obtainAccessToken(username, password).getToken();
+        }
+    }
 }

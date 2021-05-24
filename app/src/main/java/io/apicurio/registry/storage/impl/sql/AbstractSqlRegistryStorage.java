772203a318856e65da2548a93296b0f18fbd2575
Eric Wittmann <eric.wittmann@gmail.com>
2021-05-24 14:44:12 -0400

    reverted an incorrect cherry-pick merge

--- app/src/main/java/io/apicurio/registry/storage/impl/sql/AbstractSqlRegistryStorage.java
+++ app/src/main/java/io/apicurio/registry/storage/impl/sql/AbstractSqlRegistryStorage.java
@@ -2266,63 +2266,6 @@ public abstract class AbstractSqlRegistryStorage extends AbstractRegistryStorage
         });
     }
 
-    /**
-     * @see io.apicurio.registry.storage.RegistryStorage#countArtifacts()
-     */
-    @Override
-    public long countArtifacts() throws RegistryStorageException {
-        return withHandle(handle -> {
-
-            String sql = sqlStatements.selectAllArtifactCount();
-            Long count = handle.createQuery(sql)
-                    .bind(0, tenantContext.tenantId())
-                    .mapTo(Long.class)
-                    .one();
-
-            return count;
-        });
-    }
-
-    /**
-     * @see io.apicurio.registry.storage.RegistryStorage#countArtifactVersions(java.lang.String, java.lang.String)
-     */
-    @Override
-    public long countArtifactVersions(String groupId, String artifactId) throws RegistryStorageException {
-        return withHandle(handle -> {
-
-            if (!isArtifactExists(groupId, artifactId)) {
-                throw new ArtifactNotFoundException(groupId, artifactId);
-            }
-
-            String sql = sqlStatements.selectAllArtifactVersionsCount();
-            Long count = handle.createQuery(sql)
-                    .bind(0, tenantContext.tenantId())
-                    .bind(1, normalizeGroupId(groupId))
-                    .bind(2, artifactId)
-                    .mapTo(Long.class)
-                    .one();
-
-            return count;
-        });
-    }
-
-    /**
-     * @see io.apicurio.registry.storage.RegistryStorage#countTotalArtifactVersions()
-     */
-    @Override
-    public long countTotalArtifactVersions() throws RegistryStorageException {
-        return withHandle(handle -> {
-
-            String sql = sqlStatements.selectTotalArtifactVersionsCount();
-            Long count = handle.createQuery(sql)
-                    .bind(0, tenantContext.tenantId())
-                    .mapTo(Long.class)
-                    .one();
-
-            return count;
-        });
-    }
-
     protected void resetGlobalId(Handle handle) {
         String sql = sqlStatements.selectMaxGlobalId();
         Optional<Long> maxGlobalId = handle.createQuery(sql)

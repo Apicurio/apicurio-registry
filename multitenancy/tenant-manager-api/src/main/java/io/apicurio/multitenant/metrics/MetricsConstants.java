48c785f85985bd8d951c6d08682e229ef7897931
Fabian Martinez <46371672+famartinrh@users.noreply.github.com>
2021-07-12 15:35:18 +0200

    tenant manager - disable micrometer http server metrics (#1669)

---
+++ multitenancy/tenant-manager-api/src/main/java/io/apicurio/multitenant/metrics/MetricsConstants.java
@@ -0,0 +1,26 @@
+package io.apicurio.multitenant.metrics;
+
+/**
+ * Metrics naming constants.
+ * <p>
+ * See:
+ * - https://micrometer.io/docs/concepts#_naming_meters
+ * - https://prometheus.io/docs/practices/naming/ (Micrometer abstracts some naming aspects.)
+ *
+ * @author Jakub Senko 'jsenko@redhat.com'
+ */
+public interface MetricsConstants {
+
+    // REST
+
+    String REST_PREFIX = "rest.";
+    String REST_REQUESTS = REST_PREFIX + "requests";
+    String REST_REQUESTS_DESCRIPTION = "Timing and results of REST endpoints calls";
+
+    // REST tags/labels
+
+    String REST_REQUESTS_TAG_PATH = "path";
+    String REST_REQUESTS_TAG_METHOD = "method";
+    String REST_REQUESTS_TAG_STATUS_CODE_FAMILY = "status_code_group";
+
+}

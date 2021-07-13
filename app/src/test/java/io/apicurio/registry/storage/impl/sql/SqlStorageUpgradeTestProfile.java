1facc85cbad0128ab04625a4c3849e66297e9bca
Eric Wittmann <eric.wittmann@gmail.com>
2021-07-13 12:09:05 -0400

    updated Auth related properties in openshift template

--- app/src/test/java/io/apicurio/registry/storage/impl/sql/SqlStorageUpgradeTestProfile.java
+++ app/src/test/java/io/apicurio/registry/storage/impl/sql/SqlStorageUpgradeTestProfile.java
@@ -42,6 +42,8 @@ public class SqlStorageUpgradeTestProfile implements QuarkusTestProfile {
     @Override
     public Map<String, String> getConfigOverrides() {
         Map<String, String> props = new HashMap<>();
+        // Note: we need to enable these properties so that we can access the role mapping REST API
+        // If these are not set, then the role mapping REST API will fail with a 403
         props.put("registry.auth.role-based-authorization", "true");
         props.put("registry.auth.role-source", "application");
         return props;

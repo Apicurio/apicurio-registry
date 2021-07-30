f99fdcef9088681145a211fb2e65f16e7c4e2348
Miroslav Safar <msafar@redhat.com>
2021-07-30 11:43:13 +0200

    Removed wildcard import

--- client/src/main/java/io/apicurio/registry/rest/client/impl/RegistryClientImpl.java
+++ client/src/main/java/io/apicurio/registry/rest/client/impl/RegistryClientImpl.java
@@ -17,7 +17,11 @@
 package io.apicurio.registry.rest.client.impl;
 
 import java.io.InputStream;
-import java.util.*;
+import java.util.Base64;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.nio.charset.StandardCharsets;
 
 import com.fasterxml.jackson.core.JsonProcessingException;

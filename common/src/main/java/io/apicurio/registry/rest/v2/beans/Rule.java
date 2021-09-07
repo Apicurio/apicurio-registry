c0ae09b41c7ccaf0f5808d5c14fc764025bd7633
Fabian Martinez <famartin@redhat.com>
2021-09-07 10:00:07 +0200

    undo DTO changes

--- common/src/main/java/io/apicurio/registry/rest/v2/beans/Rule.java
+++ common/src/main/java/io/apicurio/registry/rest/v2/beans/Rule.java
@@ -74,40 +74,4 @@ public class Rule {
         this.type = type;
     }
 
-    //TODO generate hashCode and equals from apicurio studio
-
-    /**
-     * @see java.lang.Object#hashCode()
-     */
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((config == null) ? 0 : config.hashCode());
-        result = prime * result + ((type == null) ? 0 : type.hashCode());
-        return result;
-    }
-
-    /**
-     * @see java.lang.Object#equals(java.lang.Object)
-     */
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Rule other = (Rule) obj;
-        if (config == null) {
-            if (other.config != null)
-                return false;
-        } else if (!config.equals(other.config))
-            return false;
-        if (type != other.type)
-            return false;
-        return true;
-    }
-
 }

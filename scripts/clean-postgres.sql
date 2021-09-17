229bb72e0a5d2adf524b9f80a6f20a00b4b6c1b6
Fabian Martinez <famartin@redhat.com>
2021-09-17 12:52:35 +0200

    clean sequences

--- scripts/clean-postgres.sql
+++ scripts/clean-postgres.sql
@@ -9,3 +9,15 @@ BEGIN
 END $$;
 
 SELECT schemaname, tablename, tableowner FROM pg_tables WHERE schemaname = 'public';
+
+SELECT relname FROM pg_class WHERE relkind = 'S';
+
+DO $$ DECLARE
+    r RECORD;
+BEGIN
+    FOR r IN (SELECT relname FROM pg_class WHERE relkind = 'S') LOOP
+        EXECUTE 'DROP SEQUENCE IF EXISTS ' || quote_ident(r.relname) || ' CASCADE';
+    END LOOP;
+END $$;
+
+SELECT relname FROM pg_class WHERE relkind = 'S';

ef9013047776aaa94bf6f36c432829533e250a90
Fabian Martinez <famartin@redhat.com>
2021-10-01 12:23:24 +0200

    tenant-manager: audit logging

---
+++ multitenancy/tenant-manager-api/src/main/java/io/apicurio/multitenant/logging/audit/AuditLogService.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2021 Red Hat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.apicurio.multitenant.logging.audit;
+
+import java.util.Map;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+
+/**
+ * @author Fabian Martinez
+ */
+@ApplicationScoped
+public class AuditLogService {
+
+    @Inject
+    Logger log;
+
+    public void log(String action, String result, Map<String, String> metadata) {
+        StringBuilder m = new StringBuilder();
+        m.append("tenant-manager")
+            .append(" ")
+            .append("action=\"").append(action).append("\" ")
+            .append("result=\"").append(result).append("\" ");
+        for (Map.Entry<String, String> e : metadata.entrySet()) {
+            m.append(e.getKey()).append("=\"").append(e.getValue()).append("\" ");
+        }
+        log.info(m.toString());
+    }
+
+}

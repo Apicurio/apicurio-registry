399bdad58007ddf3096b6ac90c08c0422ab93d73
Fabian Martinez <famartin@redhat.com>
2021-10-05 16:44:40 +0200

    tests: run legacy compatibility integration tests combined with new c… (#1903)
    * tests: run legacy compatibility integration tests combined with new ccompat api legacy mode feature

--- integration-tests/integration-tests-common/src/main/java/io/apicurio/tests/common/RegistryFacade.java
+++ integration-tests/integration-tests-common/src/main/java/io/apicurio/tests/common/RegistryFacade.java
@@ -159,6 +159,8 @@ public class RegistryFacade {
         appEnv.put("LOG_LEVEL", "DEBUG");
         appEnv.put("REGISTRY_LOG_LEVEL", "DEBUG");
 
+        loadProvidedAppEnv(appEnv);
+
         if (RegistryUtils.TEST_PROFILE.contains(Constants.MIGRATION)) {
             Map<String, String> registry1Env = new HashMap<>(appEnv);
             deployStorage(registry1Env);
@@ -464,7 +466,7 @@ public class RegistryFacade {
 
     private void runRegistry(String path, Map<String, String> appEnv) {
         Exec executor = new Exec();
-        LOGGER.info("Starting Registry app from: {}", path);
+        LOGGER.info("Starting Registry app from: {} env: {}", path, appEnv);
         CompletableFuture.supplyAsync(() -> {
             try {
 
@@ -644,6 +646,23 @@ public class RegistryFacade {
             }
         });
         processes.clear();
+    }
+
+    /**
+     * Reads environment variables with the prefix TEST_APP_ENV
+     */
+    private void loadProvidedAppEnv(Map<String, String> appEnv) {
+
+        String envPrefix = "TEST_APP_ENV_";
+
+        System.getenv()
+            .entrySet()
+            .stream()
+            .filter(env -> env.getKey().startsWith(envPrefix))
+            .forEach(env -> {
+                appEnv.put(env.getKey().substring(envPrefix.length()), env.getValue());
+            });
+
     }
 
 }

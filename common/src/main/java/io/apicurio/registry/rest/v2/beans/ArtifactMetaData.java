4bd314254301ae851c5c70d567a0130d7a859bfe
Eric Wittmann <eric.wittmann@gmail.com>
2021-10-08 13:55:21 -0400

    Added principalName to role mapping.

--- common/src/main/java/io/apicurio/registry/rest/v2/beans/ArtifactMetaData.java
+++ common/src/main/java/io/apicurio/registry/rest/v2/beans/ArtifactMetaData.java
@@ -59,7 +59,7 @@ public class ArtifactMetaData {
      * (Required)
      * 
      */
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssZ", timezone = "UTC")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
     @JsonProperty("createdOn")
     private Date createdOn;
     /**
@@ -74,7 +74,7 @@ public class ArtifactMetaData {
      * (Required)
      * 
      */
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssZ", timezone = "UTC")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
     @JsonProperty("modifiedOn")
     private Date modifiedOn;
     /**

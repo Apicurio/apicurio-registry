c733cab104325601ae89ff868a1786280f41621e
Carles Arnal <carlesarnal92@gmail.com>
2021-10-25 17:25:45 +0200

    Fix version type and upgrade scripts ddl

--- app/src/main/java/io/apicurio/registry/storage/impl/sql/AbstractSqlRegistryStorage.java
+++ app/src/main/java/io/apicurio/registry/storage/impl/sql/AbstractSqlRegistryStorage.java
@@ -754,7 +754,7 @@ public abstract class AbstractSqlRegistryStorage extends AbstractRegistryStorage
             //FIXME not the best solution possible since we're executing one query per reference, but works for now
             for (ArtifactReferenceDto reference : references) {
                 //To test if we can find a reference we look at the artifact metadata by globalId
-                getArtifactMetaData(reference.getGlobalId());
+                getArtifactVersionMetaData(reference.getGroupId(), reference.getArtifactId(), reference.getVersion());
             }
         }
     }

01a5effcda48c8838256c086c6e7f722dbfb24dc
Carles Arnal <carlesarnal92@gmail.com>
2021-11-04 17:53:45 +0100

    Add confluent serde test with references

---
+++ app/src/test/java/io/apicurio/registry/ccompat/ConfluentSerdeTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2021 Red Hat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.apicurio.registry.ccompat;
+
+import io.api.sample.TableNotification;
+import io.apicurio.registry.AbstractResourceTestBase;
+import io.confluent.kafka.serializers.protobuf.KafkaProtobufDeserializer;
+import io.confluent.kafka.serializers.protobuf.KafkaProtobufSerializer;
+import io.confluent.kafka.serializers.protobuf.KafkaProtobufSerializerConfig;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.Properties;
+
+
+@QuarkusTest
+public class ConfluentSerdeTest extends AbstractResourceTestBase {
+
+    @Test
+    public void testProtobufSchemaWithReferences() {
+        Properties properties = new Properties();
+        properties.setProperty(KafkaProtobufSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081/apis/ccompat/v6");
+        properties.setProperty(KafkaProtobufSerializerConfig.AUTO_REGISTER_SCHEMAS, "true");
+
+        KafkaProtobufSerializer kafkaProtobufSerializer = new KafkaProtobufSerializer();
+        kafkaProtobufSerializer.configure(properties, false);
+
+        byte[] data = kafkaProtobufSerializer.serialize("test",  TableNotification.newBuilder().build());
+
+        KafkaProtobufDeserializer protobufKafkaDeserializer = new KafkaProtobufDeserializer();
+        protobufKafkaDeserializer.configure(properties, false);
+
+        protobufKafkaDeserializer.deserialize("test", data);
+    }
+}

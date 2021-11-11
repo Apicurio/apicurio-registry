9f603b30049d049c86817887366c4dca1f451c40
Eric Wittmann <eric.wittmann@gmail.com>
2021-11-11 14:39:35 -0500

    Fixes #2007

--- app/src/main/java/io/apicurio/registry/ui/servlets/ResourceCacheControlFilter.java
+++ app/src/main/java/io/apicurio/registry/ui/servlets/ResourceCacheControlFilter.java
@@ -70,9 +70,18 @@ public class ResourceCacheControlFilter implements Filter {
         HttpServletResponse httpResponse = (HttpServletResponse) response;
         httpResponse.setDateHeader("Date", now.getTime()); //$NON-NLS-1$
 
-        // Don't cache non-file paths.  Also don't cache version.js or config.js files - those are dynamically generated.
-        if (requestURI == null || !requestURI.contains(".") || requestURI.contains("version.js") || 
-                requestURI.contains("config.js")) {
+        boolean disableCaching = false;
+        if (requestURI == null) {
+            disableCaching = false;
+        } else if (requestURI.contains("version.js")) {
+            disableCaching = false;
+        } else if (requestURI.contains("config.js")) {
+            disableCaching = false;
+        } else if (requestURI.contains("/apis/")) {
+            disableCaching = false;
+        }
+        
+        if (disableCaching) {
             disableHttpCaching(httpResponse);
         } else {
             // Cache most files for one year

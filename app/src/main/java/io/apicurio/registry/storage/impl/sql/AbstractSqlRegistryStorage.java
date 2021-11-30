b3ddff183bb400a0be9344ea13f1bc5cfd9051a8
Carles Arnal <carlesarnal92@gmail.com>
2021-11-30 18:21:55 +0100

    Update database version for consistency with MAS

--- app/src/main/java/io/apicurio/registry/storage/impl/sql/AbstractSqlRegistryStorage.java
+++ app/src/main/java/io/apicurio/registry/storage/impl/sql/AbstractSqlRegistryStorage.java
@@ -125,7 +125,7 @@ import io.quarkus.security.identity.SecurityIdentity;
  */
 public abstract class AbstractSqlRegistryStorage extends AbstractRegistryStorage {
 
-    private static int DB_VERSION = 5;
+    private static int DB_VERSION = 6;
     private static final Object dbMutex = new Object();
     private static final Object inmemorySequencesMutex = new Object();
 

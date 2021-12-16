7276515a309651cf7cc4667a9115d65c84254c95
Fabian Martinez <famartin@redhat.com>
2021-12-16 21:09:30 +0100

    feat: add missing metric for total requests counter (#2118)

--- app/src/main/java/io/apicurio/registry/metrics/MetricsConstants.java
+++ app/src/main/java/io/apicurio/registry/metrics/MetricsConstants.java
@@ -17,6 +17,9 @@ public interface MetricsConstants {
     String REST_REQUESTS = REST_PREFIX + "requests";
     String REST_REQUESTS_DESCRIPTION = "Timing and results of REST endpoints calls";
 
+    String REST_REQUESTS_COUNTER = REST_REQUESTS + ".count";
+    String REST_REQUESTS_COUNTER_DESCRIPTION = "Count and results of REST endpoints calls";
+
     // REST tags/labels
 
     String REST_REQUESTS_TAG_PATH = "path";

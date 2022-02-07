a8a7f2d8bc0d9d90e994b96f1298fe7da2d1ae17
Eric Wittmann <eric.wittmann@gmail.com>
2022-02-07 09:45:40 -0500

    removed some unused imports

--- common/src/main/java/io/apicurio/registry/rest/v2/IdsResource.java
+++ common/src/main/java/io/apicurio/registry/rest/v2/IdsResource.java
@@ -1,12 +1,9 @@
 package io.apicurio.registry.rest.v2;
 
-import io.apicurio.registry.rest.v2.beans.ArtifactReference;
-import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
 /**

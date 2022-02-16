05e523159eab50d807c223b6a96722149109c17b
Ondrej Babec <ond.babec@gmail.com>
2022-02-16 13:26:51 +0100

    Logging configuration

--- systemtests/src/test/java/io/apicurio/registry/systemtest/SimpleTestsIT.java
+++ systemtests/src/test/java/io/apicurio/registry/systemtest/SimpleTestsIT.java
@@ -1,14 +1,17 @@
 package io.apicurio.registry.systemtest;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class SimpleTestsIT {
-
+    private static Logger LOGGER = LoggerFactory.getLogger(SimpleTestsIT.class);
     @Test
     public void simpleTestIT() {
+        LOGGER.info("First test log!");
         assertThat("123", is("123"));
     }
 }

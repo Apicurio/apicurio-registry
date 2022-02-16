8644b5afd53543e194a55f09180e17603ceec1c2
Eric Wittmann <eric.wittmann@gmail.com>
2022-02-16 07:36:06 -0500

    Handle unknown errors in UI better.

--- ui/src/services/baseService.ts
+++ ui/src/services/baseService.ts
@@ -285,17 +285,17 @@ export abstract class BaseService implements Service {
     }
 
     private unwrapErrorData(error: any): any {
-        if (error.response && error.response.data) {
+        if (error && error.response && error.response.data) {
             return {
                 message: error.message,
                 ...error.response.data,
                 status: error.response.status
-            };
+            }
         } else if (error.response) {
             return {
                 message: error.message,
                 status: error.response.status
-            };
+            }
         } else {
             console.error("Unknown error detected: ", error);
             return {

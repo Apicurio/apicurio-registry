325336bbccbd08b3bb0a51b9957ed62dde9847e7
Carles Arnal <carlesarnal92@gmail.com>
2022-02-21 12:54:59 +0100

    Register artifact reference dto for reflection

--- app/src/main/java/io/apicurio/registry/storage/dto/ArtifactReferenceDto.java
+++ app/src/main/java/io/apicurio/registry/storage/dto/ArtifactReferenceDto.java
@@ -16,6 +16,7 @@
 
 package io.apicurio.registry.storage.dto;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -25,6 +26,7 @@ import lombok.ToString;
 @Builder
 @EqualsAndHashCode
 @ToString
+@RegisterForReflection
 public class ArtifactReferenceDto {
 
     private String groupId;

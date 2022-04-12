dcf791efdbaacd8519dc096d29ab9fffceac1252
Radim Kubis <rkubis@redhat.com>
2022-04-12 15:40:22 +0200

    New: Tests for operator installation

--- systemtests/src/test/java/io/apicurio/registry/systemtest/SimpleTestsIT.java
+++ systemtests/src/test/java/io/apicurio/registry/systemtest/SimpleTestsIT.java
@@ -3,9 +3,11 @@ package io.apicurio.registry.systemtest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import io.apicurio.registry.operator.api.model.ApicurioRegistry;
 import io.apicurio.registry.systemtest.framework.DatabaseUtils;
+import io.apicurio.registry.systemtest.operator.types.ApicurioRegistryBundleOperatorType;
 import io.apicurio.registry.systemtest.platform.Kubernetes;
 import io.apicurio.registry.systemtest.registryinfra.resources.ApicurioRegistryResourceType;
 import io.apicurio.registry.systemtest.registryinfra.resources.DeploymentResourceType;
+import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
 import io.fabric8.kubernetes.client.internal.SerializationUtils;
 import org.junit.jupiter.api.AfterAll;
@@ -13,7 +15,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
-import java.util.HashMap;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.List;
 
 public class SimpleTestsIT extends TestBase {
 
@@ -44,7 +48,7 @@ public class SimpleTestsIT extends TestBase {
     public void testApicurioRegistryWithSqlPersistenceBecomeReady(ExtensionContext testContext) {
         DatabaseUtils.deployDefaultPostgresqlDatabase(testContext);
 
-        ApicurioRegistry apicurioRegistry = ApicurioRegistryResourceType.getDefaultSql();
+        ApicurioRegistry apicurioRegistry = ApicurioRegistryResourceType.getDefaultSql("rkubis-test", "rkubis-namespace");
 
         try {
             resourceManager.createResource(testContext, true, apicurioRegistry);
@@ -53,6 +57,34 @@ public class SimpleTestsIT extends TestBase {
         }
 
         resourceManager.deleteResources(testContext);
+    }
+
+    /*
+    Operator test from file
+     */
+
+    @Test
+    public void testInstallApicurioRegistryBundleOperatorFile(ExtensionContext testContext) {
+        operatorManager.installOperator(new ApicurioRegistryBundleOperatorType("/Users/rkubis/codes/apicurio/install/install.yaml"), true);
+    }
+
+    @Test
+    public void testUninstallApicurioRegistryBundleOperatorFile(ExtensionContext testContext) {
+        operatorManager.uninstallOperator(new ApicurioRegistryBundleOperatorType("/Users/rkubis/codes/apicurio/install/install.yaml"), true);
+    }
+
+    /*
+    Operator test from URL
+     */
+
+    @Test
+    public void testInstallApicurioRegistryBundleOperatorUrl(ExtensionContext testContext) {
+        operatorManager.installOperator(new ApicurioRegistryBundleOperatorType("http://radimkubis.cz/install.yaml"), true);
+    }
+
+    @Test
+    public void testUninstallApicurioRegistryBundleOperatorUrl(ExtensionContext testContext) {
+        operatorManager.uninstallOperator(new ApicurioRegistryBundleOperatorType("http://radimkubis.cz/install.yaml"), true);
     }
 
     @Test
@@ -69,7 +101,19 @@ public class SimpleTestsIT extends TestBase {
     }
 
     @Test
-    public void testCode(ExtensionContext testContext) {
-        testLogger.info(Kubernetes.getClient().pods().inNamespace("postgresql").withLabels(new HashMap<String, String>() {{ put("app", "postgresql"); }}).list().getItems().toString());
+    public void testCode(ExtensionContext testContext) throws FileNotFoundException {
+        // System.getenv()
+        // testLogger.info(Kubernetes.getClient().pods().inNamespace("postgresql").withLabels(new HashMap<String, String>() {{ put("app", "postgresql"); }}).list().getItems().toString());
+        // testLogger.info(System.getenv("TEST"));
+
+        // Load Yaml into Kubernetes resources
+        List<HasMetadata> result = Kubernetes.getClient().load(new FileInputStream("/Users/rkubis/codes/apicurio/install/install.yaml")).get();
+        // Apply Kubernetes Resources
+        // Kubernetes.getClient().resourceList(result).inNamespace("rkubis-namespace").createOrReplace();
+
+        for (HasMetadata r : result) {
+            testLogger.info(r.getKind());
+        }
+
     }
 }

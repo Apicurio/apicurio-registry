7c9b5f3949b91c11c62a2f0d49e53563da374811
Radim Kubis <rkubis@redhat.com>
2022-04-13 12:14:18 +0200

    Update: Change default operator namespace name

--- systemtests/src/main/java/io/apicurio/registry/systemtest/framework/Constants.java
+++ systemtests/src/main/java/io/apicurio/registry/systemtest/framework/Constants.java
@@ -4,7 +4,12 @@ public final class Constants {
     public static final String APICURIO_REGISTRY_OPERATOR_SOURCE_PATH_ENV_VARIABLE = "APICURIO_REGISTRY_OPERATOR_SOURCE_PATH";
     public static final String APICURIO_REGISTRY_OPERATOR_SOURCE_PATH_DEFAULT_VALUE = "/tmp/install.yaml";
     public static final String APICURIO_REGISTRY_OPERATOR_NAMESPACE_ENV_VARIABLE = "APICURIO_REGISTRY_OPERATOR_NAMESPACE";
-    public static final String APICURIO_REGISTRY_OPERATOR_NAMESPACE_DEFAULT_VALUE = "apicurio-registry-test-namespace";
+    public static final String APICURIO_REGISTRY_OPERATOR_NAMESPACE_DEFAULT_VALUE = "apicurio-registry-operator-test-namespace";
 
     // TODO: Move waiting timeout and waiting check interval here, and other constants too.
+    // PostgreSQL port
+    // PostgreSQL username
+    // PostgreSQL password
+    // PostgreSQL admin password
+    // PostgreSQL database name
 }

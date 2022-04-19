276c25a6b2348789fe1565723031b85bfdc06369
Radim Kubis <rkubis@redhat.com>
2022-04-19 23:26:11 +0200

    New: Constants for Strimzi operator

--- systemtests/src/main/java/io/apicurio/registry/systemtest/framework/Constants.java
+++ systemtests/src/main/java/io/apicurio/registry/systemtest/framework/Constants.java
@@ -6,6 +6,11 @@ public final class Constants {
     public static final String APICURIO_REGISTRY_OPERATOR_NAMESPACE_ENV_VARIABLE = "APICURIO_REGISTRY_OPERATOR_NAMESPACE";
     public static final String APICURIO_REGISTRY_OPERATOR_NAMESPACE_DEFAULT_VALUE = "apicurio-registry-operator-namespace";
 
+    public static final String STRIMZI_CLUSTER_OPERATOR_SOURCE_PATH_ENV_VARIABLE = "STRIMZI_CLUSTER_OPERATOR_SOURCE_PATH";
+    public static final String STRIMZI_CLUSTER_OPERATOR_SOURCE_PATH_DEFAULT_VALUE = "https://strimzi.io/install/latest?namespace=strimzi-cluster-operator-namespace";
+    public static final String STRIMZI_CLUSTER_OPERATOR_NAMESPACE_ENV_VARIABLE = "STRIMZI_CLUSTER_OPERATOR_NAMESPACE";
+    public static final String STRIMZI_CLUSTER_OPERATOR_NAMESPACE_DEFAULT_VALUE = "strimzi-cluster-operator-namespace";
+
     // TODO: Move waiting timeout and waiting check interval here, and other constants too.
     // PostgreSQL port
     // PostgreSQL username

a64a95b1bf05e2d5ffe309c3b061903665cd90ec
Radim Kubis <rkubis@redhat.com>
2022-04-19 23:29:58 +0200

    New: OperatorKind for Strimzi operator

--- systemtests/src/main/java/io/apicurio/registry/systemtest/operator/types/OperatorKind.java
+++ systemtests/src/main/java/io/apicurio/registry/systemtest/operator/types/OperatorKind.java
@@ -3,4 +3,5 @@ package io.apicurio.registry.systemtest.operator.types;
 public interface OperatorKind {
     String APICURIO_REGISTRY_BUNDLE_OPERATOR = "ApicurioRegistryBundleOperator";
     String APICURIO_REGISTRY_OLM_OPERATOR = "ApicurioRegistryOLMOperator";
+    String STRIMZI_CLUSTER_BUNDLE_OPERATOR = "StrimziClusterBundleOperator";
 }

4a26e48b964f2982c62f978734de6950f5666805
Radim Kubis <rkubis@redhat.com>
2022-04-21 23:58:05 +0200

    New: Constants for OLM operator

--- systemtests/src/main/java/io/apicurio/registry/systemtest/framework/Constants.java
+++ systemtests/src/main/java/io/apicurio/registry/systemtest/framework/Constants.java
@@ -5,13 +5,16 @@ public final class Constants {
     public static final String APICURIO_REGISTRY_OPERATOR_SOURCE_PATH_DEFAULT_VALUE = "https://raw.githubusercontent.com/Apicurio/apicurio-registry-operator/master/install/apicurio-registry-operator-1.0.0-v2.0.0.final.yaml";
     public static final String APICURIO_REGISTRY_OPERATOR_NAMESPACE_ENV_VARIABLE = "APICURIO_REGISTRY_OPERATOR_NAMESPACE";
     public static final String APICURIO_REGISTRY_OPERATOR_NAMESPACE_DEFAULT_VALUE = "apicurio-registry-operator-namespace";
+    public static final String APICURIO_REGISTRY_OPERATOR_DEPLOYMENT_NAME = "apicurio-registry-operator";
+    public static final String APICURIO_REGISTRY_OPERATOR_CLUSTER_WIDE_NAMESPACE = "openshift-operators";
+
 
     public static final String STRIMZI_CLUSTER_OPERATOR_SOURCE_PATH_ENV_VARIABLE = "STRIMZI_CLUSTER_OPERATOR_SOURCE_PATH";
     public static final String STRIMZI_CLUSTER_OPERATOR_SOURCE_PATH_DEFAULT_VALUE = "https://strimzi.io/install/latest?namespace=strimzi-cluster-operator-namespace";
     public static final String STRIMZI_CLUSTER_OPERATOR_NAMESPACE_ENV_VARIABLE = "STRIMZI_CLUSTER_OPERATOR_NAMESPACE";
     public static final String STRIMZI_CLUSTER_OPERATOR_NAMESPACE_DEFAULT_VALUE = "strimzi-cluster-operator-namespace";
 
-    // TODO: Move waiting timeout and waiting check interval here, and other constants too.
+    // TODO: Move other constants here too.
     // PostgreSQL port
     // PostgreSQL username
     // PostgreSQL password

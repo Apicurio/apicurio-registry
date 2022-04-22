acde2c5910faf4fc1da9d7bdc38e13543b75c227
Radim Kubis <rkubis@redhat.com>
2022-04-22 06:44:14 +0200

    New: Flag detecting if namespace for operator was created or already existed Refactor: Rename variable for operator source from 'path' to 'source'

--- systemtests/src/main/java/io/apicurio/registry/systemtest/operator/types/Operator.java
+++ systemtests/src/main/java/io/apicurio/registry/systemtest/operator/types/Operator.java
@@ -1,15 +1,26 @@
 package io.apicurio.registry.systemtest.operator.types;
 
-import io.apicurio.registry.systemtest.framework.Constants;
 import io.apicurio.registry.systemtest.framework.LoggerUtils;
 import org.slf4j.Logger;
 
 public abstract class Operator {
     protected static final Logger operatorLogger = LoggerUtils.getLogger();
+    protected boolean namespaceCreated = false;
 
-    protected String path;
+    /*
+        Contains path to bundle operator file or OLM operator image.
+     */
+    protected String source;
 
-    public Operator(String path) {
-        this.path = path;
+    public Operator(String source) {
+        this.source = source;
+    }
+
+    public void setNamespaceCreated(boolean created) {
+        namespaceCreated = created;
+    }
+
+    public boolean getNamespaceCreated() {
+        return namespaceCreated;
     }
 }

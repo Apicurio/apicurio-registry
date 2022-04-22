d7477b8f634d5c564a8697fab44494734d89618f
Radim Kubis <rkubis@redhat.com>
2022-04-22 06:47:52 +0200

    Refactor: Rename variables in file downloading from url,path to source,destination New: Waiting methods for namespace to be ready/removed

--- systemtests/src/main/java/io/apicurio/registry/systemtest/framework/OperatorUtils.java
+++ systemtests/src/main/java/io/apicurio/registry/systemtest/framework/OperatorUtils.java
@@ -1,17 +1,22 @@
 package io.apicurio.registry.systemtest.framework;
 
+import io.apicurio.registry.systemtest.platform.Kubernetes;
 import io.apicurio.registry.systemtest.registryinfra.resources.ResourceKind;
+import io.apicurio.registry.systemtest.time.TimeoutBudget;
 import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
+import org.slf4j.Logger;
 
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.time.Duration;
 import java.util.List;
 
 public class OperatorUtils {
+    private static final Logger operatorUtilsLogger = LoggerUtils.getLogger();
     public static String getApicurioRegistryOperatorNamespace() {
         return System.getenv().getOrDefault(Constants.APICURIO_REGISTRY_OPERATOR_NAMESPACE_ENV_VARIABLE, Constants.APICURIO_REGISTRY_OPERATOR_NAMESPACE_DEFAULT_VALUE);
     }
@@ -30,9 +35,66 @@ public class OperatorUtils {
         return null;
     }
 
-    public static void downloadFile(String url, String path) throws Exception {
-        try (InputStream inputStream = (new URL(url)).openStream()) {
-            Files.copy(inputStream, Paths.get(path), StandardCopyOption.REPLACE_EXISTING);
+    public static void downloadFile(String source, String destination) throws Exception {
+        try (InputStream inputStream = (new URL(source)).openStream()) {
+            Files.copy(inputStream, Paths.get(destination), StandardCopyOption.REPLACE_EXISTING);
         }
     }
+
+    public static boolean waitNamespaceReady(String namespaceName) {
+        return waitNamespaceReady(namespaceName, TimeoutBudget.ofDuration(Duration.ofMinutes(1)));
+    }
+
+    public static boolean waitNamespaceReady(String namespaceName, TimeoutBudget timeoutBudget) {
+        while (!timeoutBudget.timeoutExpired()) {
+            if (Kubernetes.getClient().namespaces().withName(namespaceName).get().getStatus().getPhase().equals("Active")) {
+                return true;
+            }
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+
+                return false;
+            }
+        }
+
+        boolean pass = Kubernetes.getClient().namespaces().withName(namespaceName).get().getStatus().getPhase().equals("Active");
+
+        if (!pass) {
+            operatorUtilsLogger.info("Namespace {} failed readiness check.", namespaceName);
+        }
+
+        return pass;
+    }
+
+    public static boolean waitNamespaceRemoved(String namespaceName) {
+        return waitNamespaceRemoved(namespaceName, TimeoutBudget.ofDuration(Duration.ofMinutes(5)));
+    }
+
+    public static boolean waitNamespaceRemoved(String namespaceName, TimeoutBudget timeoutBudget) {
+        while (!timeoutBudget.timeoutExpired()) {
+            if (Kubernetes.getClient().namespaces().withName(namespaceName).get() == null) {
+                return true;
+            }
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+
+                return false;
+            }
+        }
+
+        boolean pass = Kubernetes.getClient().namespaces().withName(namespaceName).get() == null;
+
+        if (!pass) {
+            operatorUtilsLogger.info("Namespace {} failed removal check.", namespaceName);
+        }
+
+        return pass;
+    }
+
 }

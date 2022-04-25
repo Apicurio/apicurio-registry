bb6bbdf240c6bb902712fb93f49b166f61788bfe
Radim Kubis <rkubis@redhat.com>
2022-04-25 22:59:37 +0200

    New: OperatorKind for Keycloak operator

--- systemtests/src/main/java/io/apicurio/registry/systemtest/operator/types/OperatorKind.java
+++ systemtests/src/main/java/io/apicurio/registry/systemtest/operator/types/OperatorKind.java
@@ -4,4 +4,5 @@ public interface OperatorKind {
     String APICURIO_REGISTRY_BUNDLE_OPERATOR = "ApicurioRegistryBundleOperator";
     String APICURIO_REGISTRY_OLM_OPERATOR = "ApicurioRegistryOLMOperator";
     String STRIMZI_CLUSTER_BUNDLE_OPERATOR = "StrimziClusterBundleOperator";
+    String KEYCLOAK_OLM_OPERATOR = "KeycloakOLMOperator";
 }

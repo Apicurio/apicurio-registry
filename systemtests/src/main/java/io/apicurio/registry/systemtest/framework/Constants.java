913a1d1f113711a0234cf639b0d4e91fef02841c
Radim Kubis <rkubis@redhat.com>
2022-05-11 10:28:35 +0200

    New: Constants class for resource names

---
+++ systemtests/src/main/java/io/apicurio/registry/systemtest/framework/Constants.java
@@ -0,0 +1,9 @@
+package io.apicurio.registry.systemtest.framework;
+
+public final class Constants {
+    public static final String KAFKA_CONNECT_NAME = "kafka-connect-for-registry";
+    public static final String KAFKA_NAME = "kafka-for-registry";
+    public static final String KAFKA_USER = "kafka-user-for-registry";
+    public static final String REGISTRY_NAME = "registry";
+    public static final String REGISTRY_NAMESPACE = "namespace-for-registry";
+}

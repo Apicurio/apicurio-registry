8e27634efdd9f35fac813eb46e32b0bfad1848ea
Radim Kubis <rkubis@redhat.com>
2022-05-12 23:30:29 +0200

    Update: Add all Artifact types for registry

--- systemtests/src/main/java/io/apicurio/registry/systemtest/client/ArtifactType.java
+++ systemtests/src/main/java/io/apicurio/registry/systemtest/client/ArtifactType.java
@@ -2,4 +2,13 @@ package io.apicurio.registry.systemtest.client;
 
 public enum ArtifactType {
     AVRO,
+    PROTOBUF,
+    JSON,
+    OPENAPI,
+    ASYNCAPI,
+    GRAPHQL,
+    KCONNECT,
+    WSDL,
+    XSD,
+    XML
 }

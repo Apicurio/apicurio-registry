0a27936367bb967dfdc647c218ac6cdfefa71ca3
Eric Wittmann <eric.wittmann@gmail.com>
2022-07-01 09:14:08 -0400

    Updated merge script

--- merge-master.sh
+++ merge-master.sh
@@ -14,25 +14,16 @@ do
   fi
 done
 
-echo "OK great, updating version number using MAS scheme."
-CURRENT_VERSION=`mvn help:evaluate -Dexpression=project.version -q -DforceStdout`
-if [[ "$CURRENT_VERSION" =~ .*"-".* ]]; then
-   arrIN=(${CURRENT_VERSION//-/ })
-   CURRENT_VERSION=${arrIN[0]}
-fi
+echo "OK great, updating MAS build number."
 MAS_BUILD_NUMBER=`mvn help:evaluate -Dexpression=mas.build.number -q -DforceStdout`
 NEW_MAS_BUILD_NUMBER=$(($MAS_BUILD_NUMBER+1))
-NEW_VERSION="$CURRENT_VERSION-$NEW_MAS_BUILD_NUMBER"
 
-echo "Updating the MAS version to: $NEW_VERSION"
-mvn versions:set -DnewVersion=$NEW_VERSION -DgenerateBackupPoms=false -DprocessAllModules=true
 echo "Updating the MAS build number to: $NEW_MAS_BUILD_NUMBER"
 mvn versions:set-property -Dproperty=mas.build.number -DgenerateBackupPoms=false -DnewVersion=$NEW_MAS_BUILD_NUMBER
 
 git status
 echo "---"
-echo "MAS version and build numbers updated (see changes above)."
-echo "MAS version is now set to: $NEW_VERSION"
+echo "MAS build number updated (see changes above)."
 echo "MAS build number is now set to: $NEW_MAS_BUILD_NUMBER"
 echo "---"
 
@@ -46,7 +37,7 @@ do
 done
 
 git add .
-git commit -m "Updated MAS version to $NEW_VERSION and build number to $NEW_MAS_BUILD_NUMBER"
+git commit -m "Updated MAS build number to $NEW_MAS_BUILD_NUMBER"
 git push upstream mas-sr
 
 echo "All done!  Everything was successful.  Great job.  You're killing it!"

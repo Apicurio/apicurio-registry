279804a8c8828b66de2349dda27a09516e6157ec
Miroslav Šafář <msafar@redhat.com>
2022-07-18 18:48:25 +0200

    getContentIdsReferengingArtifact distinct

--- app/src/main/java/io/apicurio/registry/ccompat/store/RegistryStorageFacadeImpl.java
+++ app/src/main/java/io/apicurio/registry/ccompat/store/RegistryStorageFacadeImpl.java
@@ -344,6 +344,7 @@ public class RegistryStorageFacadeImpl implements RegistryStorageFacade {
             return contentIds.stream()
                     .flatMap(contentId -> storage.getArtifactVersionsByContentId(contentId).stream())
                     .map(ArtifactMetaDataDto::getGlobalId)
+                    .distinct()
                     .collect(Collectors.toList());
         }
 

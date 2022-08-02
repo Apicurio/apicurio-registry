7afeb55203a01eb007f665051ddee1bf50e91dc5
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2022-08-02 18:10:06 +0100

    Infrastructure to validate the generated code for the api v2

--- .github/scripts/install-tools.sh
+++ .github/scripts/install-tools.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 npm install -g @stoplight/spectral-cli
+
+wget https://github.com/andreaTP/apicurio-codegen/releases/download/0.0.5-SNAPSHOT/apicurio-codegen-linux -O apicurio-codegen
+chmod a+x apicurio-codegen

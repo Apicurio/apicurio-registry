ba3cb63414c251b1cd413b01483cb4fa026360a4
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2022-08-17 13:48:08 +0100

    use officially released codegen cli

--- .github/scripts/install-tools.sh
+++ .github/scripts/install-tools.sh
@@ -2,5 +2,5 @@
 
 npm install -g @stoplight/spectral-cli
 
-wget https://github.com/andreaTP/apicurio-codegen/releases/download/0.0.5-SNAPSHOT/apicurio-codegen-linux -O apicurio-codegen
+wget https://github.com/Apicurio/apicurio-codegen/releases/download/1.0.11.Final/apicurio-codegen-linux -O apicurio-codegen
 chmod a+x apicurio-codegen

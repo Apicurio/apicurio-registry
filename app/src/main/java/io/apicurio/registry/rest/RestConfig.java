927b30f87466fe703b3703ec78247cdb8c4a2357
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2022-08-26 16:00:19 +0100

    add Info to remaining configs

--- app/src/main/java/io/apicurio/registry/rest/RestConfig.java
+++ app/src/main/java/io/apicurio/registry/rest/RestConfig.java
@@ -2,15 +2,19 @@ package io.apicurio.registry.rest;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import io.apicurio.common.apps.config.Info;
+
 import javax.inject.Singleton;
 
 @Singleton
 public class RestConfig {
 
     @ConfigProperty(name = "registry.rest.artifact.download.maxSize", defaultValue = "1000000")
+    @Info( category = "rest", description = "Max size of the artifact allowed to be downloaded from URL", availableSince = "2.2.6-SNAPSHOT")
     int downloadMaxSize;
 
     @ConfigProperty(name = "registry.rest.artifact.download.skipSSLValidation", defaultValue = "false")
+    @Info( category = "rest", description = "Skip SSL validation when downloading artifacts from URL", availableSince = "2.2.6-SNAPSHOT")
     boolean downloadSkipSSLValidation;
 
     public int getDownloadMaxSize() { return this.downloadMaxSize; }

7ab24c38843cafa35ba37f4e1d91c339986173b9
Radim Kubis <rkubis@redhat.com>
2022-08-29 11:36:31 +0200

    Refactor(KeycloakUtils): Use separated Base64 functions from Base64Utils

--- system-tests/src/main/java/io/apicurio/registry/systemtests/framework/KeycloakUtils.java
+++ system-tests/src/main/java/io/apicurio/registry/systemtests/framework/KeycloakUtils.java
@@ -21,7 +21,6 @@ import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Paths;
 import java.time.Duration;
-import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -198,9 +197,9 @@ public class KeycloakUtils {
         // Get secret with Keycloak admin credentials
         Secret secret = Kubernetes.getSecret(Constants.TESTSUITE_NAMESPACE, "credential-" + Constants.SSO_NAME);
         // Get Keycloak admin username
-        String username = new String(Base64.getDecoder().decode(secret.getData().get("ADMIN_USERNAME")));
+        String username = Base64Utils.decode(secret.getData().get("ADMIN_USERNAME"));
         // Get Keycloak admin password
-        String password = new String(Base64.getDecoder().decode(secret.getData().get("ADMIN_PASSWORD")));
+        String password = Base64Utils.decode(secret.getData().get("ADMIN_PASSWORD"));
         // Get Keycloak admin URL
         String url = getKeycloakURL(Constants.TESTSUITE_NAMESPACE, "keycloak", true);
         // Construct token API URI of admin Keycloak Realm

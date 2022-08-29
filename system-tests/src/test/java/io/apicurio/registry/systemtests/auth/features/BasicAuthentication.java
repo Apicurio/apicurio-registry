1a379bb5d6ecd1dfac54790a8d6e6ffeb5d6d852
Radim Kubis <rkubis@redhat.com>
2022-08-29 15:20:25 +0200

    Fix(BasicAuthentication): Use service account credentials instead of user credentials

--- system-tests/src/test/java/io/apicurio/registry/systemtests/auth/features/BasicAuthentication.java
+++ system-tests/src/test/java/io/apicurio/registry/systemtests/auth/features/BasicAuthentication.java
@@ -4,7 +4,9 @@ import io.apicurio.registry.operator.api.model.ApicurioRegistry;
 import io.apicurio.registry.systemtests.client.ApicurioRegistryApiClient;
 import io.apicurio.registry.systemtests.client.ArtifactType;
 import io.apicurio.registry.systemtests.client.AuthMethod;
+import io.apicurio.registry.systemtests.client.KeycloakAdminApiClient;
 import io.apicurio.registry.systemtests.framework.ApicurioRegistryUtils;
+import io.apicurio.registry.systemtests.framework.Constants;
 import io.apicurio.registry.systemtests.framework.DeploymentUtils;
 import io.apicurio.registry.systemtests.framework.KeycloakUtils;
 import io.apicurio.registry.systemtests.platform.Kubernetes;
@@ -13,30 +15,50 @@ import io.fabric8.kubernetes.api.model.apps.Deployment;
 import org.apache.hc.core5.http.HttpStatus;
 import org.junit.jupiter.api.Assertions;
 
-import java.util.List;
-
 public class BasicAuthentication {
     public static void testBasicAuthentication(ApicurioRegistry apicurioRegistry, String username, String password) {
         /* RUN PRE-TEST ACTIONS */
 
-        // INITIALIZE API CLIENT
+        // GET REGISTRY HOSTNAME
         // Wait for readiness of registry hostname
         Assertions.assertTrue(ApicurioRegistryUtils.waitApicurioRegistryHostnameReady(apicurioRegistry));
-        // Create API client
-        ApicurioRegistryApiClient client = new ApicurioRegistryApiClient(
-                // Set hostname only
-                ApicurioRegistryUtils.getApicurioRegistryHostname(apicurioRegistry)
-        );
+        // Get registry hostname
+        String hostname = ApicurioRegistryUtils.getApicurioRegistryHostname(apicurioRegistry);
+
+        // GET CONTROL API CLIENT
+        // Create control API client for setup of registry before test
+        ApicurioRegistryApiClient controlClient = new ApicurioRegistryApiClient(hostname);
         // Get access token from Keycloak and update API client with it
-        client.setToken(KeycloakUtils.getAccessToken(apicurioRegistry, username, password));
+        controlClient.setToken(KeycloakUtils.getAccessToken(apicurioRegistry, username, password));
         // Set authentication method to token
-        client.setAuthMethod(AuthMethod.TOKEN);
-        // Set username
-        client.setUsername(username);
-        // Set password
-        client.setPassword(password);
+        controlClient.setAuthMethod(AuthMethod.TOKEN);
         // Wait for API availability
-        Assertions.assertTrue(client.waitServiceAvailable());
+        Assertions.assertTrue(controlClient.waitServiceAvailable());
+
+        // GET KEYCLOAK ADMIN API CLIENT
+        // Get Keycloak admin API URL
+        String keycloakUrl = KeycloakUtils.getDefaultKeycloakAdminURL();
+        // Create Keycloak admin API client
+        KeycloakAdminApiClient keycloakAdminApiClient = new KeycloakAdminApiClient(
+                // Set Keycloak admin API URL
+                keycloakUrl,
+                // Set admin access token
+                KeycloakUtils.getAdminAccessToken()
+        );
+        // Check that admin access token is available
+        Assertions.assertNotNull(keycloakAdminApiClient.getToken());
+
+        // GET TEST API CLIENT
+        // Create test API client for test actions
+        ApicurioRegistryApiClient testClient = new ApicurioRegistryApiClient(hostname);
+        // Set username
+        testClient.setUsername(Constants.SSO_TEST_CLIENT_API);
+        // Set secret
+        testClient.setPassword(keycloakAdminApiClient.getApiClientSecret());
+        // Check that client secret is available
+        Assertions.assertNotNull(testClient.getPassword());
+        // Set authentication method to basic
+        testClient.setAuthMethod(AuthMethod.BASIC);
 
         // PREPARE NECESSARY VARIABLES
         // Get registry deployment
@@ -44,8 +66,6 @@ public class BasicAuthentication {
                 apicurioRegistry.getMetadata().getNamespace(),
                 apicurioRegistry.getMetadata().getName() + "-deployment"
         );
-        // Save state of deployment's environment variables
-        List<EnvVar> savedEnvVars = DeploymentUtils.cloneDeploymentEnvVars(deployment);
         // Define artifact group ID
         String groupId = "default";
         // Define artifact ID
@@ -61,23 +81,34 @@ public class BasicAuthentication {
 
         // PREPARE REGISTRY CONTENT
         // Create artifact for test
-        Assertions.assertTrue(client.createArtifact(groupId, id, type, content));
-
-        // PREPARE API CLIENT
-        // Set authentication method to Basic
-        client.setAuthMethod(AuthMethod.BASIC);
+        Assertions.assertTrue(controlClient.createArtifact(groupId, id, type, content));
 
         /* RUN TEST ACTIONS */
 
         // TRY DEFAULT VALUE OF HTTP BASIC AUTHENTICATION
         // Wait for API availability
-        Assertions.assertTrue(client.waitServiceAvailable());
+        Assertions.assertTrue(controlClient.waitServiceAvailable());
         // Check that API returns 401 Unauthorized when reading artifacts
-        Assertions.assertNotNull(client.listArtifacts(1, HttpStatus.SC_UNAUTHORIZED));
+        Assertions.assertNotNull(testClient.listArtifacts(1, HttpStatus.SC_UNAUTHORIZED));
         // Check that API returns 401 Unauthorized when creating artifact
-        Assertions.assertTrue(client.createArtifact(groupId, failId, type, content, HttpStatus.SC_UNAUTHORIZED));
+        Assertions.assertTrue(testClient.createArtifact(groupId, failId, type, content, HttpStatus.SC_UNAUTHORIZED));
         // Check that API returns 401 Unauthorized when deleting artifact
-        Assertions.assertTrue(client.deleteArtifact(groupId, id, HttpStatus.SC_UNAUTHORIZED));
+        Assertions.assertTrue(testClient.deleteArtifact(groupId, id, HttpStatus.SC_UNAUTHORIZED));
+
+        // ENABLE HTTP BASIC AUTHENTICATION AND TEST IT
+        // Set environment variable REGISTRY_AUTH_ANONYMOUS_READ_ACCESS_ENABLED of deployment to true
+        DeploymentUtils.createOrReplaceDeploymentEnvVar(deployment, new EnvVar() {{
+            setName("CLIENT_CREDENTIALS_BASIC_AUTH_ENABLED");
+            setValue("true");
+        }});
+        // Wait for API availability
+        Assertions.assertTrue(controlClient.waitServiceAvailable());
+        // Check that API returns 200 OK when reading artifacts
+        Assertions.assertNotNull(testClient.listArtifacts());
+        // Check that API returns 200 OK when creating artifact
+        Assertions.assertTrue(testClient.createArtifact(groupId, succeedId + "true-value", type, content));
+        // Check that API returns 204 No Content when deleting artifact
+        Assertions.assertTrue(testClient.deleteArtifact(groupId, succeedId + "true-value"));
 
         // DISABLE HTTP BASIC AUTHENTICATION AND TEST IT
         // Set environment variable CLIENT_CREDENTIALS_BASIC_AUTH_ENABLED of deployment to false
@@ -86,39 +117,12 @@ public class BasicAuthentication {
             setValue("false");
         }});
         // Wait for API availability
-        Assertions.assertTrue(client.waitServiceAvailable());
+        Assertions.assertTrue(controlClient.waitServiceAvailable());
         // Check that API returns 401 Unauthorized when reading artifacts
-        Assertions.assertNotNull(client.listArtifacts(1, HttpStatus.SC_UNAUTHORIZED));
+        Assertions.assertNotNull(testClient.listArtifacts(1, HttpStatus.SC_UNAUTHORIZED));
         // Check that API returns 401 Unauthorized when creating artifact
-        Assertions.assertTrue(client.createArtifact(groupId, failId, type, content, HttpStatus.SC_UNAUTHORIZED));
+        Assertions.assertTrue(testClient.createArtifact(groupId, failId, type, content, HttpStatus.SC_UNAUTHORIZED));
         // Check that API returns 401 Unauthorized when deleting artifact
-        Assertions.assertTrue(client.deleteArtifact(groupId, id, HttpStatus.SC_UNAUTHORIZED));
-
-        // ENABLE HTTP BASIC AUTHENTICATION AND TEST IT
-        // Set environment variable REGISTRY_AUTH_ANONYMOUS_READ_ACCESS_ENABLED of deployment to false
-        DeploymentUtils.createOrReplaceDeploymentEnvVar(deployment, new EnvVar() {{
-            setName("CLIENT_CREDENTIALS_BASIC_AUTH_ENABLED");
-            setValue("true");
-        }});
-        // Wait for API availability
-        Assertions.assertTrue(client.waitServiceAvailable());
-        // Check that API returns 200 OK when reading artifacts
-        Assertions.assertNotNull(client.listArtifacts());
-        // Check that API returns 200 OK when creating artifact
-        Assertions.assertTrue(client.createArtifact(groupId, succeedId + "true-value", type, content));
-        // Check that API returns 204 No Content when deleting artifact
-        Assertions.assertTrue(client.deleteArtifact(groupId, succeedId + "true-value"));
-
-        /* RUN POST-TEST ACTIONS */
-
-        // RETURN REGISTRY INTO PRE-TEST STATE
-        // Restore pre-test state of environment variables
-        DeploymentUtils.setDeploymentEnvVars(deployment, savedEnvVars);
-        // Wait for API availability
-        Assertions.assertTrue(client.waitServiceAvailable());
-        // Set authentication method back to Token
-        client.setAuthMethod(AuthMethod.TOKEN);
-        // Remove artifact created for test
-        Assertions.assertTrue(client.deleteArtifact(groupId, id));
+        Assertions.assertTrue(testClient.deleteArtifact(groupId, id, HttpStatus.SC_UNAUTHORIZED));
     }
 }

961210daa604f270b4f0be1d3b4f3c0b1fa456a3
Carles Arnal <carnalca@redhat.com>
2022-09-06 13:31:41 +0200

    Regenerate JAX-RS after adding group administration operations.

---
+++ common/src/main/java/io/apicurio/registry/rest/v2/beans/CreateGroupMetaData.java
@@ -0,0 +1,66 @@
+
+package io.apicurio.registry.rest.v2.beans;
+
+import java.util.Map;
+import javax.annotation.processing.Generated;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+
+/**
+ * Root Type for CreateGroupMetaData
+ * <p>
+ * 
+ * 
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "description",
+    "properties"
+})
+@Generated("jsonschema2pojo")
+@io.quarkus.runtime.annotations.RegisterForReflection
+@lombok.ToString
+public class CreateGroupMetaData {
+
+    @JsonProperty("description")
+    private String description;
+    /**
+     * User-defined name-value pairs. Name and value must be strings.
+     * 
+     */
+    @JsonProperty("properties")
+    @JsonPropertyDescription("User-defined name-value pairs. Name and value must be strings.")
+    private Map<String, String> properties;
+
+    @JsonProperty("description")
+    public String getDescription() {
+        return description;
+    }
+
+    @JsonProperty("description")
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * User-defined name-value pairs. Name and value must be strings.
+     * 
+     */
+    @JsonProperty("properties")
+    public Map<String, String> getProperties() {
+        return properties;
+    }
+
+    /**
+     * User-defined name-value pairs. Name and value must be strings.
+     * 
+     */
+    @JsonProperty("properties")
+    public void setProperties(Map<String, String> properties) {
+        this.properties = properties;
+    }
+
+}

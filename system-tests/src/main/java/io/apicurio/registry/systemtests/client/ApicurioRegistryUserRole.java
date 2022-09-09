00382507db4e999604abaaf9916af435f0ef74bc
Radim Kubis <rkubis@redhat.com>
2022-09-09 10:51:38 +0200

    New(ApicurioRegistryUserRole): Internal Apicurio Registry roles

---
+++ system-tests/src/main/java/io/apicurio/registry/systemtests/client/ApicurioRegistryUserRole.java
@@ -0,0 +1,10 @@
+package io.apicurio.registry.systemtests.client;
+
+public enum ApicurioRegistryUserRole {
+    ADMIN("ADMIN"),
+    DEVELOPER("DEVELOPER"),
+    READ_ONLY("READ_ONLY");
+
+    ApicurioRegistryUserRole(String value) {
+    }
+}

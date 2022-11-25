a6000e49c331261ca126f29d359a053cb27e12e8
Radim Kubis <rkubis@redhat.com>
2022-11-25 11:57:57 +0100

    Remove(Environment.java): No interaction with environment is needed, will use SmallRye in the future

--- new-system-tests/src/main/java/io/apicurio/registry/systemtests/Environment.java
+++
@@ -1,9 +0,0 @@
-package io.apicurio.registry.systemtests;
-
-/**
- * Environment variables used in test suite and tests.
- */
-public class Environment {
-    /** Type of cluster for tests. */
-    public static final String CLUSTER_TYPE = System.getenv().getOrDefault("CLUSTER_TYPE", "kubernetes");
-}

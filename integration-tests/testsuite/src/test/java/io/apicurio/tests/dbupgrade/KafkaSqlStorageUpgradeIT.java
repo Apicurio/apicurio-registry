4b146a3e4606c0895621ba2cf72040bd5b4a0c64
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2022-12-06 12:42:23 +0000

    refactoring

--- integration-tests/testsuite/src/test/java/io/apicurio/tests/dbupgrade/KafkaSqlStorageUpgradeIT.java
+++ integration-tests/testsuite/src/test/java/io/apicurio/tests/dbupgrade/KafkaSqlStorageUpgradeIT.java
@@ -20,12 +20,9 @@ import static io.apicurio.tests.utils.CustomTestsUtils.createArtifact;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -35,13 +32,7 @@ import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.images.RemoteDockerImage;
 import org.testcontainers.utility.DockerImageName;
 
-import io.apicurio.registry.rest.client.RegistryClient;
-import io.apicurio.registry.rest.client.RegistryClientFactory;
-import io.apicurio.registry.rest.v2.beans.ArtifactMetaData;
-import io.apicurio.registry.rest.v2.beans.Rule;
-import io.apicurio.registry.rest.v2.beans.VersionMetaData;
 import io.apicurio.registry.types.ArtifactType;
-import io.apicurio.registry.types.RuleType;
 import io.apicurio.registry.utils.IoUtil;
 import io.apicurio.registry.utils.tests.SimpleDisplayName;
 import io.apicurio.tests.ApicurioV2BaseIT;
@@ -50,8 +41,6 @@ import io.apicurio.tests.common.RegistryFacade;
 import io.apicurio.tests.common.RegistryStorageType;
 import io.apicurio.tests.common.interfaces.TestSeparator;
 import io.apicurio.tests.common.utils.RegistryUtils;
-import io.apicurio.tests.multitenancy.MultitenancySupport;
-import io.apicurio.tests.multitenancy.TenantUserClient;
 import io.apicurio.tests.utils.CustomTestsUtils.ArtifactData;
 
 /**
@@ -96,7 +85,7 @@ public class KafkaSqlStorageUpgradeIT implements TestSeparator, Constants {
 
             //
 
-            var registryClient = RegistryClientFactory.create("http://localhost:8081");
+            var registryClient = RetrocompatibleRegistryClientUtils.create("http://localhost:8081");
 
             createArtifact(registryClient, ArtifactType.AVRO, ApicurioV2BaseIT.resourceToString("artifactTypes/" + "avro/multi-field_v1.json"));
             createArtifact(registryClient, ArtifactType.JSON, ApicurioV2BaseIT.resourceToString("artifactTypes/" + "jsonSchema/person_v1.json"));
@@ -161,90 +150,6 @@ public class KafkaSqlStorageUpgradeIT implements TestSeparator, Constants {
             }
         }
 
-    }
-
-    private List<TenantData> loadData(MultitenancySupport mt) throws Exception {
-
-        List<TenantData> tenants = new ArrayList<>();
-
-        for (int i = 0; i < 50; i++) {
-
-            TenantData tenant = new TenantData();
-            TenantUserClient user = mt.createTenant();
-            tenant.tenant = user;
-            tenants.add(tenant);
-
-            RegistryClient client = user.client;
-
-            Rule comp = new Rule();
-            comp.setType(RuleType.COMPATIBILITY);
-            comp.setConfig("BACKWARD");
-            client.createGlobalRule(comp);
-            Rule val = new Rule();
-            val.setType(RuleType.VALIDITY);
-            val.setConfig("SYNTAX_ONLY");
-            client.createGlobalRule(val);
-
-            tenant.artifacts.add(createArtifact(client, ArtifactType.AVRO, ApicurioV2BaseIT.resourceToString("artifactTypes/" + "avro/multi-field_v1.json")));
-            tenant.artifacts.add(createArtifact(client, ArtifactType.JSON, ApicurioV2BaseIT.resourceToString("artifactTypes/" + "jsonSchema/person_v1.json")));
-            tenant.artifacts.add(createArtifact(client, ArtifactType.ASYNCAPI, ApicurioV2BaseIT.resourceToString("artifactTypes/" + "asyncapi/2.0-streetlights_v1.json")));
-
-        }
-
-        return tenants;
-
-    }
-
-    private void createMoreArtifacts(List<TenantData> tenants) throws Exception {
-        for (TenantData tenant : tenants) {
-            var client = tenant.tenant.client;
-
-            tenant.artifacts.add(createArtifact(client, ArtifactType.AVRO, ApicurioV2BaseIT.resourceToString("artifactTypes/" + "avro/multi-field_v1.json")));
-            tenant.artifacts.add(createArtifact(client, ArtifactType.KCONNECT, ApicurioV2BaseIT.resourceToString("artifactTypes/" + "kafkaConnect/simple_v1.json")));
-        }
-    }
-
-    private void verifyData(List<TenantData> tenants) {
-        for (TenantData tenant : tenants) {
-
-            var client = tenant.tenant.client;
-
-            var grules = client.listGlobalRules();
-            assertEquals(2, grules.size());
-
-            for (ArtifactData data : tenant.artifacts) {
-                ArtifactMetaData meta = data.meta;
-
-                String content = IoUtil.toString(client.getArtifactVersion(meta.getGroupId(), meta.getId(), meta.getVersion()));
-                String contentHash = DigestUtils.sha256Hex(IoUtil.toBytes(content));
-
-                assertEquals(data.contentHash, contentHash);
-
-                String contentgid = IoUtil.toString(client.getContentByGlobalId(meta.getGlobalId()));
-                String contentgidHash = DigestUtils.sha256Hex(IoUtil.toBytes(contentgid));
-
-                assertEquals(data.contentHash, contentgidHash);
-
-                String contentcid = IoUtil.toString(client.getContentById(meta.getContentId()));
-                String contentcidHash = DigestUtils.sha256Hex(IoUtil.toBytes(contentcid));
-
-                assertEquals(data.contentHash, contentcidHash);
-
-                VersionMetaData vmeta = client.getArtifactVersionMetaData(meta.getGroupId(), meta.getId(), meta.getVersion());
-                assertEquals(meta.getContentId(), vmeta.getContentId());
-
-            }
-
-        }
-    }
-
-    private class TenantData {
-        TenantUserClient tenant;
-        List<ArtifactData> artifacts;
-
-        public TenantData() {
-            artifacts = new ArrayList<>();
-        }
     }
 
 }

4c43e6d367150623f9eb617f714897329b62e68d
Jakub Senko <jsenko@redhat.com>
2022-12-07 13:09:29 +0100

    fix: redirect configuration (#2955)
    * fix: redirect filter is applied only to '/'
    
    * fix: mark filter as supporting async, to work with ibmcompat API
    
    * fix: attempt to avoid CheckPeriodCache error [1], add eviction
    
    [1]: java.lang.IllegalStateException: Recursive update
     at java.base/java.util.concurrent.ConcurrentHashMap.compute(ConcurrentHashMap.java:1991)
     at io.apicurio.registry.utils.CheckPeriodCache.compute(CheckPeriodCache.java:36)
     at io.apicurio.registry.mt.TenantContextLoader.loadRequestContext(TenantContextLoader.java:81)
    
    * fix: add eviction check to all methods that potentially increase the size
    
    * Update pom.xml
    
    Co-authored-by: Eric Wittmann <eric.wittmann@gmail.com>

--- common/src/main/java/io/apicurio/registry/utils/CheckPeriodCache.java
+++ common/src/main/java/io/apicurio/registry/utils/CheckPeriodCache.java
@@ -16,34 +16,89 @@
 
 package io.apicurio.registry.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * @author Fabian Martinez
+ * @author Jakub Senko <m@jsenko.net>
  */
 public class CheckPeriodCache<K, V> {
 
-    private Map<K, CheckValue<V>> cache = new ConcurrentHashMap<>();
-    private long checkPeriodMillis = 0;
+    private static final Logger log = LoggerFactory.getLogger(CheckPeriodCache.class);
 
-    public CheckPeriodCache(long checkPeriodMillis) {
-        this.checkPeriodMillis = checkPeriodMillis;
+    private final ConcurrentHashMap<K, CheckValue<V>> cache = new ConcurrentHashMap<>();
+
+    private final Duration checkPeriod;
+
+    private final AtomicBoolean evicting = new AtomicBoolean(false);
+    private final int evictionThreshold;
+
+    public CheckPeriodCache(Duration checkPeriod, int evictionThreshold) {
+        this.checkPeriod = checkPeriod;
+        this.evictionThreshold = evictionThreshold;
+    }
+
+    public CheckPeriodCache(Duration checkPeriod) {
+        this.checkPeriod = checkPeriod;
+        this.evictionThreshold = 1000;
+    }
+
+    private boolean isExpired(CheckValue<?> checkedValue) {
+        return Instant.now().isAfter(checkedValue.lastUpdate.plus(checkPeriod));
+    }
+
+    private void checkEviction() {
+        final int currentSize = cache.size();
+        if (currentSize > evictionThreshold) {
+            if (evicting.compareAndSet(false, true)) {
+                try {
+                    // This thread gets to evict
+                    log.debug("Thread {} is evicting the cache. Current size is {}, threshold is {}.",
+                            Thread.currentThread().getName(), currentSize, evictionThreshold);
+                    var toEvict = cache.entrySet().stream()
+                            .filter(entry -> isExpired(entry.getValue()))
+                            .map(Entry::getKey)
+                            .collect(Collectors.toList());
+
+                    log.debug("Thread {} is evicting the cache. Found {} candidates for eviction.",
+                            Thread.currentThread().getName(), toEvict.size());
+                    toEvict.forEach(k -> {
+                        cache.compute(k, (key, value) -> {
+                            if (value == null || isExpired(value)) {
+                                return null;
+                            } else {
+                                return value;
+                            }
+                        });
+                    });
+
+                    log.debug("Thread {} has finished evicting the cache. The new size is {}.",
+                            Thread.currentThread().getName(), cache.size());
+                } finally {
+                    evicting.set(false);
+                }
+            }
+        }
     }
 
     public V compute(K k, Function<K, V> remappingFunction) {
+        checkEviction();
         CheckValue<V> returnValue = cache.compute(k, (key, checkedValue) -> {
-            long now = System.currentTimeMillis();
-            if (checkedValue == null) {
+            if (checkedValue == null || isExpired(checkedValue)) {
+                // Only execute if expired, but do it first in case an exception is thrown
                 V value = remappingFunction.apply(key);
-                return new CheckValue<>(now, value);
+                return new CheckValue<>(Instant.now(), value);
             } else {
-                if (checkedValue.lastUpdate + checkPeriodMillis < now) {
-                    V value = remappingFunction.apply(key);
-                    checkedValue.lastUpdate = now;
-                    checkedValue.value = value;
-                }
                 return checkedValue;
             }
         });
@@ -51,21 +106,16 @@ public class CheckPeriodCache<K, V> {
     }
 
     public void put(K k, V v) {
-        cache.put(k, new CheckValue<>(System.currentTimeMillis(), v));
+        checkEviction();
+        cache.put(k, new CheckValue<>(Instant.now(), v));
     }
 
     public V get(K k) {
         CheckValue<V> value = cache.compute(k, (key, checkedValue) -> {
-            if (checkedValue == null) {
+            if (checkedValue == null || isExpired(checkedValue)) {
                 return null;
             } else {
-                long now = System.currentTimeMillis();
-                if (checkedValue.lastUpdate + checkPeriodMillis < now) {
-                    //value expired
-                    return null;
-                } else {
-                    return checkedValue;
-                }
+                return checkedValue;
             }
         });
         return value == null ? null : value.value;
@@ -79,15 +129,25 @@ public class CheckPeriodCache<K, V> {
         cache.clear();
     }
 
-    private static class CheckValue<V> {
+    /**
+     * USE FOR TEST ONLY
+     */
+    Map<K, CheckValue<V>> getInternal() {
+        return cache;
+    }
 
-        CheckValue(long ts, V value) {
-            this.lastUpdate = ts;
+    int size() {
+        return cache.size();
+    }
+
+    static class CheckValue<V> {
+
+        CheckValue(Instant lastUpdate, V value) {
+            this.lastUpdate = lastUpdate;
             this.value = value;
         }
 
-        long lastUpdate;
+        Instant lastUpdate;
         V value;
     }
-
 }

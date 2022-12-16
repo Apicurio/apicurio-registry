92aef1058f9c092a56238bc769eaa62cfc99f3c7
Riccardo Forina <rforina@redhat.com>
2022-12-16 14:24:37 +0100

    fix: unset the singleton option for react-router-dom

--- ui/webpack.common.js
+++ ui/webpack.common.js
@@ -136,7 +136,7 @@ module.exports = (env, argv) => {
             requiredVersion: dependencies["react-dom"],
           },
           "react-router-dom": {
-            singleton: true,
+            singleton: false, // consoledot needs this to be off to be able to upgrade the router to v6. We don't need this to be a singleton, so let's keep this off
             requiredVersion: dependencies["react-router-dom"],
           },
         }

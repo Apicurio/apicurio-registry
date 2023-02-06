5812f5ec4fe6aa254b6ca2ca57a52b0e89454d8a
Eric Wittmann <eric.wittmann@gmail.com>
2023-02-06 08:01:48 -0500

    Upgraded to 2.0 of Apicurio Data Models (#3119)
    * Upgraded to 2.0 of Apicurio Data Models
    
    * Revert some unrelated changes
    
    * Update CustomAuthenticationMechanism.java

--- app/tools/extractRegisterForReflection.java
+++ app/tools/extractRegisterForReflection.java
@@ -1,6 +1,6 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //DEPS org.reflections:reflections:0.10.2
-//DEPS io.apicurio:apicurio-data-models:1.1.26
+//DEPS io.apicurio:apicurio-data-models:2.0.0-SNAPSHOT
 
 import java.util.Set;
 import org.reflections.Reflections;
@@ -12,8 +12,8 @@ public class extractRegisterForReflection {
     public static void main(String... args) {
         Reflections reflections = new Reflections("io.apicurio");
 
-        Set<Class<?>> subTypes = reflections.get(SubTypes.of(io.apicurio.datamodels.core.validation.ValidationRule.class).asClass());
-        subTypes.addAll(reflections.get(SubTypes.of(io.apicurio.datamodels.core.models.Node.class).asClass()));
+        Set<Class<?>> subTypes = reflections.get(SubTypes.of(io.apicurio.datamodels.validation.ValidationRule.class).asClass());
+        subTypes.addAll(reflections.get(SubTypes.of(io.apicurio.datamodels.models.Node.class).asClass()));
 
         subTypes
             .stream()

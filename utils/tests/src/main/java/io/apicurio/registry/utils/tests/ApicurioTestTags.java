e5745410b203f34488633981f2e40679b0403af7
Eric Wittmann <eric.wittmann@gmail.com>
2023-03-03 13:49:28 -0500

    Added missing test tag

--- utils/tests/src/main/java/io/apicurio/registry/utils/tests/ApicurioTestTags.java
+++ utils/tests/src/main/java/io/apicurio/registry/utils/tests/ApicurioTestTags.java
@@ -21,6 +21,9 @@ package io.apicurio.registry.utils.tests;
  */
 public class ApicurioTestTags {
 
+    /**Docker is required in the running machine to run this test. */
     public static final String DOCKER = "docker";
 
+    /**Test marked as slow. This usually means that this test uses a profile and therefore an application restart is required. */
+    public static final String SLOW = "slow";
 }

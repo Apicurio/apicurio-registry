57411ac70bf2a1ebc9bc2a83657c11898cff8faf
Carles Arnal <carlesarnal92@gmail.com>
2023-05-30 20:59:45 +0200

    Implement read authz roles from header (#3395)

---
+++ utils/tests/src/main/java/io/apicurio/registry/utils/tests/AuthTestProfileWithHeaderRoles.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2023 Red Hat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.apicurio.registry.utils.tests;
+
+import io.quarkus.test.junit.QuarkusTestProfile;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class AuthTestProfileWithHeaderRoles implements QuarkusTestProfile {
+
+    @Override
+    public Map<String, String> getConfigOverrides() {
+        return Map.of("smallrye.jwt.sign.key.location", "privateKey.jwk", "registry.auth.role-source", "header");
+    }
+
+    @Override
+    public List<QuarkusTestProfile.TestResourceEntry> testResources() {
+        return Collections.singletonList(
+                new QuarkusTestProfile.TestResourceEntry(JWKSMockServer.class));
+    }
+}

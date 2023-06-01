ced64f6858c93563a8e861a184c4a15cbd65ed3b
Carles Arnal <carlesarnal92@gmail.com>
2023-06-01 14:34:18 +0200

    Simplify kubernetes resources management

--- e2e-system-tests/src/main/java/io/apicurio/registry/deployment/RegistryDeploymentManager.java
+++ e2e-system-tests/src/main/java/io/apicurio/registry/deployment/RegistryDeploymentManager.java
@@ -16,106 +16,56 @@
 
 package io.apicurio.registry.deployment;
 
-import io.fabric8.kubernetes.api.model.HasMetadata;
-import io.fabric8.kubernetes.api.model.Namespace;
-import io.fabric8.kubernetes.api.model.NamespaceBuilder;
-import io.fabric8.kubernetes.api.model.Service;
-import io.fabric8.kubernetes.api.model.apps.Deployment;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
 import org.junit.jupiter.api.extension.AfterAllCallback;
-import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
-import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
-public class RegistryDeploymentManager implements BeforeEachCallback, AfterEachCallback, BeforeAllCallback, AfterAllCallback, ExtensionContext.Store.CloseableResource {
+public class RegistryDeploymentManager implements BeforeAllCallback, AfterAllCallback {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RegistryDeploymentManager.class);
 
-    private static final String KUBERNETES_IN_MEMORY_DEPLOYMENT = "in-memory-deployment.yml";
-    private static final String KUBERNETES_INGRESS = "ingress.yml";
-    private static final String KUBERNETES_SERVICE = "service.yml";
-
-    private static final String NAMESPACE = "apicurio-registry";
+    private static final String KUBERNETES_IN_MEMORY_DEPLOYMENT = "in-memory.yml";
+    private static final String IN_MEMORY_NAMESPACE = "apicurio-registry-e2e-in-memory";
 
     KubernetesClient kubernetesClient;
 
     @Override
     public void beforeAll(ExtensionContext extensionContext) throws Exception {
         if (System.getProperty("quarkus.http.test-host") != null) {
-
             kubernetesClient = new KubernetesClientBuilder()
                     .build();
 
             try {
-                Namespace namespace = new NamespaceBuilder().withNewMetadata().withName(NAMESPACE)
-                        .endMetadata()
-                        .build();
+                kubernetesClient.load(getClass().getResourceAsStream(KUBERNETES_IN_MEMORY_DEPLOYMENT))
+                        .create();
 
-                LOGGER.info("Created namespace: {}", kubernetesClient.namespaces().resource(namespace).createOrReplace());
-
-                Deployment deployment = kubernetesClient.apps()
-                        .deployments()
-                        .inNamespace(NAMESPACE)
-                        .load(getClass().getResource(KUBERNETES_IN_MEMORY_DEPLOYMENT))
-                        .createOrReplace();
-
-                Service service = kubernetesClient.services()
-                        .inNamespace(NAMESPACE)
-                        .load(getClass().getResource(KUBERNETES_SERVICE))
-                        .createOrReplace();
-
-                HasMetadata ingress = kubernetesClient.resource(getClass().getResourceAsStream(KUBERNETES_INGRESS))
-                        .inNamespace(NAMESPACE)
-                        .createOrReplace();
-
-                kubernetesClient.pods().inNamespace(NAMESPACE)
+                kubernetesClient.pods().inNamespace(IN_MEMORY_NAMESPACE)
                         .waitUntilReady(30, TimeUnit.SECONDS);
 
             } catch (Exception e) {
                 e.printStackTrace();
-                if (kubernetesClient != null) {
-                    kubernetesClient.namespaces()
-                            .withName(NAMESPACE)
-                            .delete();
-                }
             }
             LOGGER.info("Test suite started ##################################################");
         }
     }
 
     @Override
-    public void beforeEach(ExtensionContext extensionContext) throws Exception {
-        LOGGER.info("Individual Test started ##################################################");
-    }
-
-    @Override
     public void afterAll(ExtensionContext extensionContext) throws Exception {
         LOGGER.info("Test suite ended ##################################################");
+        LOGGER.info("Closing test resources ##################################################");
 
-        LOGGER.info("Closing test resources ##################################################3");
+        kubernetesClient.namespaces()
+                .withName(IN_MEMORY_NAMESPACE)
+                .delete();
 
         if (kubernetesClient != null) {
-
-            kubernetesClient.namespaces().withName(NAMESPACE).delete();
-
             kubernetesClient.close();
         }
-    }
-
-    @Override
-    public void afterEach(ExtensionContext extensionContext) throws Exception {
-        LOGGER.info("Individual Test executed ##################################################");
-
-    }
-
-    @Override
-    public void close() throws Throwable {
-
     }
 }

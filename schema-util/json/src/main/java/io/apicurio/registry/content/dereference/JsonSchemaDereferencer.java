c55471b10f429dd7a59dbe2bd8350058f1e58c85
Eric Wittmann <eric.wittmann@gmail.com>
2023-06-30 10:02:12 -0400

    Update REST API to support different artifact reference strategies on GET (#3462)
    * Changing "derefence" support to support both deref and ref rewriting
    
    * Add tests for ref writing.  Fix failing build.

--- schema-util/json/src/main/java/io/apicurio/registry/content/dereference/JsonSchemaDereferencer.java
+++ schema-util/json/src/main/java/io/apicurio/registry/content/dereference/JsonSchemaDereferencer.java
@@ -16,20 +16,24 @@
 
 package io.apicurio.registry.content.dereference;
 
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsonorg.JsonOrgModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+
 import io.apicurio.registry.content.ContentHandle;
 import io.apicurio.registry.rules.compatibility.jsonschema.JsonUtil;
-
-import java.io.IOException;
-import java.util.Map;
-
-import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
 
 /**
  * @author carnalca@redhat.com
@@ -56,6 +60,44 @@ public class JsonSchemaDereferencer implements ContentDereferencer {
             return ContentHandle.create(JsonUtil.readSchema(content.content(), resolvedReferences).toString());
         } catch (IOException e) {
             throw new IllegalArgumentException("Invalid JSON " + content.content(), e);
+        }
+    }
+    
+    /**
+     * @see io.apicurio.registry.content.dereference.ContentDereferencer#rewriteReferences(io.apicurio.registry.content.ContentHandle, java.util.Map)
+     */
+    @Override
+    public ContentHandle rewriteReferences(ContentHandle content, Map<String, String> resolvedReferenceUrls) {
+        try {
+            JsonNode tree = objectMapper.readTree(content.content());
+            rewriteIn(tree, resolvedReferenceUrls);
+            String converted = objectMapper.writeValueAsString(objectMapper.treeToValue(tree, Object.class));
+            return ContentHandle.create(converted);
+        } catch (Exception e) {
+            return content;
+        }
+    }
+
+    private void rewriteIn(JsonNode node, Map<String, String> resolvedReferenceUrls) {
+        if (node.isObject()) {
+            rewriteInObject((ObjectNode) node, resolvedReferenceUrls);
+        }
+    }
+
+    private void rewriteInObject(ObjectNode node, Map<String, String> resolvedReferenceUrls) {
+        if (node.hasNonNull("$ref")) {
+            String $ref = node.get("$ref").asText();
+            if (resolvedReferenceUrls.containsKey($ref)) {
+                node.put("$ref", resolvedReferenceUrls.get($ref));
+            }
+        }
+        Iterator<String> fieldNames = node.fieldNames();
+        while (fieldNames.hasNext()) {
+            String fieldName = fieldNames.next();
+            JsonNode fieldValue = node.get(fieldName);
+            if (fieldValue.isObject()) {
+                rewriteInObject((ObjectNode) fieldValue, resolvedReferenceUrls);
+            }
         }
     }
 }

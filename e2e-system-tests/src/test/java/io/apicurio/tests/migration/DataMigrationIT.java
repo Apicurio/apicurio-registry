a68b741077d4c581276ece348b22d70433daa30b
Carles Arnal <carlesarnal92@gmail.com>
2023-07-17 12:37:06 +0200

    Add data migration tests

--- e2e-system-tests/src/test/java/io/apicurio/tests/migration/DataMigrationIT.java
+++ e2e-system-tests/src/test/java/io/apicurio/tests/migration/DataMigrationIT.java
@@ -1,53 +1,111 @@
-/*
- * Copyright 2021 Red Hat
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package io.apicurio.tests.migration;
 
+import io.apicurio.registry.rest.client.RegistryClient;
+import io.apicurio.registry.rest.client.RegistryClientFactory;
+import io.apicurio.registry.rest.v2.beans.ArtifactReference;
+import io.apicurio.registry.types.RuleType;
+import io.apicurio.registry.utils.tests.TestUtils;
 import io.apicurio.tests.ApicurioRegistryBaseIT;
+import io.apicurio.tests.dbupgrade.MigrationTestsDataInitializer;
+import io.apicurio.tests.utils.AbstractTestDataInitializer;
 import io.apicurio.tests.utils.Constants;
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusIntegrationTest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.GenericContainer;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static io.apicurio.tests.dbupgrade.MigrationTestsDataInitializer.matchesReferences;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
- * @author Fabian Martinez
+ * @author Carles Arnal
  */
+@QuarkusIntegrationTest
+@QuarkusTestResource(value = DataMigrationIT.MigrateTestInitializer.class, restrictToAnnotatedClass = true)
 @Tag(Constants.MIGRATION)
 public class DataMigrationIT extends ApicurioRegistryBaseIT {
 
+    private static final Logger log = LoggerFactory.getLogger(DataMigrationIT.class);
 
+    public static InputStream migrateDataToImport;
+    public static HashMap<Long, List<ArtifactReference>> migrateReferencesMap = new HashMap<>();
+    public static List<Long> migrateGlobalIds = new ArrayList<>();
+    public static Map<String, String> doNotPreserveIdsImportArtifacts = new HashMap<>();
+
+    /**
+     * The data required for this test is initialized by MigrationTestsDataInitializer.initializeMigrateTest(RegistryClient)
+     *
+     * @throws Exception
+     */
     @Test
     public void migrate() throws Exception {
+        RegistryClient dest = RegistryClientFactory.create(ApicurioRegistryBaseIT.getRegistryV2ApiUrl());
+        dest.importData(migrateDataToImport);
 
-    }
-
-    @Test
-    public void testDoNotPreserveIdsImport() throws Exception {
-
+        retry(() -> {
+            for (long gid : migrateGlobalIds) {
+                dest.getContentByGlobalId(gid);
+                if (migrateReferencesMap.containsKey(gid)) {
+                    List<ArtifactReference> srcReferences = migrateReferencesMap.get(gid);
+                    List<ArtifactReference> destReferences = dest.getArtifactReferencesByGlobalId(gid);
+                    assertTrue(matchesReferences(srcReferences, destReferences));
+                }
+            }
+            assertEquals("SYNTAX_ONLY", dest.getArtifactRuleConfig("migrateTest", "avro-0", RuleType.VALIDITY).getConfig());
+            assertEquals("BACKWARD", dest.getGlobalRuleConfig(RuleType.COMPATIBILITY).getConfig());
+        });
     }
 
     @Test
     public void testGeneratingCanonicalHashOnImport() throws Exception {
+
     }
 
     @AfterEach
     public void tearDownRegistries() throws IOException {
     }
 
+    public static class MigrateTestInitializer extends AbstractTestDataInitializer {
 
+        GenericContainer registryContainer;
+
+
+        @Override
+        public Map<String, String> start() {
+
+            String registryBaseUrl = startRegistryApplication("quay.io/apicurio/apicurio-registry-mem:latest-release");
+            RegistryClient source = RegistryClientFactory.create(registryBaseUrl);
+
+            migrateGlobalIds = new ArrayList<>();
+            migrateReferencesMap = new HashMap<>();
+
+            try {
+
+                //Warm up until the source registry is ready.
+                TestUtils.retry(() -> {
+                    source.listArtifactsInGroup(null);
+                });
+
+                MigrationTestsDataInitializer.initializeMigrateTest(source);
+
+            } catch (Exception ex) {
+                log.error("Error filling origin registry with data:", ex);
+            }
+
+            return Collections.emptyMap();
+        }
+    }
 }

64080bc01aa189d61727a73488c041f02b391197
Jakub Senko <m@jsenko.net>
2023-08-02 15:54:22 +0200

    chore: keep apicurio-client-gen dist file in the repo

--- ui/webpack.common.js
+++ ui/webpack.common.js
@@ -27,6 +27,7 @@ process.argv.forEach(arg => {
 
 module.exports = (env, argv) => {
   const isProduction = argv && argv.mode === "production";
+  const clientGenVersion = "0.1.0" // Use resources/download-client-gen-dist.sh to update
   console.info("Is production build? %o", isProduction);
   return {
     entry: {
@@ -91,8 +92,11 @@ module.exports = (env, argv) => {
       new WebpackShellPluginNext({
         onBuildStart:{
           scripts: [
+            'echo Deleting client-gen/dist...',
             'rm -rf client-gen/dist',
-            'curl -sL https://github.com/Apicurio/apicurio-client-gen/releases/download/0.1.0/dist.tar.gz | tar -xz -C client-gen'
+            'echo Unzipping client-gen-' + clientGenVersion + '.zip...',
+            'unzip -q resources/client-gen-' + clientGenVersion + '.zip -d client-gen',
+            'echo Done!'
           ],
           blocking: true,
           parallel: false

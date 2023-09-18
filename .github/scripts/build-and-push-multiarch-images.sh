998f0a10bacd3f775e7e39e7025d90b3fb405f9f
Jakub Senko <m@jsenko.net>
2023-09-18 13:42:31 +0200

    fix: update image variant check

--- .github/scripts/build-and-push-multiarch-images.sh
+++ .github/scripts/build-and-push-multiarch-images.sh
@@ -13,9 +13,14 @@ RELEASE_TYPE="$4"      # Either 'snapshot' or 'release' or 'pre-release'
 RELEASE_VERSION=${5:-$defvalue}   # Release version (Pass the release version if you also want images tagged with the release version)
 
 # Check if variant type is valid
-if [[ ($VARIANT != "mem-multiarch-images") &&  ($VARIANT != "sql-multiarch-images") &&  ($VARIANT != "kafkasql-multiarch-images") && ($VARIANT != "multiarch-registry-images") ]]
+if [[ ($VARIANT != "mem-multiarch-images") && \
+      ($VARIANT != "sql-multiarch-images") && \
+      ($VARIANT != "kafkasql-multiarch-images") && \
+      ($VARIANT != "gitops-multiarch-images") && \
+      ($VARIANT != "multiarch-registry-images") ]]
 then
-    echo "ERROR: Illegal value '${VARIANT}' for variable '$VARIANT'. Values can only be [mem-multiarch-images, sql-multiarch-images, kafkasql-multiarch-images, multiarch-registry-images]"
+    echo -n "ERROR: Illegal value '${VARIANT}' for variable '$VARIANT'. Values can only be "
+    echo    "[mem-multiarch-images, sql-multiarch-images, kafkasql-multiarch-images, gitops-multiarch-images, multiarch-registry-images]"
     exit 1
 fi
 

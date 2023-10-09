f01e4000419505e98694518963904e130742cc38
Eric Wittmann <eric.wittmann@gmail.com>
2023-10-09 08:55:20 -0400

    Fixed a package decl

--- app/src/test/java/io/apicurio/registry/noprofile/ccompat/rest/v7/UpdateState.java
+++ app/src/test/java/io/apicurio/registry/noprofile/ccompat/rest/v7/UpdateState.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.apicurio.registry.noprofile.ccompat.rest.v6;
+package io.apicurio.registry.noprofile.ccompat.rest.v7;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;

96708c4e0f424a582345357c4f78e77b51da501d
sunmeplz <evgeny.potapov@gmail.com>
2023-10-26 14:16:17 +0600

    #3862 adds ReflectAllowNullAvroDatumProvider (#3863)
    * #3862 adds ReflectAllowNullAvroDatumProvider
    
    * #3862 revert reformat
    
    ---------
    
    Co-authored-by: Carles Arnal <carlesarnal92@gmail.com>

--- serdes/avro-serde/src/main/java/io/apicurio/registry/serde/avro/ReflectAvroDatumProvider.java
+++ serdes/avro-serde/src/main/java/io/apicurio/registry/serde/avro/ReflectAvroDatumProvider.java
@@ -19,6 +19,7 @@ package io.apicurio.registry.serde.avro;
 import org.apache.avro.Schema;
 import org.apache.avro.io.DatumReader;
 import org.apache.avro.io.DatumWriter;
+import org.apache.avro.reflect.ReflectData;
 import org.apache.avro.reflect.ReflectDatumReader;
 import org.apache.avro.reflect.ReflectDatumWriter;
 
@@ -28,30 +29,41 @@ import org.apache.avro.reflect.ReflectDatumWriter;
 public class ReflectAvroDatumProvider<T> implements AvroDatumProvider<T> {
 
     private Schema readerSchema;
+    private final ReflectData reflectData;
 
     public ReflectAvroDatumProvider() {
+        this(ReflectData.get());
+    }
+
+    public ReflectAvroDatumProvider(ReflectData reflectData) {
+        this.reflectData = reflectData;
     }
 
     public ReflectAvroDatumProvider(Class<T> clazz) {
-        this.readerSchema = AvroSchemaUtils.getReflectSchema(clazz);
+        this(ReflectData.get(),clazz);
+    }
+
+    public ReflectAvroDatumProvider(ReflectData reflectData,Class<T> clazz) {
+        this(reflectData);
+        this.readerSchema = AvroSchemaUtils.getReflectSchema(reflectData,clazz);
     }
 
     @Override
     public DatumWriter<T> createDatumWriter(T data, Schema schema) {
-        return new ReflectDatumWriter<>(schema);
+        return new ReflectDatumWriter<>(schema,reflectData);
     }
 
     @Override
     public DatumReader<T> createDatumReader(Schema schema) {
         if (readerSchema == null) {
-            return new ReflectDatumReader<>(schema);
+            return new ReflectDatumReader<>(schema,schema,reflectData);
         } else {
-            return new ReflectDatumReader<>(schema, readerSchema);
+            return new ReflectDatumReader<>(schema, readerSchema,reflectData);
         }
     }
 
     @Override
     public Schema toSchema(T data) {
-        return AvroSchemaUtils.getReflectSchema(data);
+        return AvroSchemaUtils.getReflectSchema(reflectData,data);
     }
 }

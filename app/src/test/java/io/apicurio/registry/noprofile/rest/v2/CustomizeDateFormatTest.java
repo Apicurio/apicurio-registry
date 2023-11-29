ea6c31ecb884d465640902376eff59b2d73347f4
Carles Arnal <carlesarnal92@gmail.com>
2023-11-29 16:50:17 +0100

    Fix date time customizer

--- app/src/test/java/io/apicurio/registry/noprofile/rest/v2/CustomizeDateFormatTest.java
+++ app/src/test/java/io/apicurio/registry/noprofile/rest/v2/CustomizeDateFormatTest.java
@@ -1,12 +1,9 @@
-package io.apicurio.registry.rest;
+package io.apicurio.registry.noprofile.rest.v2;
 
 
 import io.apicurio.registry.AbstractResourceTestBase;
 import io.apicurio.registry.types.ArtifactType;
 import io.quarkus.test.junit.QuarkusTest;
-import io.quarkus.test.junit.TestProfile;
-import jakarta.inject.Inject;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
 import org.junit.jupiter.api.BeforeAll;
@@ -14,34 +11,25 @@ import org.junit.jupiter.api.Test;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.TimeZone;
 
 import static io.restassured.RestAssured.given;
 
 @QuarkusTest
-@TestProfile(CustomizeDateFormatTestProfile.class)
 public class CustomizeDateFormatTest extends AbstractResourceTestBase {
 
     private static final String GROUP = "CustomizeDateFormatTest";
 
-    @Inject
-    @ConfigProperty(name = "registry.apis.v2.date-format")
-    String dateFormat;
-    @Inject
-    @ConfigProperty(name = "registry.apis.v2.date-format-timezone")
-    String timezone;
+    private static final String dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'";
 
-    SimpleDateFormat newPattern;
+    SimpleDateFormat compliantPattern;
 
     @BeforeAll
     public void init() {
-        newPattern = new SimpleDateFormat(dateFormat);
-        newPattern.setTimeZone(TimeZone.getTimeZone(timezone));
+        compliantPattern = new SimpleDateFormat(dateFormat);
     }
 
     @Test
     public void testOpenApiCompliantDateFormat() throws Exception {
-
 
         String artifactContent = resourceToString("openapi-empty.json");
 
@@ -66,7 +54,7 @@ public class CustomizeDateFormatTest extends AbstractResourceTestBase {
                     @Override
                     public boolean matches(Object o) {
                         try {
-                            newPattern.parse(o.toString());
+                            compliantPattern.parse(o.toString());
                             return true;
                         } catch (ParseException e) {
                             throw new RuntimeException(e);

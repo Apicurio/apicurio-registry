b60bacefffeffc2e166488b02fdf0e3bc7db58e5
renjingxiao <renjingwsjy@gmail.com>
2023-12-04 13:20:19 +0000

    copyright cleanup

--- system-tests/src/main/java/io/apicurio/registry/systemtests/executor/ExecutionResultData.java
+++ system-tests/src/main/java/io/apicurio/registry/systemtests/executor/ExecutionResultData.java
@@ -1,7 +1,3 @@
-/*
- * Copyright 2018, EnMasse authors.
- * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
- */
 package io.apicurio.registry.systemtests.executor;
 
 public class ExecutionResultData {

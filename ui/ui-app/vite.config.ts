8abd76e7565458065840987c78b0649e2610b4d0
Eric Wittmann <eric.wittmann@gmail.com>
2023-12-08 13:05:03 -0500

    Upgrade patternfly (#4106)

--- ui/ui-app/vite.config.ts
+++ ui/ui-app/vite.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
+const PORT: number = parseInt(process.env.SERVER_PORT || "8888");
+
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
     server: {
-        port: 8888
+        port: PORT
     }
     // define: {
     //     "process.platform": {}

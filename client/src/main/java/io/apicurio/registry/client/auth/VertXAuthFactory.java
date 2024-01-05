2f7b4534859937d8b9d6c3a7ee12cb27b6b0f83d
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2024-01-05 17:49:50 +0000

    auth debugging

--- client/src/main/java/io/apicurio/registry/client/auth/VertXAuthFactory.java
+++ client/src/main/java/io/apicurio/registry/client/auth/VertXAuthFactory.java
@@ -44,6 +44,25 @@ public class VertXAuthFactory {
                 OAuth2WebClient.create(WebClient.create(vertx), oAuth2Auth)
                         .withCredentials(oauth2Credentials);
 
+        // Carles: This doesn't look correct the performed request look like:
+//        {
+//            "method" : "POST",
+//                "path" : "/",
+//                "headers" : {
+//            "host" : [ "localhost:1080" ],
+//            "content-length" : [ "29" ],
+//            "Content-Type" : [ "application/x-www-form-urlencoded" ],
+//            "Authorization" : [ "Basic YWRtaW4tY2xpZW50OnRlc3Qx" ],
+//            "Accept" : [ "application/json,application/x-www-form-urlencoded;q=0.9" ]
+//        },
+//            "keepAlive" : true,
+//                "secure" : false,
+//                "protocol" : "HTTP_1_1",
+//                "localAddress" : "dc83711c2da5/172.17.0.2:1080",
+//                "remoteAddress" : "172.17.0.1:44646",
+//                "body" : "grant_type=client_credentials"
+//        }
+
         return oAuth2WebClient;
     }
 

c55bf9af6dcd08fb9baa7f45754c427578a6e144
Carles Arnal <carlesarnal92@gmail.com>
2024-01-22 16:31:14 +0100

    Revert some dependencie updates

--- app/src/test/java/io/apicurio/registry/storage/impl/sql/MssqlStorageTest.java
+++ app/src/test/java/io/apicurio/registry/storage/impl/sql/MssqlStorageTest.java
@@ -6,10 +6,13 @@ import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.TestProfile;
 import jakarta.enterprise.inject.Typed;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 @QuarkusTest
 @Tag(ApicurioTestTags.SLOW)
 @TestProfile(MssqlTestProfile.class)
 @Typed(MssqlStorageTest.class)
+@DisabledOnOs(OS.MAC)
 public class MssqlStorageTest extends DefaultRegistryStorageTest {
 }

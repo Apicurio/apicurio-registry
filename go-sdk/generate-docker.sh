41d2b22ed900f147ef80b0141e1816bd4efb1e18
Eric Wittmann <eric.wittmann@gmail.com>
2024-02-08 11:46:41 -0500

    Fix go-sdk generation so output files are local user instead of root owned (#4307)

--- go-sdk/generate-docker.sh
+++ go-sdk/generate-docker.sh
@@ -8,6 +8,11 @@ then
   exit 1
 fi
 
+
+UID=`id -u`
+GID=`id -g`
+
+
 read -r -d '' SCRIPT <<- END
 apt-get update &&
 apt-get -y install unzip &&
@@ -15,7 +20,11 @@ apt-get -y install libicu-dev &&
 chmod u+rw /registry &&
 cd /registry/go-sdk &&
 make generate &&
-make format
+make format &&
+groupadd -g $GID -o localuser &&
+useradd -m -u $UID -g $GID -o -s /bin/bash localuser &&
+chown -R $UID /registry/go-sdk &&
+chgrp -R $GID /registry/go-sdk
 END
 
 docker run --rm -v $SCRIPT_DIR/..:/registry golang:1.21.6 /bin/bash -c "$SCRIPT"

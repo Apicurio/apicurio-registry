3bb2945fbaba870da51712dcb3805200922c585b
Carles Arnal <carlesarnal92@gmail.com>
2024-03-28 14:33:08 +0100

    Make integration tests publish multi arch images

--- utils/kafka/src/test/java/io/apicurio/registry/test/utils/KafkaTestContainerManager.java
+++ utils/kafka/src/test/java/io/apicurio/registry/test/utils/KafkaTestContainerManager.java
@@ -3,9 +3,11 @@ package io.apicurio.registry.test.utils;
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.Network;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.redpanda.RedpandaContainer;
 
+import java.util.Collections;
 import java.util.Map;
 
 public class KafkaTestContainerManager implements QuarkusTestResourceLifecycleManager {
@@ -15,27 +17,38 @@ public class KafkaTestContainerManager implements QuarkusTestResourceLifecycleMa
 
     @Override
     public Map<String, String> start() {
-        log.info("Starting the Kafka Test Container");
-        kafka = new RedpandaContainer("docker.redpanda.com/vectorized/redpanda");
+        if (!Boolean.parseBoolean(System.getProperty("cluster.tests"))) {
 
-        kafka.addEnv("KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR", "1");
-        kafka.addEnv("KAFKA_TRANSACTION_STATE_LOG_MIN_ISR", "1");
-        kafka.withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger("kafka-testcontainer")));
-        kafka.start();
+            log.info("Starting the Kafka Test Container");
+            kafka = new RedpandaContainer("docker.redpanda.com/vectorized/redpanda");
 
-        String bootstrapServers = kafka.getBootstrapServers();
+            kafka.addEnv("KAFKA_TRANSACTION_STATE_LOG_REPLICATION_FACTOR", "1");
+            kafka.addEnv("KAFKA_TRANSACTION_STATE_LOG_MIN_ISR", "1");
+            kafka.withListener(() -> "redpanda:19092");
+            kafka.withNetwork(Network.SHARED);
+            kafka.withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger("kafka-testcontainer")));
+            kafka.start();
 
-        System.setProperty("bootstrap.servers", bootstrapServers);
+            String externalBootstrapServers = kafka.getBootstrapServers();
 
-        return Map.of(
-                "bootstrap.servers", bootstrapServers,
-                "registry.events.kafka.config.bootstrap.servers", bootstrapServers,
-                "registry.kafkasql.bootstrap.servers", bootstrapServers);
+            String bootstrapServers = "redpanda:19092";
+            System.setProperty("bootstrap.servers.internal", bootstrapServers);
+            System.setProperty("bootstrap.servers.external", externalBootstrapServers);
+
+            return Map.of(
+                    "bootstrap.servers", externalBootstrapServers,
+                    "registry.events.kafka.config.bootstrap.servers", externalBootstrapServers,
+                    "registry.kafkasql.bootstrap.servers", externalBootstrapServers);
+        } else {
+            return Collections.emptyMap();
+        }
     }
 
     @Override
     public void stop() {
-        log.info("Stopping the Kafka Test Container");
-        kafka.stop();
+        if (kafka != null) {
+            log.info("Stopping the Kafka Test Container");
+            kafka.stop();
+        }
     }
 }

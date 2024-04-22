5e4ead19b3e9f53e693174b0153b3ee88c915352
Eric Wittmann <eric.wittmann@gmail.com>
2024-04-22 16:49:41 -0400

    Fix artifact util

--- integration-tests/src/test/java/io/apicurio/tests/utils/ArtifactUtils.java
+++ integration-tests/src/test/java/io/apicurio/tests/utils/ArtifactUtils.java
@@ -12,7 +12,7 @@ public class ArtifactUtils {
 
     public static Response getArtifact(String groupId, String artifactId, String version, int returnCode) {
         return
-            BaseHttpUtils.getRequest(RestConstants.JSON, ApicurioRegistryBaseIT.getRegistryV3ApiUrl() + "/groups/" + encodeURIComponent(groupId) + "/artifacts/" + encodeURIComponent(artifactId) + "/versions/" + version, returnCode);
+            BaseHttpUtils.getRequest(RestConstants.JSON, ApicurioRegistryBaseIT.getRegistryV3ApiUrl() + "/groups/" + encodeURIComponent(groupId) + "/artifacts/" + encodeURIComponent(artifactId) + "/versions/" + version + "/content", returnCode);
     }
 
     public static Response createArtifact(String groupId, String artifactId, String artifact, int returnCode) {

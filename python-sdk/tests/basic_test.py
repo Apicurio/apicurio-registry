665adb8a06778d0d6a096b3482dc4721a4872aa7
Eric Wittmann <eric.wittmann@gmail.com>
2024-04-23 08:08:32 -0400

    Fix python sdk basic test

--- python-sdk/tests/basic_test.py
+++ python-sdk/tests/basic_test.py
@@ -94,7 +94,7 @@ async def test_basic_upload_download():
         await client.groups.by_group_id("default")
         .artifacts.by_artifact_id(meta_data.artifact_id)
         .versions.by_version_expression("branch=latest")
-        .content()
+        .content
         .get()
     )
     print(str(return_artifact, "utf-8"))

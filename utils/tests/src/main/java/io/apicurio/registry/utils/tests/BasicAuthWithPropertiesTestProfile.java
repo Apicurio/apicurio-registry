d0f9cc0018be96d42247aa9e10392e5892ea2831
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2024-05-02 17:58:59 +0100

    tests

---
+++ utils/tests/src/main/java/io/apicurio/registry/utils/tests/BasicAuthWithPropertiesTestProfile.java
@@ -0,0 +1,40 @@
+package io.apicurio.registry.utils.tests;
+
+import io.quarkus.test.junit.QuarkusTestProfile;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BasicAuthWithPropertiesTestProfile implements QuarkusTestProfile {
+
+    @Override
+    public Map<String, String> getConfigOverrides() {
+        Map<String, String> map = new HashMap<>();
+        map.put("quarkus.oidc.tenant-enabled", "false");
+        map.put("quarkus.http.auth.basic", "true");
+        map.put("apicurio.auth.role-based-authorization", "true");
+        map.put("quarkus.security.users.embedded.enabled", "true");
+        map.put("quarkus.security.users.embedded.plain-text", "true");
+        map.put("quarkus.security.users.embedded.users.alice", "alice");
+        map.put("quarkus.security.users.embedded.users.bob1", "bob1");
+        map.put("quarkus.security.users.embedded.users.bob2", "bob2");
+        map.put("quarkus.security.users.embedded.users.duncan", "duncan");
+        map.put("quarkus.security.users.embedded.roles.alice", "sr-admin");
+        map.put("quarkus.security.users.embedded.roles.bob1", "sr-developer");
+        map.put("quarkus.security.users.embedded.roles.bob2", "sr-developer");
+        map.put("quarkus.security.users.embedded.roles.duncan", "sr-readonly");
+        return map;
+    }
+
+    @Override
+    public List<TestResourceEntry> testResources() {
+        if (!Boolean.parseBoolean(System.getProperty("cluster.tests"))) {
+            return List.of(
+                    new TestResourceEntry(JWKSMockServer.class));
+        } else {
+            return Collections.emptyList();
+        }
+    }
+}

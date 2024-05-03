76605eb3d64fce1e4e24b0e5ef52f0713a3c84e8
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2024-05-03 16:12:51 +0100

    cleanup

--- docs/generateAllConfigPartial.java
+++ docs/generateAllConfigPartial.java
@@ -216,8 +216,7 @@ public class generateAllConfigPartial {
 
         // TODO: include all the relevant jars, to be determined
         // Extract configuration from Jandex
-        // TODO: re-include-me
-        // extractConfigurations(baseDir + "/../app/target/lib/io.apicurio.apicurio-common-app-components-auth-" + commonComponentsVersion + ".jar", allConfiguration);
+        extractConfigurations(baseDir + "/../app/target/lib/io.apicurio.apicurio-common-app-components-auth-" + commonComponentsVersion + ".jar", allConfiguration);
         extractConfigurations(baseDir + "/../app/target/apicurio-registry-app-" + currentVersion + ".jar", allConfiguration);
 
         // TODO

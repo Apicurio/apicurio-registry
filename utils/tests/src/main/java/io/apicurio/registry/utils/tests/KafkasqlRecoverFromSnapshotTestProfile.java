2c62f7e521bd0ddf3700e8d8a7fc8fcd58b7eeda
Carles Arnal <carlesarnal92@gmail.com>
2024-05-22 14:27:58 +0200

    Randomize db test url

--- utils/tests/src/main/java/io/apicurio/registry/utils/tests/KafkasqlRecoverFromSnapshotTestProfile.java
+++ utils/tests/src/main/java/io/apicurio/registry/utils/tests/KafkasqlRecoverFromSnapshotTestProfile.java
@@ -5,12 +5,13 @@ import io.quarkus.test.junit.QuarkusTestProfile;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 public class KafkasqlRecoverFromSnapshotTestProfile implements QuarkusTestProfile {
 
     @Override
     public Map<String, String> getConfigOverrides() {
-        return Map.of("apicurio.storage.kind", "kafkasql");
+        return Map.of("apicurio.storage.kind", "kafkasql", "apicurio.datasource.url", "jdbc:h2:mem:" + UUID.randomUUID());
     }
 
     @Override

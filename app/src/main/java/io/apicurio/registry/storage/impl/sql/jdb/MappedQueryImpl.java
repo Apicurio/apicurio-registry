97692ec0b09a6f7bee145fd7cfd5c2f9bb60501d
Carles Arnal <carlesarnal92@gmail.com>
2024-07-25 18:14:17 +0200

    Close result set using try with resources (#4929)

--- app/src/main/java/io/apicurio/registry/storage/impl/sql/jdb/MappedQueryImpl.java
+++ app/src/main/java/io/apicurio/registry/storage/impl/sql/jdb/MappedQueryImpl.java
@@ -17,7 +17,6 @@ public class MappedQueryImpl<T> implements MappedQuery<T>, Closeable {
 
     final PreparedStatement statement;
     final RowMapper<T> mapper;
-    final ResultSet resultSet;
 
     /**
      * Constructor.
@@ -29,7 +28,6 @@ public class MappedQueryImpl<T> implements MappedQuery<T>, Closeable {
     public MappedQueryImpl(PreparedStatement statement, RowMapper<T> mapper) throws SQLException {
         this.statement = statement;
         this.mapper = mapper;
-        this.resultSet = statement.executeQuery();
     }
 
     /**
@@ -38,18 +36,21 @@ public class MappedQueryImpl<T> implements MappedQuery<T>, Closeable {
     @Override
     public T one() {
         T rval = null;
-        try {
-            if (this.resultSet.next()) {
+        try (ResultSet resultSet = statement.executeQuery()) {
+            if (resultSet.next()) {
                 rval = this.mapper.map(resultSet);
-                if (this.resultSet.next()) {
+                if (resultSet.next()) {
                     throw new RuntimeSqlException("SQL error: Expected only one result but got multiple.");
                 }
-            } else {
+            }
+            else {
                 throw new RuntimeSqlException("SQL error: Expected only one result row but got none.");
             }
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             throw new RuntimeSqlException(e);
-        } finally {
+        }
+        finally {
             close();
         }
         return rval;
@@ -61,15 +62,18 @@ public class MappedQueryImpl<T> implements MappedQuery<T>, Closeable {
     @Override
     public T first() {
         T rval = null;
-        try {
-            if (this.resultSet.next()) {
+        try (ResultSet resultSet = statement.executeQuery()) {
+            if (resultSet.next()) {
                 rval = this.mapper.map(resultSet);
-            } else {
+            }
+            else {
                 throw new RuntimeSqlException("SQL error: Expected AT LEAST one result row but got none.");
             }
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             throw new RuntimeSqlException(e);
-        } finally {
+        }
+        finally {
             close();
         }
         return rval;
@@ -81,18 +85,21 @@ public class MappedQueryImpl<T> implements MappedQuery<T>, Closeable {
     @Override
     public Optional<T> findOne() {
         Optional<T> rval;
-        try {
-            if (this.resultSet.next()) {
+        try (ResultSet resultSet = statement.executeQuery()) {
+            if (resultSet.next()) {
                 rval = Optional.of(this.mapper.map(resultSet));
-                if (this.resultSet.next()) {
+                if (resultSet.next()) {
                     throw new RuntimeSqlException("SQL error: Expected only one result but got multiple.");
                 }
-            } else {
+            }
+            else {
                 rval = Optional.empty();
             }
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             throw new RuntimeSqlException(e);
-        } finally {
+        }
+        finally {
             close();
         }
         return rval;
@@ -104,15 +111,18 @@ public class MappedQueryImpl<T> implements MappedQuery<T>, Closeable {
     @Override
     public Optional<T> findFirst() {
         Optional<T> rval = null;
-        try {
-            if (this.resultSet.next()) {
+        try (ResultSet resultSet = statement.executeQuery()) {
+            if (resultSet.next()) {
                 rval = Optional.of(this.mapper.map(resultSet));
-            } else {
+            }
+            else {
                 rval = Optional.empty();
             }
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             throw new RuntimeSqlException(e);
-        } finally {
+        }
+        finally {
             close();
         }
         return rval;
@@ -124,16 +134,18 @@ public class MappedQueryImpl<T> implements MappedQuery<T>, Closeable {
     @Override
     public Optional<T> findLast() {
         Optional<T> rval = null;
-        try {
-            while (this.resultSet.next()) {
+        try (ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
                 rval = Optional.of(this.mapper.map(resultSet));
             }
             if (rval == null) {
                 rval = Optional.empty();
             }
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             throw new RuntimeSqlException(e);
-        } finally {
+        }
+        finally {
             close();
         }
         return rval;
@@ -145,14 +157,16 @@ public class MappedQueryImpl<T> implements MappedQuery<T>, Closeable {
     @Override
     public List<T> list() {
         List<T> rval = new LinkedList<>();
-        try {
-            while (this.resultSet.next()) {
+        try (ResultSet resultSet = statement.executeQuery()) {
+            while (resultSet.next()) {
                 T t = this.mapper.map(resultSet);
                 rval.add(t);
             }
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             throw new RuntimeSqlException(e);
-        } finally {
+        }
+        finally {
             close();
         }
         return rval;
@@ -163,23 +177,38 @@ public class MappedQueryImpl<T> implements MappedQuery<T>, Closeable {
      */
     @Override
     public Stream<T> stream() {
-        return StreamSupport.stream(new Spliterators.AbstractSpliterator<T>(Long.MAX_VALUE,
-                Spliterator.IMMUTABLE | Spliterator.ORDERED | Spliterator.DISTINCT | Spliterator.NONNULL) {
-            @Override
-            public boolean tryAdvance(Consumer<? super T> action) {
-                try {
-                    if (!resultSet.next()) {
-                        return false;
-                    }
-                    T t = mapper.map(resultSet);
-                    action.accept(t);
-                    return true;
-                } catch (SQLException e) {
-                    throw new RuntimeSqlException(e);
-                }
-            }
+        try {
+            ResultSet resultSet = statement.executeQuery();
+            return StreamSupport.stream(
+                    new Spliterators.AbstractSpliterator<T>(Long.MAX_VALUE, Spliterator.IMMUTABLE | Spliterator.ORDERED | Spliterator.DISTINCT | Spliterator.NONNULL) {
+                        @Override
+                        public boolean tryAdvance(Consumer<? super T> action) {
+                            try {
+                                if (!resultSet.next()) {
+                                    return false;
+                                }
+                                T t = mapper.map(resultSet);
+                                action.accept(t);
+                                return true;
+                            }
+                            catch (SQLException e) {
+                                throw new RuntimeSqlException(e);
+                            }
+                        }
 
-        }, false).onClose(this::close);
+                    }, false).onClose(() -> {
+                try {
+                    resultSet.close();
+                    close();
+                }
+                catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+        catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
@@ -189,7 +218,8 @@ public class MappedQueryImpl<T> implements MappedQuery<T>, Closeable {
     public void close() {
         try {
             this.statement.close();
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             throw new RuntimeSqlException(e);
         }
     }

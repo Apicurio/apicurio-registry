89b0e256186ccf68572012bdfd6f7339a7377e1d
Eric Wittmann <eric.wittmann@gmail.com>
2024-08-08 14:58:57 -0400

    Fix a broken UI integration test

--- ui/tests/specs/settings.spec.ts
+++ ui/tests/specs/settings.spec.ts
@@ -12,7 +12,7 @@ test.beforeEach(async ({ page }) => {
 test("Settings - Filter", async ({ page }) => {
     await expect(page.getByTestId("settings-search-widget").locator("input")).toBeEmpty();
     expect(page.getByTestId("config-groups")).toBeDefined();
-    await expect(page.getByTestId("config-groups").locator(".configuration-property")).toHaveCount(8);
+    await expect(page.getByTestId("config-groups").locator(".configuration-property")).toHaveCount(10);
 
     await page.getByTestId("settings-search-widget").locator("input").fill("legacy");
     await expect(page.getByTestId("settings-search-widget").locator("input")).toHaveValue("legacy");

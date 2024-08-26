99c5193c858e2e19e75d68e7ba2c68f6f763fcf5
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2024-08-26 16:45:54 +0100

    lombok

--- operator/model/src/test/java/io/apicurio/registry/operator/api/v1/SerDeTest.java
+++ operator/model/src/test/java/io/apicurio/registry/operator/api/v1/SerDeTest.java
@@ -1,5 +1,6 @@
 package io.apicurio.registry.operator.api.v1;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.junit.jupiter.api.Test;
@@ -21,5 +22,22 @@ public class SerDeTest {
         // Assert
         assertEquals("test", registry.getMetadata().getName());
         assertEquals("test-namespace", registry.getMetadata().getNamespace());
+    }
+
+    @Test
+    public void basicReadOfMultipleCRs() throws Exception {
+        // Arrange
+        var resource = getClass().getResourceAsStream("/demo-crs.yaml");
+
+        // Act
+        var registries = MAPPER
+                .readValues(MAPPER.createParser(resource), new TypeReference<ApicurioRegistry3>() {
+                }).readAll();
+
+        // Assert
+        assertEquals("test1", registries.get(0).getMetadata().getName());
+        assertEquals("test-namespace1", registries.get(0).getMetadata().getNamespace());
+        assertEquals("test2", registries.get(1).getMetadata().getName());
+        assertEquals("test-namespace2", registries.get(1).getMetadata().getNamespace());
     }
 }

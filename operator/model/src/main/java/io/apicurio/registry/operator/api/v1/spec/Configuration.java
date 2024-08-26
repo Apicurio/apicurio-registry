3506285cae027176ba3d48ab51fc70bdf91cc554
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2024-08-26 16:32:24 +0100

    add lombok getters and setters

--- operator/model/src/main/java/io/apicurio/registry/operator/api/v1/spec/Configuration.java
+++ operator/model/src/main/java/io/apicurio/registry/operator/api/v1/spec/Configuration.java
@@ -9,12 +9,16 @@ import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import io.fabric8.kubernetes.api.model.EnvVar;
 import io.fabric8.kubernetes.api.model.KubernetesResource;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @JsonInclude(com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({ "env" })
 @JsonDeserialize(using = JsonDeserializer.None.class)
+@Getter
+@Setter
 public class Configuration implements KubernetesResource {
 
     /**
@@ -26,11 +30,4 @@ public class Configuration implements KubernetesResource {
     @JsonSetter(nulls = com.fasterxml.jackson.annotation.Nulls.SKIP)
     private List<EnvVar> env;
 
-    public List<EnvVar> getEnv() {
-        return env;
-    }
-
-    public void setEnv(List<EnvVar> env) {
-        this.env = env;
-    }
 }

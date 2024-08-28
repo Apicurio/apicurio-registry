d5506e4cb72cddd802126562dd9ffaec13e178d7
Carles Arnal <carlesarnal92@gmail.com>
2024-08-28 13:40:52 +0200

    Add test for json schema object and property level reference

--- schema-util/util-provider/src/test/java/io/apicurio/registry/content/dereference/JsonSchemaContentDereferencerTest.java
+++ schema-util/util-provider/src/test/java/io/apicurio/registry/content/dereference/JsonSchemaContentDereferencerTest.java
@@ -1,5 +1,6 @@
 package io.apicurio.registry.content.dereference;
 
+import io.apicurio.registry.content.ContentHandle;
 import io.apicurio.registry.content.TypedContent;
 import io.apicurio.registry.content.refs.ExternalReference;
 import io.apicurio.registry.content.refs.JsonPointerExternalReference;
@@ -9,6 +10,7 @@ import io.apicurio.registry.rules.validity.ArtifactUtilProviderTestBase;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -30,4 +32,37 @@ public class JsonSchemaContentDereferencerTest extends ArtifactUtilProviderTestB
                 .contains(new JsonPointerExternalReference("https://www.example.org/schemas/ssn.json")));
     }
 
+    @Test
+    public void testDereferenceObjectLevel() {
+        ContentHandle content = resourceToContentHandle("json-schema-to-deref-object-level.json");
+        JsonSchemaDereferencer dereferencer = new JsonSchemaDereferencer();
+        // Note: order is important.  The JSON schema dereferencer needs to convert the ContentHandle Map
+        // to a JSONSchema map.  So it *must* resolve the leaves of the dependency tree before the branches.
+        Map<String, ContentHandle> resolvedReferences = new LinkedHashMap<>();
+        resolvedReferences.put("types/city/qualification.json", resourceToContentHandle("types/city/qualification.json"));
+        resolvedReferences.put("city/qualification.json", resourceToContentHandle("types/city/qualification.json"));
+        resolvedReferences.put("identifier/qualification.json", resourceToContentHandle("types/identifier/qualification.json"));
+        resolvedReferences.put("types/all-types.json#/definitions/City", resourceToContentHandle("types/all-types.json"));
+        resolvedReferences.put("types/all-types.json#/definitions/Identifier", resourceToContentHandle("types/all-types.json"));
+        ContentHandle modifiedContent = dereferencer.dereference(content, resolvedReferences);
+        String expectedContent = resourceToString("expected-testDereference-object-level-json.json");
+        Assertions.assertEquals(normalizeMultiLineString(expectedContent), normalizeMultiLineString(modifiedContent.content()));
+    }
+
+    @Test
+    public void testDereferencePropertyLevel() {
+        ContentHandle content = resourceToContentHandle("json-schema-to-deref-property-level.json");
+        JsonSchemaDereferencer dereferencer = new JsonSchemaDereferencer();
+        // Note: order is important.  The JSON schema dereferencer needs to convert the ContentHandle Map
+        // to a JSONSchema map.  So it *must* resolve the leaves of the dependency tree before the branches.
+        Map<String, ContentHandle> resolvedReferences = new LinkedHashMap<>();
+        resolvedReferences.put("types/city/qualification.json", resourceToContentHandle("types/city/qualification.json"));
+        resolvedReferences.put("city/qualification.json", resourceToContentHandle("types/city/qualification.json"));
+        resolvedReferences.put("identifier/qualification.json", resourceToContentHandle("types/identifier/qualification.json"));
+        resolvedReferences.put("types/all-types.json#/definitions/City/properties/name", resourceToContentHandle("types/all-types.json"));
+        resolvedReferences.put("types/all-types.json#/definitions/Identifier", resourceToContentHandle("types/all-types.json"));
+        ContentHandle modifiedContent = dereferencer.dereference(content, resolvedReferences);
+        String expectedContent = resourceToString("expected-testDereference-property-level-json.json");
+        Assertions.assertEquals(normalizeMultiLineString(expectedContent), normalizeMultiLineString(modifiedContent.content()));
+    }
 }

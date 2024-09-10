c5acb15c02e335014fd75f8a55318ea2d5aeef7f
Simon Wagner <32711522+simonnepomuk@users.noreply.github.com>
2024-09-10 15:22:38 +0200

    Add OpenAPI compatibility checker (#5106)
    * add openapi compatibility checker
    
    * formatting
    
    * fix native build
    
    * formatting
    
    ---------
    
    Co-authored-by: simon.wagner <simon.wagner@rise-world.com>

--- schema-util/util-provider/src/main/java/io/apicurio/registry/types/provider/OpenApiArtifactTypeUtilProvider.java
+++ schema-util/util-provider/src/main/java/io/apicurio/registry/types/provider/OpenApiArtifactTypeUtilProvider.java
@@ -12,7 +12,7 @@ import io.apicurio.registry.content.refs.OpenApiReferenceFinder;
 import io.apicurio.registry.content.refs.ReferenceFinder;
 import io.apicurio.registry.content.util.ContentTypeUtil;
 import io.apicurio.registry.rules.compatibility.CompatibilityChecker;
-import io.apicurio.registry.rules.compatibility.NoopCompatibilityChecker;
+import io.apicurio.registry.rules.compatibility.OpenApiCompatibilityChecker;
 import io.apicurio.registry.rules.validity.ContentValidator;
 import io.apicurio.registry.rules.validity.OpenApiContentValidator;
 import io.apicurio.registry.types.ArtifactType;
@@ -49,7 +49,7 @@ public class OpenApiArtifactTypeUtilProvider extends AbstractArtifactTypeUtilPro
 
     @Override
     protected CompatibilityChecker createCompatibilityChecker() {
-        return NoopCompatibilityChecker.INSTANCE;
+        return new OpenApiCompatibilityChecker();
     }
 
     @Override

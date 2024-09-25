dbe2851488bde233c8cac6fa6db9b2e913e43cec
Eric Wittmann <eric.wittmann@gmail.com>
2024-09-25 13:39:13 -0400

    Update dateTime and timezone property names

--- common/src/main/java/io/apicurio/registry/rest/JacksonDateTimeCustomizer.java
+++ common/src/main/java/io/apicurio/registry/rest/JacksonDateTimeCustomizer.java
@@ -21,10 +21,10 @@ public class JacksonDateTimeCustomizer implements ObjectMapperCustomizer {
     private static final String DEFAULT_DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
     private static final String DEFAULT_DATE_TIME_FORMAT_TZ = "UTC";
 
-    @ConfigProperty(name = "apicurio.apis.v2.date-format", defaultValue = DEFAULT_DATE_TIME_FORMAT)
+    @ConfigProperty(name = "apicurio.apis.date-format", defaultValue = DEFAULT_DATE_TIME_FORMAT)
     @Info(category = "api", description = "API date format", availableSince = "2.4.3.Final")
     String dateFormat;
-    @ConfigProperty(name = "apicurio.apis.v2.date-format-timezone", defaultValue = DEFAULT_DATE_TIME_FORMAT_TZ)
+    @ConfigProperty(name = "apicurio.apis.date-format-timezone", defaultValue = DEFAULT_DATE_TIME_FORMAT_TZ)
     @Info(category = "api", description = "API date format (TZ)", availableSince = "2.4.3.Final")
     String timezone;
 

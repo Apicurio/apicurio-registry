6b8cd90f6b8a8707961006fe7a78cda59041b2fd
Carles Arnal <carlesarnal92@gmail.com>
2024-09-30 16:58:11 +0200

    Restore storage test

--- app/src/main/java/io/apicurio/registry/storage/impl/sql/AbstractSqlRegistryStorage.java
+++ app/src/main/java/io/apicurio/registry/storage/impl/sql/AbstractSqlRegistryStorage.java
@@ -1588,6 +1588,9 @@ public abstract class AbstractSqlRegistryStorage implements RegistryStorage {
                 case globalId:
                     orderByQuery.append(" ORDER BY v.globalId");
                     break;
+                case groupId:
+                    orderByQuery.append(" ORDER BY v.groupId");
+                    break;
                 case version:
                     orderByQuery.append(" ORDER BY v.version");
                     break;

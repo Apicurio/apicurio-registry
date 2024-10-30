1cb9fa18fa6964cca7688ffc4fa6fd9a1da214c9
Jakub Senko <m@jsenko.net>
2024-10-30 15:50:33 +0100

    feat(operator): KafkaSQL with TLS support

--- operator/controller/src/main/java/io/apicurio/registry/operator/feat/KafkaSql.java
+++ operator/controller/src/main/java/io/apicurio/registry/operator/feat/KafkaSql.java
@@ -6,19 +6,26 @@ import io.apicurio.registry.operator.api.v1.spec.AppSpec;
 import io.apicurio.registry.operator.api.v1.spec.StorageSpec;
 import io.fabric8.kubernetes.api.model.EnvVar;
 import io.fabric8.kubernetes.api.model.EnvVarBuilder;
+import io.fabric8.kubernetes.api.model.apps.Deployment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
+import static io.apicurio.registry.operator.api.v1.ContainerNames.REGISTRY_APP_CONTAINER_NAME;
 import static io.apicurio.registry.operator.resource.app.AppDeploymentResource.addEnvVar;
 import static io.apicurio.registry.operator.utils.Utils.isBlank;
 import static java.util.Optional.ofNullable;
 
 public class KafkaSql {
 
+    private static final Logger log = LoggerFactory.getLogger(KafkaSql.class);
+
     public static String ENV_STORAGE_KIND = "APICURIO_STORAGE_KIND";
     public static String ENV_KAFKASQL_BOOTSTRAP_SERVERS = "APICURIO_KAFKASQL_BOOTSTRAP_SERVERS";
 
-    public static void configureKafkaSQL(ApicurioRegistry3 primary, Map<String, EnvVar> env) {
+    public static void configureKafkaSQL(ApicurioRegistry3 primary, Deployment deployment,
+            Map<String, EnvVar> env) {
         ofNullable(primary.getSpec()).map(ApicurioRegistry3Spec::getApp).map(AppSpec::getStorage)
                 .map(StorageSpec::getKafkasql).ifPresent(kafkasql -> {
                     if (!isBlank(kafkasql.getBootstrapServers())) {
@@ -26,6 +33,11 @@ public class KafkaSql {
                                 new EnvVarBuilder().withName(ENV_STORAGE_KIND).withValue("kafkasql").build());
                         addEnvVar(env, new EnvVarBuilder().withName(ENV_KAFKASQL_BOOTSTRAP_SERVERS)
                                 .withValue(kafkasql.getBootstrapServers()).build());
+
+                        if (KafkaSqlTLS.configureKafkaSQLTLS(primary, deployment, REGISTRY_APP_CONTAINER_NAME,
+                                env)) {
+                            log.info("KafkaSQL storage with TLS security configured.");
+                        }
                     }
                 });
     }

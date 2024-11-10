94d1afc5ff1e6e079ee33b415ccfa2d0f1f70cbc
souravagrawal <souravagrawal1111@gmail.com>
2024-11-10 13:26:33 +0530

    Fixing Indentation Issues

--- integration-tests/src/test/java/io/apicurio/tests/serdes/apicurio/nats/AvroNatsSerdeIT.java
+++ integration-tests/src/test/java/io/apicurio/tests/serdes/apicurio/nats/AvroNatsSerdeIT.java
@@ -109,11 +109,12 @@ public class AvroNatsSerdeIT extends ApicurioRegistryBaseIT {
             Assertions.assertTrue(jsm.getStreamInfo(stream.getName()).getStreamState().getMsgCount() == 1);
 
             jsm.purgeStream(stream.getName());
-            //producer.publish(record);
-            //consumer.fetch().term(); // this will terminate the message, since there was only one message in
-                                     // stream and after calling terminate we should not have any message left
-                                     // in stream
-            //Assertions.assertTrue(jsm.getStreamInfo(stream.getName()).getStreamState().getMsgCount() == 0);
+            // producer.publish(record);
+            // consumer.fetch().term(); // this will terminate the message, since there was only one message
+            // in
+            // stream and after calling terminate we should not have any message left
+            // in stream
+            // Assertions.assertTrue(jsm.getStreamInfo(stream.getName()).getStreamState().getMsgCount() == 0);
             producer.publish(record);
 
             NatsConsumerRecord<GenericRecord> newMessage = consumer.fetch();

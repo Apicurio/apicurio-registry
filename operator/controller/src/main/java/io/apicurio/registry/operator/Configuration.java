75c607c195f484e11dc5c2412ae38f09ee8e8a49
Jakub Senko <m@jsenko.net>
2024-11-25 10:41:57 +0100

    prod(operator): support related image prefix

--- operator/controller/src/main/java/io/apicurio/registry/operator/Configuration.java
+++ operator/controller/src/main/java/io/apicurio/registry/operator/Configuration.java
@@ -1,26 +1,36 @@
 package io.apicurio.registry.operator;
 
+import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.ConfigProvider;
 
 public class Configuration {
+
+    private static final Config config = ConfigProvider.getConfig();
 
     private Configuration() {
     }
 
     public static String getAppImage() {
-        return ConfigProvider.getConfig().getOptionalValue("registry.app.image", String.class)
-                .orElseThrow(() -> new OperatorException(
-                        "Required configuration option 'registry.app.image' is not set."));
+        // spotless:off
+        return config.getOptionalValue("registry.app.image", String.class)
+                .or(() -> config.getOptionalValue("related.image.registry.app.image", String.class))
+                .orElseThrow(() -> new OperatorException("Required configuration option 'registry.app.image' is not set."));
+        // spotless:on
     }
 
     public static String getUIImage() {
-        return ConfigProvider.getConfig().getOptionalValue("registry.ui.image", String.class).orElseThrow(
-                () -> new OperatorException("Required configuration option 'registry.ui.image' is not set."));
+        // spotless:off
+        return config.getOptionalValue("registry.ui.image", String.class)
+                .or(() -> config.getOptionalValue("related.image.registry.ui.image", String.class))
+                .orElseThrow(() -> new OperatorException("Required configuration option 'registry.ui.image' is not set."));
+        // spotless:on
     }
 
     public static String getDefaultBaseHost() {
-        return ConfigProvider.getConfig()
-                .getOptionalValue("apicurio.operator.default-base-host", String.class).map(v -> "." + v)
+        // spotless:off
+        return config.getOptionalValue("apicurio.operator.default-base-host", String.class)
+                .map(v -> "." + v)
                 .orElse("");
+        // spotless:on
     }
 }

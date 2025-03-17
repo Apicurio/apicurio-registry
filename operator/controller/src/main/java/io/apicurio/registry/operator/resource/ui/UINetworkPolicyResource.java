4a31695bb4c8a820f1032bef960fc65a8b097ea6
Jakub Senko <m@jsenko.net>
2025-03-17 14:15:17 +0100

    fix(operator): OCP deployment issues

--- operator/controller/src/main/java/io/apicurio/registry/operator/resource/ui/UINetworkPolicyResource.java
+++ operator/controller/src/main/java/io/apicurio/registry/operator/resource/ui/UINetworkPolicyResource.java
@@ -2,7 +2,9 @@ package io.apicurio.registry.operator.resource.ui;
 
 import io.apicurio.registry.operator.api.v1.ApicurioRegistry3;
 import io.apicurio.registry.operator.resource.LabelDiscriminators.UINetworkPolicyDiscriminator;
+import io.fabric8.kubernetes.api.model.IntOrString;
 import io.fabric8.kubernetes.api.model.networking.v1.NetworkPolicy;
+import io.fabric8.kubernetes.api.model.networking.v1.NetworkPolicyIngressRuleBuilder;
 import io.javaoperatorsdk.operator.api.reconciler.Context;
 import io.javaoperatorsdk.operator.processing.dependent.kubernetes.CRUDKubernetesDependentResource;
 import io.javaoperatorsdk.operator.processing.dependent.kubernetes.KubernetesDependent;
@@ -31,6 +33,16 @@ public class UINetworkPolicyResource
     @Override
     protected NetworkPolicy desired(ApicurioRegistry3 primary, Context<ApicurioRegistry3> context) {
         var networkPolicy = UI_NETWORK_POLICY_KEY.getFactory().apply(primary);
+
+        // @formatter:off
+        networkPolicy.getSpec().getIngress().add(new NetworkPolicyIngressRuleBuilder()
+                    .addNewPort()
+                        .withProtocol("TCP")
+                        .withPort(new IntOrString(8080))
+                    .endPort()
+                .build());
+        // @formatter:on
+
         log.trace("Desired {} is {}", UI_NETWORK_POLICY_KEY.getId(), toYAML(networkPolicy));
         return networkPolicy;
     }

ef4a4050dfda8f074f7cb4076cfb092c9c5e7d28
Eric Wittmann <eric.wittmann@gmail.com>
2025-04-07 19:13:19 -0400

    Update the local ui config

--- ui/ui-app/configs/config-local.js
+++ ui/ui-app/configs/config-local.js
@@ -1,5 +1,8 @@
 var ApicurioRegistryConfig = {
     artifacts: {
         url: "http://localhost:8080/apis/registry/v3"
-    }
+    },
+    ui: {
+        oaiDocsUrl: "http://localhost:8889"
+    },
 };

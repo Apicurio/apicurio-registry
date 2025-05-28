6c394a1dd08480198446de09ede646142acbc431
Eric Wittmann <eric.wittmann@gmail.com>
2025-05-28 18:50:29 -0400

    Fix readonly test

--- app/src/test/java/io/apicurio/registry/storage/impl/readonly/ReadOnlyRegistryStorageTest.java
+++ app/src/test/java/io/apicurio/registry/storage/impl/readonly/ReadOnlyRegistryStorageTest.java
@@ -117,7 +117,7 @@ public class ReadOnlyRegistryStorageTest {
                 entry("getConfigProperty1", new State(false, s -> s.getConfigProperty(null))),
                 entry("getContentIdsReferencingArtifactVersion3",
                         new State(false, s -> s.getContentIdsReferencingArtifactVersion(null, null, null))),
-                entry("getGlobalIdsReferencingArtifactVersion2",
+                entry("getGlobalIdsReferencingArtifact2",
                         new State(false, s -> s.getGlobalIdsReferencingArtifact(null, null))),
                 entry("getGlobalIdsReferencingArtifactVersion3",
                         new State(false, s -> s.getGlobalIdsReferencingArtifactVersion(null, null, null))),

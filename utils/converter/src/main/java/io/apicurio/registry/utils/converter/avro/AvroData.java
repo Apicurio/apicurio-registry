47d933951baad7a2e90db5c3c5730a9fb231a08a
wobu <buchnerw@googlemail.com>
2025-06-16 15:35:29 +0200

    removed unused import in AvroData.java

--- utils/converter/src/main/java/io/apicurio/registry/utils/converter/avro/AvroData.java
+++ utils/converter/src/main/java/io/apicurio/registry/utils/converter/avro/AvroData.java
@@ -2,7 +2,6 @@ package io.apicurio.registry.utils.converter.avro;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.IntNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.NullNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;

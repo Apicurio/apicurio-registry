39f1dcd910c60358e08bc155e3cecfd66cf83e7c
Eric Wittmann <eric.wittmann@gmail.com>
2025-06-16 11:31:50 -0400

    Allow the auth token type to be configurable ID vs access

---
+++ ui/ui-app/configs/config-entra.js
@@ -0,0 +1,14 @@
+var ApicurioRegistryConfig = {
+    artifacts: {
+        url: "http://localhost:8080/apis/registry/v3"
+    },
+    ui: {
+        oaiDocsUrl: "http://localhost:8889"
+    },
+    auth: {
+        options: {
+            "tokenType": "id",
+            "logTokens": "true"
+        }
+    }
+};

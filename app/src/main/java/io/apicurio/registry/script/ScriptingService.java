c2049481b6acb91383743de90b025cb4986ee5c1
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2025-06-17 12:07:32 +0100

    Enable Quickjs4j caching by reusing Proxies (#6345)

--- app/src/main/java/io/apicurio/registry/script/ScriptingService.java
+++ app/src/main/java/io/apicurio/registry/script/ScriptingService.java
@@ -3,16 +3,26 @@ package io.apicurio.registry.script;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @ApplicationScoped
 public class ScriptingService {
 
     @Inject
     ArtifactTypeScriptProviderContext context;
 
+    Map<String, ArtifactTypeScriptProvider> cache = new HashMap();
+
     public ArtifactTypeScriptProvider createScriptProvider(String scriptLocation) {
-        String script = ScriptInterfaceUtils.loadScriptLibrary(scriptLocation);
-        ArtifactTypeScriptProvider_Proxy proxy = new ArtifactTypeScriptProvider_Proxy(script, context);
-        return proxy;
+        if (cache.containsKey(scriptLocation)) {
+            return cache.get(scriptLocation);
+        } else {
+            String script = ScriptInterfaceUtils.loadScriptLibrary(scriptLocation);
+            ArtifactTypeScriptProvider_Proxy proxy = new ArtifactTypeScriptProvider_Proxy(script, context);
+            cache.put(scriptLocation, proxy);
+            return proxy;
+        }
     }
 
 }

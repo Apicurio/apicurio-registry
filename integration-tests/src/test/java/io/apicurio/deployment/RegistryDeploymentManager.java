18630513c359b885a3d0a9e80c196436c32fde41
Eric Wittmann <eric.wittmann@gmail.com>
2025-07-29 09:39:28 -0400

    chore(ci): Remove unused test profile from SimpleAuthIt (#6439)
    * Remove unused test profile from SimpleAuthIt
    
    * Support https in integration tests
    
    * Add a test watcher to improve error output of integration test failures
    
    * Add debugging of registry base url for integration tests
    
    * Remove incorrect test port setting
    
    * Revert port setting change
    
    * Restore base url logic when REGISTRY_URL is null

--- integration-tests/src/test/java/io/apicurio/deployment/RegistryDeploymentManager.java
+++ integration-tests/src/test/java/io/apicurio/deployment/RegistryDeploymentManager.java
@@ -178,7 +178,6 @@ public class RegistryDeploymentManager implements TestExecutionListener {
 
                 System.setProperty("quarkus.http.test-host", registryRoute.getSpec().getHost());
                 System.setProperty("quarkus.http.test-port", "80");
-
             } catch (Exception ex) {
                 LOGGER.warn("The registry route already exists: ", ex);
             }

c059d39d1f4d437ef7efa78859e3d71dd48a224e
Eric Wittmann <eric.wittmann@gmail.com>
2025-07-30 08:39:17 -0400

    Masthead UI test now supports downstream, where the logo and alt text are branded differently

--- ui/tests/specs/masthead.spec.ts
+++ ui/tests/specs/masthead.spec.ts
@@ -1,8 +1,9 @@
 import { test, expect } from "@playwright/test";
 
 const REGISTRY_UI_URL: string = process.env["REGISTRY_UI_URL"] || "http://localhost:8888/";
-const EXPECTED_TITLE: string = process.env["EXPECTED_TITLE"] || "Apicurio Registry";
-const EXPECTED_LOGO_SRC: string = process.env["EXPECTED_LOGO_SRC"] || "/apicurio_registry_logo_reverse.svg";
+const IS_DOWNSTREAM: boolean = process.env["IS_DOWNSTREAM"] === "true";
+const EXPECTED_ALT: string = process.env["EXPECTED_ALT"] || IS_DOWNSTREAM ? "Red Hat build of Apicurio Registry" : "Apicurio Registry";
+const EXPECTED_LOGO_SRC: string = process.env["EXPECTED_LOGO_SRC"] || IS_DOWNSTREAM ? "/red-hat-logo-reverse-transparent-100px.png" : "/apicurio_registry_logo_reverse.svg";
 
 test("Masthead - Logo verification", async ({ page }) => {
     await page.goto(REGISTRY_UI_URL);
@@ -20,5 +21,5 @@ test("Masthead - Logo verification", async ({ page }) => {
     await expect(logoImage).toHaveAttribute("src", EXPECTED_LOGO_SRC);
 
     // Verify the alt text
-    await expect(logoImage).toHaveAttribute("alt", EXPECTED_TITLE);
+    await expect(logoImage).toHaveAttribute("alt", EXPECTED_ALT);
 });

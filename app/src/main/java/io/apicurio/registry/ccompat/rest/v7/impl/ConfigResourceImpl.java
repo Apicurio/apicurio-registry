b8fd8c8d3cda28662a4a583e6ccd705b553422d0
Eric Wittmann <eric.wittmann@gmail.com>
2025-08-06 12:03:56 -0400

    Forward port of e115183032d63edf28bbf0a627a7f15cbbf384d0

--- app/src/main/java/io/apicurio/registry/ccompat/rest/v7/impl/ConfigResourceImpl.java
+++ app/src/main/java/io/apicurio/registry/ccompat/rest/v7/impl/ConfigResourceImpl.java
@@ -13,12 +13,13 @@ import io.apicurio.registry.logging.audit.AuditingConstants;
 import io.apicurio.registry.metrics.health.liveness.ResponseErrorLivenessCheck;
 import io.apicurio.registry.metrics.health.readiness.ResponseTimeoutReadinessCheck;
 import io.apicurio.registry.model.GA;
+import io.apicurio.registry.rules.RulesProperties;
 import io.apicurio.registry.rules.compatibility.CompatibilityLevel;
 import io.apicurio.registry.storage.dto.RuleConfigurationDto;
 import io.apicurio.registry.storage.error.RuleNotFoundException;
 import io.apicurio.registry.types.RuleType;
 import io.apicurio.registry.utils.Functional.Runnable1Ex;
-import io.apicurio.registry.utils.Functional.RunnableEx;
+import jakarta.inject.Inject;
 import jakarta.interceptor.Interceptors;
 
 import java.util.Optional;
@@ -27,6 +28,9 @@ import java.util.function.Supplier;
 @Interceptors({ ResponseErrorLivenessCheck.class, ResponseTimeoutReadinessCheck.class })
 @Logged
 public class ConfigResourceImpl extends AbstractResource implements ConfigResource {
+
+    @Inject
+    RulesProperties rulesProperties;
 
     //FIMXE: The configuration implementation is not complete. We only do global and artifact level compatibility. Confluent has more options.
 
@@ -40,29 +44,28 @@ public class ConfigResourceImpl extends AbstractResource implements ConfigResour
         }
         catch (RuleNotFoundException ex) {
             GlobalConfigResponse response = new GlobalConfigResponse();
-            response.setCompatibilityLevel(CompatibilityLevel.NONE.name());
+            var compatRuleDto = rulesProperties.getDefaultGlobalRuleConfiguration(RuleType.COMPATIBILITY);
+            if (compatRuleDto != null) {
+                // Fallback to the default global rule
+                response.setCompatibilityLevel(compatRuleDto.getConfiguration());
+            } else {
+                // Fallback to NONE if no default rule is found
+                response.setCompatibilityLevel(CompatibilityLevel.NONE.name());
+            }
             return response;
         }
     }
 
     private <X extends Exception> void updateCompatibilityLevel(String level,
-                                                                Runnable1Ex<RuleConfigurationDto, X> updater, RunnableEx<X> deleter) throws X {
-        if (CompatibilityLevel.NONE.name().equals(level)) {
-            // delete the rule
-            deleter.run();
+                                                                Runnable1Ex<RuleConfigurationDto, X> updater) throws X {
+        try {
+            CompatibilityLevel.valueOf(level);
         }
-        else {
-            try {
-                CompatibilityLevel.valueOf(level);
-            }
-            catch (IllegalArgumentException ex) {
-                throw new IllegalArgumentException("Illegal compatibility level: " + level);
-            }
-            updater.run(RuleConfigurationDto.builder().configuration(level).build()); // TODO config
-            // should take
-            // CompatibilityLevel
-            // as param
+        catch (IllegalArgumentException ex) {
+            throw new IllegalArgumentException("Illegal compatibility level: " + level);
         }
+        updater.run(RuleConfigurationDto.builder().configuration(level).build());
+        // TODO config should take CompatibilityLevel as param
     }
 
     @Override
@@ -82,7 +85,7 @@ public class ConfigResourceImpl extends AbstractResource implements ConfigResour
             else {
                 storage.updateGlobalRule(RuleType.COMPATIBILITY, dto);
             }
-        }, () -> storage.deleteGlobalRule(RuleType.COMPATIBILITY));
+        });
 
         GlobalConfigResponse response = new GlobalConfigResponse();
         response.setCompatibilityLevel(request.getCompatibility());
@@ -104,14 +107,6 @@ public class ConfigResourceImpl extends AbstractResource implements ConfigResour
             else {
                 storage.updateArtifactRule(ga.getRawGroupIdWithNull(), ga.getRawArtifactId(),
                         RuleType.COMPATIBILITY, dto);
-            }
-        }, () -> {
-            try {
-                storage.deleteArtifactRule(ga.getRawGroupIdWithNull(), ga.getRawArtifactId(),
-                        RuleType.COMPATIBILITY);
-            }
-            catch (RuleNotFoundException e) {
-                // Ignore, fail only when the artifact is not found
             }
         });
 

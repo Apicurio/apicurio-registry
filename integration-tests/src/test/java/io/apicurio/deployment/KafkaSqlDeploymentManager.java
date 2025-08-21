9539ab19869c92c97500a0d3083713817cd3ecc6
Eric Wittmann <eric.wittmann@gmail.com>
2025-08-21 10:21:33 -0400

    Add logging to the integration test deployment layer for debugging

--- integration-tests/src/test/java/io/apicurio/deployment/KafkaSqlDeploymentManager.java
+++ integration-tests/src/test/java/io/apicurio/deployment/KafkaSqlDeploymentManager.java
@@ -40,49 +40,67 @@ public class KafkaSqlDeploymentManager {
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaSqlDeploymentManager.class);
 
     static void deployKafkaApp(String registryImage) throws Exception {
+        LOGGER.info("Deploying KafkaSQL-based Registry variant with test profile: {}", Constants.TEST_PROFILE);
+        
         switch (Constants.TEST_PROFILE) {
             case Constants.AUTH:
+                LOGGER.info("Configuring KafkaSQL Registry with authentication (Keycloak) enabled");
                 prepareTestsInfra(KAFKA_RESOURCES, APPLICATION_KAFKA_SECURED_RESOURCES, true, registryImage, false);
                 break;
             case Constants.MULTITENANCY:
+                LOGGER.info("Configuring KafkaSQL Registry with multitenancy support enabled");
                 prepareTestsInfra(KAFKA_RESOURCES, APPLICATION_KAFKA_MULTITENANT_RESOURCES, false, registryImage, true);
                 break;
             case Constants.KAFKA_SQL:
+                LOGGER.info("Configuring KafkaSQL Registry for database upgrade testing");
                 prepareKafkaDbUpgradeTests(registryImage);
                 break;
             case Constants.KAFKASQL_MANUAL:
+                LOGGER.info("Skipping automatic deployment for manual KafkaSQL testing");
                 break;
             default:
+                LOGGER.info("Configuring standard KafkaSQL Registry deployment");
                 prepareTestsInfra(KAFKA_RESOURCES, APPLICATION_KAFKA_RESOURCES, false, registryImage, false);
                 break;
         }
+        LOGGER.info("KafkaSQL Registry variant deployment completed successfully");
     }
 
     private static void prepareKafkaDbUpgradeTests(String registryImage) throws Exception {
-        LOGGER.info("Preparing data for KafkaSQL DB Upgrade migration tests...");
+        LOGGER.info("Preparing data for KafkaSQL DB Upgrade migration tests ##################################################");
 
         //For the migration tests first we deploy the 2.1 version and add the required data.
+        LOGGER.info("Phase 1: Deploying KafkaSQL Registry v2.1 with Kafka storage for initial data setup");
         prepareTestsInfra(KAFKA_RESOURCES, APPLICATION_2_1_KAFKA_RESOURCES, false, null, false);
+        LOGGER.info("Phase 1: Populating v2.1 KafkaSQL Registry with migration test data");
         prepareKafkaSqlMigrationData(ApicurioRegistryBaseIT.getRegistryBaseUrl());
 
         //Once all the data has been introduced, the old deployment is deleted.
+        LOGGER.info("Phase 1: Removing v2.1 KafkaSQL Registry deployment to prepare for v2.4 upgrade");
         deleteRegistryDeployment();
 
         //The Registry version 2.3 is deployed, the version introducing artifact references.
+        LOGGER.info("Phase 2: Deploying KafkaSQL Registry v2.4 for references migration testing");
         prepareTestsInfra(null, APPLICATION_2_4_KAFKA_RESOURCES, false, null, false);
+        LOGGER.info("Phase 2: Adding artifact references test data to v2.4 KafkaSQL Registry");
         prepareKafkaSqlReferencesMigrationData(ApicurioRegistryBaseIT.getRegistryBaseUrl());
 
         //Once the references data is ready, we delete this old deployment and finally the current one is deployed.
+        LOGGER.info("Phase 2: Removing v2.4 KafkaSQL Registry deployment to prepare for current version");
         deleteRegistryDeployment();
 
-        LOGGER.info("Finished preparing data for the KafkaSQL DB Upgrade tests.");
+        LOGGER.info("Phase 3: Deploying current KafkaSQL Registry version for upgrade validation");
         prepareTestsInfra(null, APPLICATION_KAFKA_RESOURCES, false, registryImage, false);
+        LOGGER.info("Finished preparing data for the KafkaSQL DB Upgrade tests ##################################################");
     }
 
     private static void deleteRegistryDeployment() {
+        LOGGER.info("Deleting KafkaSQL Registry deployment: {} from namespace: {}", APPLICATION_DEPLOYMENT, TEST_NAMESPACE);
+        
         final RollableScalableResource<Deployment> deploymentResource = kubernetesClient().apps().deployments().inNamespace(TEST_NAMESPACE).withName(APPLICATION_DEPLOYMENT);
 
         kubernetesClient().apps().deployments().inNamespace(TEST_NAMESPACE).withName(APPLICATION_DEPLOYMENT).delete();
+        LOGGER.info("KafkaSQL Registry deployment deletion initiated, waiting for completion (timeout: 60 seconds)...");
 
         //Wait for the deployment to be deleted
         CompletableFuture<List<Deployment>> deployment = deploymentResource
@@ -90,24 +108,37 @@ public class KafkaSqlDeploymentManager {
 
         try {
             deployment.get(60, TimeUnit.SECONDS);
+            LOGGER.info("KafkaSQL Registry deployment {} successfully deleted", APPLICATION_DEPLOYMENT);
         } catch (ExecutionException | InterruptedException | TimeoutException e) {
-            LOGGER.warn("Error waiting for namespace deletion", e);
+            LOGGER.warn("Error waiting for KafkaSQL deployment {} deletion", APPLICATION_DEPLOYMENT, e);
         } finally {
             deployment.cancel(true);
         }
     }
 
     private static void prepareKafkaSqlMigrationData(String registryBaseUrl) throws Exception {
+        LOGGER.info("Initializing migration test data for KafkaSQL Registry v2.1");
+        LOGGER.info("  -> Registry Base URL: {}", registryBaseUrl);
+        
         var registryClient = RegistryClientFactory.create(registryBaseUrl);
 
+        LOGGER.info("  -> Preparing Protobuf hash upgrade test data...");
         UpgradeTestsDataInitializer.prepareProtobufHashUpgradeTest(registryClient);
+        LOGGER.info("  -> Preparing log compaction test data...");
         UpgradeTestsDataInitializer.prepareLogCompactionTests(registryClient);
+        LOGGER.info("  -> Preparing Avro canonical hash upgrade test data...");
         UpgradeTestsDataInitializer.prepareAvroCanonicalHashUpgradeData(registryClient);
+        
+        LOGGER.info("KafkaSQL migration test data preparation completed");
     }
 
     private static void prepareKafkaSqlReferencesMigrationData(String registryBaseUrl) throws Exception {
+        LOGGER.info("  -> Preparing artifact references migration test data for KafkaSQL Registry v2.4...");
+        LOGGER.info("  -> Registry Base URL: {}", registryBaseUrl);
+        
         var registryClient = RegistryClientFactory.create(registryBaseUrl);
 
         UpgradeTestsDataInitializer.prepareReferencesUpgradeTest(registryClient);
+        LOGGER.info("  -> Artifact references migration test data preparation completed");
     }
 }
--- integration-tests/src/test/java/io/apicurio/deployment/SqlDeploymentManager.java
+++ integration-tests/src/test/java/io/apicurio/deployment/SqlDeploymentManager.java
@@ -53,47 +53,64 @@ public class SqlDeploymentManager {
     private static final Logger LOGGER = LoggerFactory.getLogger(SqlDeploymentManager.class);
 
     protected static void deploySqlApp(String registryImage) throws Exception {
+        LOGGER.info("Deploying SQL-based Registry variant with test profile: {}", Constants.TEST_PROFILE);
+        
         switch (Constants.TEST_PROFILE) {
             case Constants.AUTH:
+                LOGGER.info("Configuring SQL Registry with authentication (Keycloak) enabled");
                 prepareTestsInfra(DATABASE_RESOURCES, APPLICATION_SQL_SECURED_RESOURCES, true, registryImage, false);
                 break;
             case Constants.MULTITENANCY:
+                LOGGER.info("Configuring SQL Registry with multitenancy support enabled");
                 prepareTestsInfra(DATABASE_RESOURCES, APPLICATION_SQL_MULTITENANT_RESOURCES, false, registryImage, true);
                 break;
             case Constants.SQL:
+                LOGGER.info("Configuring SQL Registry for database upgrade testing");
                 prepareSqlDbUpgradeTests(registryImage);
                 break;
             default:
+                LOGGER.info("Configuring standard SQL Registry deployment");
                 prepareTestsInfra(DATABASE_RESOURCES, APPLICATION_SQL_RESOURCES, false, registryImage, false);
                 break;
         }
+        LOGGER.info("SQL Registry variant deployment completed successfully");
     }
 
     private static void prepareSqlDbUpgradeTests(String registryImage) throws Exception {
-        LOGGER.info("Preparing data for SQL DB Upgrade migration tests...");
+        LOGGER.info("Preparing data for SQL DB Upgrade migration tests ##################################################");
 
         //For the migration tests first we deploy the 2.1 version and add the required data.
+        LOGGER.info("Phase 1: Deploying Registry v2.1 with SQL database for initial data setup");
         prepareTestsInfra(DATABASE_RESOURCES, APPLICATION_2_1_SQL_RESOURCES, false, null, true);
+        LOGGER.info("Phase 1: Populating v2.1 Registry with migration test data");
         prepareSqlMigrationData(ApicurioRegistryBaseIT.getTenantManagerUrl(), ApicurioRegistryBaseIT.getRegistryBaseUrl());
 
         //Once all the data has been introduced, the old deployment is deleted.
+        LOGGER.info("Phase 1: Removing v2.1 Registry deployment to prepare for v2.4 upgrade");
         deleteRegistryDeployment();
 
         //The Registry version 2.3 is deployed, the version introducing artifact references.
+        LOGGER.info("Phase 2: Deploying Registry v2.4 for references migration testing");
         prepareTestsInfra(null, APPLICATION_2_4_SQL_RESOURCES, false, null, false);
+        LOGGER.info("Phase 2: Adding artifact references test data to v2.4 Registry");
         prepareSqlReferencesMigrationData();
 
         //Once the references data is ready, we delete this old deployment and finally the current one is deployed.
+        LOGGER.info("Phase 2: Removing v2.4 Registry deployment to prepare for current version");
         deleteRegistryDeployment();
 
-        LOGGER.info("Finished preparing data for the SQL DB Upgrade tests.");
+        LOGGER.info("Phase 3: Deploying current Registry version for upgrade validation");
         prepareTestsInfra(null, APPLICATION_SQL_MULTITENANT_RESOURCES, false, registryImage, false);
+        LOGGER.info("Finished preparing data for the SQL DB Upgrade tests ##################################################");
     }
 
     private static void deleteRegistryDeployment() {
+        LOGGER.info("Deleting Registry deployment: {} from namespace: {}", APPLICATION_DEPLOYMENT, TEST_NAMESPACE);
+        
         final RollableScalableResource<Deployment> deploymentResource = kubernetesClient().apps().deployments().inNamespace(TEST_NAMESPACE).withName(APPLICATION_DEPLOYMENT);
 
         kubernetesClient().apps().deployments().inNamespace(TEST_NAMESPACE).withName(APPLICATION_DEPLOYMENT).delete();
+        LOGGER.info("Registry deployment deletion initiated, waiting for completion (timeout: 60 seconds)...");
 
         //Wait for the deployment to be deleted
         CompletableFuture<List<Deployment>> deployment = deploymentResource
@@ -101,39 +118,53 @@ public class SqlDeploymentManager {
 
         try {
             deployment.get(60, TimeUnit.SECONDS);
+            LOGGER.info("Registry deployment {} successfully deleted", APPLICATION_DEPLOYMENT);
         } catch (ExecutionException | InterruptedException | TimeoutException e) {
-            LOGGER.warn("Error waiting for namespace deletion", e);
+            LOGGER.warn("Error waiting for deployment {} deletion", APPLICATION_DEPLOYMENT, e);
         } finally {
             deployment.cancel(true);
         }
     }
 
     private static void prepareSqlMigrationData(String tenantManagerUrl, String registryBaseUrl) throws Exception {
+        LOGGER.info("Initializing migration test data for SQL Registry v2.1");
+        LOGGER.info("  -> Tenant Manager URL: {}", tenantManagerUrl);
+        LOGGER.info("  -> Registry Base URL: {}", registryBaseUrl);
+        
         final TenantManagerClientImpl tenantManagerClient = new TenantManagerClientImpl(tenantManagerUrl, Collections.emptyMap(), null);
 
         //Warm up until the tenant manager is ready.
+        LOGGER.info("  -> Waiting for Tenant Manager to be ready...");
         TestUtils.retry(() -> tenantManagerClient.listTenants(TenantStatusValue.READY, 0, 1, SortOrder.asc, SortBy.tenantId));
 
-        LOGGER.info("Tenant manager is ready, filling registry with test data...");
+        LOGGER.info("  -> Tenant manager is ready, populating registry with storage upgrade test data...");
         UpgradeTestsDataInitializer.prepareTestStorageUpgrade(SqlStorageUpgradeIT.class.getSimpleName(), tenantManagerUrl, registryBaseUrl);
         TestUtils.waitFor("Waiting for tenant data to be available...", 3000, 180000, () -> tenantManagerClient.listTenants(TenantStatusValue.READY, 0, 51, SortOrder.asc, SortBy.tenantId).getCount() == 10);
-        LOGGER.info("Done filling registry with test data...");
+        LOGGER.info("  -> Storage upgrade test data populated successfully");
 
+        LOGGER.info("  -> Creating isolated tenant for upgrade tests...");
         MultitenancySupport mt = new MultitenancySupport(tenantManagerUrl, registryBaseUrl);
         TenantUser tenantUser = new TenantUser(UUID.randomUUID().toString(), UUID.randomUUID().toString(), "storageUpgrade", UUID.randomUUID().toString());
         final TenantUserClient tenantUpgradeClient = mt.createTenant(tenantUser);
+        LOGGER.info("  -> Isolated upgrade tenant created: {}", tenantUser.getTenantId());
 
         //Prepare the data for the content and canonical hash upgraders using an isolated tenant so we don't have data conflicts.
+        LOGGER.info("  -> Preparing Protobuf hash upgrade test data...");
         UpgradeTestsDataInitializer.prepareProtobufHashUpgradeTest(tenantUpgradeClient.client);
+        LOGGER.info("  -> Preparing Avro canonical hash upgrade test data...");
         UpgradeTestsDataInitializer.prepareAvroCanonicalHashUpgradeData(tenantUpgradeClient.client);
 
         SqlStorageUpgradeIT.upgradeTenantClient = tenantUpgradeClient.client;
         SqlProtobufCanonicalHashUpgraderIT.upgradeTenantClient = tenantUpgradeClient.client;
         SqlAvroUpgraderIT.upgradeTenantClient = tenantUpgradeClient.client;
         SqlReferencesUpgraderIT.upgradeTenantClient = tenantUpgradeClient.client;
+        
+        LOGGER.info("SQL migration test data preparation completed");
     }
 
     private static void prepareSqlReferencesMigrationData() throws Exception {
+        LOGGER.info("  -> Preparing artifact references migration test data for SQL Registry v2.4...");
         UpgradeTestsDataInitializer.prepareReferencesUpgradeTest(SqlReferencesUpgraderIT.upgradeTenantClient);
+        LOGGER.info("  -> Artifact references migration test data preparation completed");
     }
 }

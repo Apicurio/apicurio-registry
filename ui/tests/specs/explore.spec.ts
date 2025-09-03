8f590ad6da6b0f1b61e66af4ab0b36e0457145bc
Eric Wittmann <eric.wittmann@gmail.com>
2025-09-03 08:46:27 -0400

    Update UI tests to work with the updated UI

--- ui/tests/specs/explore.spec.ts
+++ ui/tests/specs/explore.spec.ts
@@ -7,20 +7,74 @@ const OPENAPI_DATA_V2_STR: string = JSON.stringify(OPENAPI_DATA_V2, null, 4);
 
 const REGISTRY_UI_URL: string = process.env["REGISTRY_UI_URL"] || "http://localhost:8888";
 
-test("End to End - Create artifact", async ({ page }) => {
+test("Explore - Create group", async ({ page }) => {
     await page.goto(REGISTRY_UI_URL);
     await expect(page).toHaveTitle(/Apicurio Registry/);
 
-    expect(page.getByTestId("btn-toolbar-create-artifact")).toBeDefined();
+    expect(page.getByTestId("btn-toolbar-create-group")).toBeDefined();
+
+    // Click the "Create group" button
+    await page.getByTestId("btn-toolbar-create-group").click();
+    await expect(page.getByTestId("create-group-groupId")).toHaveValue("");
+
+    // Create a new group
+
+    // Fill out create group modal/form
+    await page.getByTestId("create-group-groupId").fill("ExploreTest");
+
+    // Click "Create" on the modal
+    await page.getByTestId("create-group-modal-btn-create").click();
+
+    // Make sure we redirected to the group page.
+    await expect(page).toHaveURL(/.+\/explore\/ExploreTest/);
+
+    // Assert the meta-data is as expected
+    await expect(page.getByText("ExploreTest")).toBeDefined();
+    await expect(page.getByText("Group metadata")).toBeDefined();
+    await expect(page.getByText("Artifacts in group")).toBeDefined();
+});
+
+
+test("Explore - Group specific rules", async ({ page }) => {
+    // Navigate to the artifact details page
+    await page.goto(`${REGISTRY_UI_URL}/explore/ExploreTest`);
+
+    // Click the "rules" tab
+    await page.getByTestId("group-rules-tab").click();
+
+    await expect(page.locator("div.rule")).toHaveCount(3);
+    await expect(page.locator("#validity-rule-name")).toContainText("Validity rule");
+    await expect(page.locator("#compatibility-rule-name")).toContainText("Compatibility rule");
+    await expect(page.locator("#integrity-rule-name")).toContainText("Integrity rule");
+
+    // Enable the Rule
+    await page.getByTestId("rules-validity-enable").click();
+    expect(page.getByTestId("rules-validity-config-toggle")).toBeDefined();
+
+    // Click the Rule Configuration toggle
+    await page.getByTestId("rules-validity-config-toggle").click();
+    expect(page.getByTestId("rules-validity-config-syntaxOnly")).toBeDefined();
+
+    // Select "syntax only" config option
+    await page.getByTestId("validity-config-syntax").click();
+    expect(page.getByTestId("rules-validity-disable")).toBeDefined();
+});
+
+
+test("Explore - Create artifact", async ({ page }) => {
+    // Navigate to the group details page
+    await page.goto(`${REGISTRY_UI_URL}/explore/ExploreTest`);
+    await expect(page).toHaveTitle(/Apicurio Registry/);
+
+    expect(page.getByTestId("btn-create-artifact")).toBeDefined();
 
     // Click the "Create artifact" button
-    await page.getByTestId("btn-toolbar-create-artifact").click();
-    await expect(page.getByTestId("create-artifact-modal-group")).toHaveValue("");
+    await page.getByTestId("btn-create-artifact").click();
+    await expect(page.getByTestId("create-artifact-modal-group")).toHaveValue("ExploreTest");
 
     // Create a new artifact
 
     // Fill out page 1 of the create artifact wizard
-    await page.getByTestId("create-artifact-modal-group").fill("e2e");
     await page.getByTestId("create-artifact-modal-id").fill("MyArtifact");
     await page.getByTestId("create-artifact-modal-type-select").click();
     await page.getByTestId("create-artifact-modal-OPENAPI").click();
@@ -46,7 +100,7 @@ test("End to End - Create artifact", async ({ page }) => {
     await page.locator("#next-wizard-page").click();
 
     // Make sure we redirected to the artifact page.
-    await expect(page).toHaveURL(/.+\/explore\/e2e\/MyArtifact/);
+    await expect(page).toHaveURL(/.+\/explore\/ExploreTest\/MyArtifact/);
 
     // Assert the meta-data is as expected
     await expect(page.getByTestId("artifact-details-name")).toHaveText("Test Artifact");
@@ -55,9 +109,9 @@ test("End to End - Create artifact", async ({ page }) => {
 });
 
 
-test("End to End - Edit artifact metadata", async ({ page }) => {
+test("Explore - Edit artifact metadata", async ({ page }) => {
     // Navigate to the artifact details page
-    await page.goto(`${REGISTRY_UI_URL}/explore/e2e/MyArtifact`);
+    await page.goto(`${REGISTRY_UI_URL}/explore/ExploreTest/MyArtifact`);
 
     // Click the "Edit" button to show the modal
     await page.getByTestId("artifact-btn-edit").click();
@@ -90,9 +144,12 @@ test("End to End - Edit artifact metadata", async ({ page }) => {
 
 
 
-test("End to End - Artifact specific rules", async ({ page }) => {
+test("Explore - Artifact specific rules", async ({ page }) => {
     // Navigate to the artifact details page
-    await page.goto(`${REGISTRY_UI_URL}/explore/e2e/MyArtifact`);
+    await page.goto(`${REGISTRY_UI_URL}/explore/ExploreTest/MyArtifact`);
+
+    // Click the "rules" tab
+    await page.getByTestId("artifact-rules-tab").click();
 
     await expect(page.locator("div.rule")).toHaveCount(3);
     await expect(page.locator("#validity-rule-name")).toContainText("Validity rule");
@@ -113,25 +170,32 @@ test("End to End - Artifact specific rules", async ({ page }) => {
 });
 
 
-test("End to End - Create new version", async ({ page }) => {
+test("Explore - Create new version", async ({ page }) => {
     // Navigate to the artifact details page
-    await page.goto(`${REGISTRY_UI_URL}/explore/e2e/MyArtifact`);
-
-    // Click the "versions" tab
-    await page.getByTestId("versions-tab").click();
+    await page.goto(`${REGISTRY_UI_URL}/explore/ExploreTest/MyArtifact`);
 
     // Create a new version
-    await page.getByTestId("btn-toolbar-create-version").click();
+    await page.getByTestId("btn-create-version").click();
+    await page.getByTestId("create-version-version").fill("2.0.0");
     await page.locator("#version-content").fill(OPENAPI_DATA_V2_STR);
     await page.getByTestId("modal-btn-create").click();
 
     // Make sure we redirected to the artifact detail page.
-    await expect(page).toHaveURL(/.+\/explore\/e2e\/MyArtifact\/versions\/2/);
+    await expect(page).toHaveURL(/.+\/explore\/ExploreTest\/MyArtifact\/versions\/2.0.0/);
 });
 
 
-test("End to End - Delete artifact", async ({ page }) => {
-    await page.goto(`${REGISTRY_UI_URL}/explore/e2e/MyArtifact`);
+test("Explore - Delete artifact", async ({ page }) => {
+    await page.goto(`${REGISTRY_UI_URL}/explore/ExploreTest/MyArtifact`);
+    await page.getByTestId("header-btn-delete").click();
+    await page.getByTestId("modal-btn-delete").click();
+
+    await expect(page).toHaveURL(/.+\/explore/);
+});
+
+
+test("Explore - Delete group", async ({ page }) => {
+    await page.goto(`${REGISTRY_UI_URL}/explore/ExploreTest`);
     await page.getByTestId("header-btn-delete").click();
     await page.getByTestId("modal-btn-delete").click();
 

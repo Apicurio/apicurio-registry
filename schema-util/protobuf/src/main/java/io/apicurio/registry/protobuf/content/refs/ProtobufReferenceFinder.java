5b8078e4b36faa4949ca00f6446728a09d0ed9df
Carles Arnal <carlesarnal92@gmail.com>
2025-11-28 14:02:58 +0100

    Improve handling of well-known types

--- schema-util/protobuf/src/main/java/io/apicurio/registry/protobuf/content/refs/ProtobufReferenceFinder.java
+++ schema-util/protobuf/src/main/java/io/apicurio/registry/protobuf/content/refs/ProtobufReferenceFinder.java
@@ -1,15 +1,10 @@
 package io.apicurio.registry.protobuf.content.refs;
 
-import com.google.protobuf.Descriptors;
 import io.apicurio.registry.content.TypedContent;
 import io.apicurio.registry.utils.protobuf.schema.ProtobufSchemaUtils;
 import io.apicurio.registry.content.refs.ExternalReference;
 import io.apicurio.registry.content.refs.ReferenceFinder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.util.Collections;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -18,28 +13,17 @@ import java.util.stream.Collectors;
  */
 public class ProtobufReferenceFinder implements ReferenceFinder {
 
-    private static final Logger log = LoggerFactory.getLogger(ProtobufReferenceFinder.class);
-
     /**
      * @see io.apicurio.registry.content.refs.ReferenceFinder#findExternalReferences(TypedContent)
      */
     @Override
     public Set<ExternalReference> findExternalReferences(TypedContent content) {
-        try {
-            // Use protobuf4j to compile and get all dependencies
-            Descriptors.FileDescriptor fileDescriptor = ProtobufSchemaUtils.parseAndCompile(
-                    "schema.proto", content.getContent().content(), Map.of());
-
-            // Get all imports from the FileDescriptor, excluding well-known types
-            return fileDescriptor.getDependencies().stream()
-                    .map(Descriptors.FileDescriptor::getName)
-                    .filter(imprt -> !imprt.startsWith("google/protobuf/"))
-                    .map(ExternalReference::new)
-                    .collect(Collectors.toSet());
-        } catch (Exception e) {
-            log.error("Error finding external references in a Protobuf file.", e);
-            return Collections.emptySet();
-        }
+        // Extract imports from the proto content without compiling
+        // This allows finding references even when the imported files are not available
+        return ProtobufSchemaUtils.extractNonWellKnownImports(content.getContent().content())
+                .stream()
+                .map(ExternalReference::new)
+                .collect(Collectors.toSet());
     }
 
 }

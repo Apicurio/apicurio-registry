b26a90ac5ca14f3acb1ce89c05ac0a37ba18cbce
Eric Wittmann <eric.wittmann@gmail.com>
2026-02-06 08:00:04 -0500

    Upgrade from eslint 8 to 9

---
+++ ui/ui-docs/eslint.config.js
@@ -0,0 +1,25 @@
+import apicurioConfig from "@apicurio/eslint-config";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+
+export default [
+    {
+        ignores: ["dist/**", "eslint.config.js"]
+    },
+    ...apicurioConfig,
+    {
+        files: ["**/*.ts", "**/*.tsx"],
+        plugins: {
+            "react-hooks": reactHooks,
+            "react-refresh": reactRefresh
+        },
+        rules: {
+            "react-hooks/rules-of-hooks": "error",
+            "react-hooks/exhaustive-deps": "off",
+            "react-refresh/only-export-components": [
+                "warn",
+                { allowConstantExport: true }
+            ]
+        }
+    }
+];

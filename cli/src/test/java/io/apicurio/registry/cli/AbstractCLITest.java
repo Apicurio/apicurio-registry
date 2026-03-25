351b524f76c3797bc18fc2497ede53e727d99c8b
Jakub Senko <m@jsenko.net>
2026-03-25 18:47:23 +0100

    ci: improve parallelism of release.yaml workflow, improve build of CLI artifacts

--- cli/src/test/java/io/apicurio/registry/cli/AbstractCLITest.java
+++ cli/src/test/java/io/apicurio/registry/cli/AbstractCLITest.java
@@ -2,6 +2,7 @@ package io.apicurio.registry.cli;
 
 import io.apicurio.registry.cli.config.Config;
 import io.apicurio.registry.cli.services.Client;
+import io.apicurio.registry.cli.tags.DockerRequired;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -26,6 +27,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  * Base class for CLI tests with common setup and utility methods.
  * Subclasses must be annotated with @QuarkusTest.
  */
+@DockerRequired
 public abstract class AbstractCLITest {
 
     protected static GenericContainer<?> registryContainer;

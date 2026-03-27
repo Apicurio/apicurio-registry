3addcf18f823877167d00eca30c6d8a21874b5e3
Matheus André Galvão Da Silva <matheusandr2@gmail.com>
2026-03-26 23:35:57 -0300

    remove import

--- schema-util/json/src/main/java/io/apicurio/registry/json/rules/validity/JsonSchemaContentValidator.java
+++ schema-util/json/src/main/java/io/apicurio/registry/json/rules/validity/JsonSchemaContentValidator.java
@@ -19,7 +19,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * A content validator implementation for the JsonSchema content type.

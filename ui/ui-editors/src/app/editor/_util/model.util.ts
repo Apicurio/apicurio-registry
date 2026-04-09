36bb5907669521f6e45278b1c8ac50e4ca6dfc8b
Eric Wittmann <eric.wittmann@gmail.com>
2026-04-09 15:23:08 -0400

    refactor(ui): inline apicurio-ts-core into ui-editors
    Migrate the source code from the apicurio-ts-core npm library directly
    into ui-editors so the external repository can be archived.
    
    - Add _messaging/topic.ts (Topic, TopicSubscription, TopicSubscriber)
    - Add _messaging/debouncer.ts (Debouncer)
    - Add _util/object.util.ts (ObjectUtils)
    - Add _util/array.util.ts (ArrayUtils)
    - Add _util/string.util.ts (StringUtils)
    - Update 38 consumer files to use local relative imports
    - Remove apicurio-ts-core dependency from package.json
    
    Closes #7728

--- ui/ui-editors/src/app/editor/_util/model.util.ts
+++ ui/ui-editors/src/app/editor/_util/model.util.ts
@@ -1,23 +1,6 @@
-/**
- * @license
- * Copyright 2022 Red Hat
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import {AaiSchema, Library, Node, OasSchema, ReferenceUtil} from "@apicurio/data-models";
 import {ApiEditorUser} from "../_models/editor-user.model";
-import {StringUtils} from "apicurio-ts-core";
+import {StringUtils} from "./string.util";
 
 // UI-facing text representation of an example value before it is parsed back into its schema type.
 export type ExampleTextValue = string | null | undefined;

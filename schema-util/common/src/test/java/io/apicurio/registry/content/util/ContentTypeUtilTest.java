0c286dea03926d20710d93fdea31c7e8745cae20
dolliecoder <dollychahar27@gmail.com.com>
2026-07-14 12:32:47 +0000

    fix(core): Resolve FIXME for GraphQL content-type auto-detection
    Signed-off-by: dolliecoder <dollychahar27@gmail.com.com>

--- schema-util/common/src/test/java/io/apicurio/registry/content/util/ContentTypeUtilTest.java
+++ schema-util/common/src/test/java/io/apicurio/registry/content/util/ContentTypeUtilTest.java
@@ -1,6 +1,7 @@
 package io.apicurio.registry.content.util;
 
 import io.apicurio.registry.content.ContentHandle;
+import io.apicurio.registry.types.ContentTypes;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -48,22 +49,65 @@ public class ContentTypeUtilTest {
 
     @Test
     public void testIsParsableGraphQL() {
+        // Valid GraphQL schema
         ContentHandle content = ContentHandle.create("type Query { hello: String }");
         Assertions.assertTrue(ContentTypeUtil.isParsableGraphQL(content));
-        
+
+        // GraphQL schema keyword
         ContentHandle content2 = ContentHandle.create("schema{ query: Query }");
         Assertions.assertTrue(ContentTypeUtil.isParsableGraphQL(content2));
 
+        // GraphQL with tab whitespace
         ContentHandle content3 = ContentHandle.create("type\tQuery { hello: String }");
         Assertions.assertTrue(ContentTypeUtil.isParsableGraphQL(content3));
 
+        // Protobuf should not be detected as GraphQL
         ContentHandle notGraphql = ContentHandle.create("message Hello { string msg = 1; }");
         Assertions.assertFalse(ContentTypeUtil.isParsableGraphQL(notGraphql));
+
+        // JSON-like content should not be detected as GraphQL
+        ContentHandle jsonLike = ContentHandle.create("{ \"key\": \"value\" }");
+        Assertions.assertFalse(ContentTypeUtil.isParsableGraphQL(jsonLike));
+
+        // XML-like content should not be detected as GraphQL
+        ContentHandle xmlLike = ContentHandle.create("<root><child/></root>");
+        Assertions.assertFalse(ContentTypeUtil.isParsableGraphQL(xmlLike));
+
+        // Empty content should not be detected as GraphQL
+        ContentHandle empty = ContentHandle.create("");
+        Assertions.assertFalse(ContentTypeUtil.isParsableGraphQL(empty));
     }
 
     @Test
     public void testDetermineContentType_GraphQL() {
+        // GraphQL schema that was previously misclassified as YAML
         ContentHandle content = ContentHandle.create("type Query { hello: String }");
-        Assertions.assertEquals(io.apicurio.registry.types.ContentTypes.APPLICATION_GRAPHQL, ContentTypeUtil.determineContentType(content));
+        Assertions.assertEquals(ContentTypes.APPLICATION_GRAPHQL, ContentTypeUtil.determineContentType(content));
+    }
+
+    @Test
+    public void testDetermineContentType_YamlStillWorks() {
+        // Normal YAML document should still resolve to application/x-yaml
+        ContentHandle yaml = ContentHandle.create("foo: bar\nbaz: qux\n");
+        Assertions.assertEquals("application/x-yaml", ContentTypeUtil.determineContentType(yaml));
+
+        // Nested YAML
+        ContentHandle nestedYaml = ContentHandle.create("apiVersion: v1\nkind: Service\nmetadata:\n  name: test\n");
+        Assertions.assertEquals("application/x-yaml", ContentTypeUtil.determineContentType(nestedYaml));
+    }
+
+    @Test
+    public void testDetermineContentType_EdgeCases() {
+        // JSON should still be detected as JSON, not GraphQL
+        ContentHandle json = ContentHandle.create("{\"type\": \"Query\"}");
+        Assertions.assertEquals("application/json", ContentTypeUtil.determineContentType(json));
+
+        // XML should still be detected as XML
+        ContentHandle xml = ContentHandle.create("<type>Query</type>");
+        Assertions.assertEquals("application/xml", ContentTypeUtil.determineContentType(xml));
+
+        // Protobuf should still be the fallback
+        ContentHandle protobuf = ContentHandle.create("message Foo { string bar = 1; }");
+        Assertions.assertEquals(ContentTypes.APPLICATION_PROTOBUF, ContentTypeUtil.determineContentType(protobuf));
     }
 }

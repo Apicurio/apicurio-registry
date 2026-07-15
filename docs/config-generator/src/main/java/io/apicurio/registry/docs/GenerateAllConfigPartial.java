91ac09d312d6d9ca7927f3063d46f1975ccf0b47
krish-kedia <krishkedia06@gmail.com>
2026-07-16 04:53:07 +0530

    fix: safely collapse whitespace in config generator descriptions
    Reverts the problematic header addition and refines the regex to safely collapse horizontal whitespace without creating multi-line gaps that trip up the license checker.
    
    Signed-off-by: krish-kedia <krishkedia06@gmail.com>

--- docs/config-generator/src/main/java/io/apicurio/registry/docs/GenerateAllConfigPartial.java
+++ docs/config-generator/src/main/java/io/apicurio/registry/docs/GenerateAllConfigPartial.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2021 Red Hat
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package io.apicurio.registry.docs;
 
 import io.apicurio.common.apps.config.ConfigPropertyCategory;
@@ -242,7 +226,7 @@ public class GenerateAllConfigPartial {
 
                     var variant = (String) info.get().value("category").value();
                     var category = ConfigPropertyCategory.valueOf(variant).getRawValue();
-                    var description = Optional.ofNullable(info.get().value("description")).map(v -> v.value().toString().replaceAll("[ \\t]+", " ").trim()).orElse("");
+                    var description = Optional.ofNullable(info.get().value("description")).map(v -> v.value().toString().replaceAll("(?<=[^\\n \\t])[ \\t]{2,}", " ").trim()).orElse("");
 
                     var availableSince = Optional.ofNullable(info.get().value("availableSince"))
                             .map(v -> v.value().toString())
@@ -290,7 +274,7 @@ public class GenerateAllConfigPartial {
 
             var variant = (String) info.get().value("category").value();
             var category = ConfigPropertyCategory.valueOf(variant).getRawValue();
-            var description = Optional.ofNullable(info.get().value("description")).map(v -> v.value().toString().replaceAll("[ \\t]+", " ").trim()).orElse("");
+            var description = Optional.ofNullable(info.get().value("description")).map(v -> v.value().toString().replaceAll("(?<=[^\\n \\t])[ \\t]{2,}", " ").trim()).orElse("");
 
             var availableSince = Optional.ofNullable(info.get().value("availableSince"))
                     .map(v -> v.value().toString())

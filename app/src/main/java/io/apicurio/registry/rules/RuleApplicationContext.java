4e316f768ff6d8332a07c2f69c225be78ba78a8b
rizzler <chinmayikr0252@gmail.com>
2026-08-07 11:31:20 +0530

    fix(rules): remove final modifier on @Builder.Default fields in RuleApplicationContext (#7820)
    Signed-off-by: rizzler <chinmayikr0252@gmail.com>

--- app/src/main/java/io/apicurio/registry/rules/RuleApplicationContext.java
+++ app/src/main/java/io/apicurio/registry/rules/RuleApplicationContext.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2026 Red Hat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.apicurio.registry.rules;
 
 import io.apicurio.registry.content.TypedContent;
@@ -26,13 +42,13 @@ public class RuleApplicationContext {
     private final TypedContent content;
 
     @Builder.Default
-    private final RuleApplicationType ruleApplicationType = RuleApplicationType.CREATE;
+    private RuleApplicationType ruleApplicationType = RuleApplicationType.CREATE;
 
     @Builder.Default
-    private final List<ArtifactReference> references = Collections.emptyList();
+    private List<ArtifactReference> references = Collections.emptyList();
 
     @Builder.Default
-    private final Map<String, TypedContent> resolvedReferences = Collections.emptyMap();
+    private Map<String, TypedContent> resolvedReferences = Collections.emptyMap();
 
     /**
      * Storage instance override. If null, {@link RulesService} uses its injected default storage.

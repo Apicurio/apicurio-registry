14f5ac55b4c3c0a66625d8d37b881813c198d817
Myself-Praveen <praveen104685@gmail.com>
2026-08-08 21:19:57 +0530

    fix(search): order static imports correctly in DataContractsResourceTest
    Signed-off-by: Myself-Praveen <praveen104685@gmail.com>

--- app/src/test/java/io/apicurio/registry/noprofile/rest/v3/DataContractsResourceTest.java
+++ app/src/test/java/io/apicurio/registry/noprofile/rest/v3/DataContractsResourceTest.java
@@ -50,9 +50,9 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
-import static org.hamcrest.Matchers.not;
 
 @QuarkusTest
 public class DataContractsResourceTest extends AbstractResourceTestBase {

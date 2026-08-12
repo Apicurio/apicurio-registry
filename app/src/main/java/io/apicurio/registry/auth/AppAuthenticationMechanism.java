c0e6782c74a0cc5ea6f8720360d66df5c7694002
jainarjav80-sys <jainarjav80@gmail.com>
2026-08-12 22:49:59 +0530

    fix(auth): Revert to defensive Instance injection per review feedback

--- app/src/main/java/io/apicurio/registry/auth/AppAuthenticationMechanism.java
+++ app/src/main/java/io/apicurio/registry/auth/AppAuthenticationMechanism.java
@@ -67,7 +67,7 @@ public class AppAuthenticationMechanism implements HttpAuthenticationMechanism {
     AuthConfig authConfig;
 
     @Inject
-    BasicAuthenticationMechanism basicAuthenticationMechanism;
+    Instance<BasicAuthenticationMechanism> basicAuthenticationMechanism;
 
     @Inject
     Instance<FormAuthenticationMechanism> formAuthenticationMechanism;
@@ -133,8 +133,8 @@ public class AppAuthenticationMechanism implements HttpAuthenticationMechanism {
         }
 
         Map<String, Supplier<AuthenticationStrategy>> strategyFactories = new LinkedHashMap<>();
-        strategyFactories.put(MECH_BASIC, () -> authConfig.isBasicAuthEnabled()
-                ? new DelegatingAuthenticationStrategy(MECH_BASIC, basicAuthenticationMechanism)
+        strategyFactories.put(MECH_BASIC, () -> authConfig.isBasicAuthEnabled() && basicAuthenticationMechanism.isResolvable()
+                ? new DelegatingAuthenticationStrategy(MECH_BASIC, basicAuthenticationMechanism.get())
                 : null);
         strategyFactories.put(MECH_FORM, () -> {
             if (!authConfig.isFormAuthEnabled()) {

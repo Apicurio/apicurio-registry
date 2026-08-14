f51398287bd74d9938b0aad6d9932ff84079f8d4
Manish-Yadav77 <manishkyadav969@gmail.com>
2026-08-14 17:15:31 +0530

    fix: set playwright baseURL for relative page.goto calls

--- ui/tests/playwright.config.ts
+++ ui/tests/playwright.config.ts
@@ -29,6 +29,7 @@ export default defineConfig({
     use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
+        baseURL: "http://localhost:8888",
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         trace: process.env.CI ? "on-first-retry" : "on",

ef10326dbd0f92eeb36cb43226664222fcaa0a2a
sahu-virendra-1908 <virendrashivsahu@gmail.com>
2026-08-14 17:07:14 +0530

    style: clean up KafkaSQL coordinator formatting

--- app/src/main/java/io/apicurio/registry/storage/impl/kafkasql/KafkaSqlCoordinator.java
+++ app/src/main/java/io/apicurio/registry/storage/impl/kafkasql/KafkaSqlCoordinator.java
@@ -47,13 +47,12 @@ public class KafkaSqlCoordinator {
      */
     public Object waitForResponse(UUID uuid) {
         try {
-           boolean completed = latches.get(uuid).await(
-                configuration.get().getResponseTimeout().toMillis(), TimeUnit.MILLISECONDS);
+            boolean completed = latches.get(uuid).await(
+                    configuration.get().getResponseTimeout().toMillis(), TimeUnit.MILLISECONDS);
             if (!completed) {
                 throw new RegistryException(
-                   "[KafkaSqlCoordinator] Timed out waiting for a Kafka Sql response for operation " + uuid);
+                        "[KafkaSqlCoordinator] Timed out waiting for a Kafka Sql response for operation " + uuid);
             }
-            
 
             Object rval = returnValues.remove(uuid);
             if (rval == NULL) {
